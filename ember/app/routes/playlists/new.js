17b1c62c0180f1f434680cf018686785b394a9ca
Oskar Rough <oskarrough@gmail.com>
2014-05-28 18:03:49 +0200

    Prevent multiple playlists

--- ember/app/routes/playlists/new.js
+++ ember/app/routes/playlists/new.js
@@ -1,5 +1,15 @@
 var PlaylistsNewRoute = Ember.Route.extend({
+	beforeModel: function() {
+		var self = this;
+		var auth = this.controllerFor('auth');
 
+		// Redirect to 'Application/Home' if the user already has a playlist
+		this.get('store').find('user', auth.get('currentUser.id')).then(function(user) {
+			if (user.get('hasPlaylist')) {
+				self.transitionTo('application');
+			}
+		});
+	}
 });
 
 export default PlaylistsNewRoute;
