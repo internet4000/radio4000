8f0ac562f2728d987c26272c625c721dd81a750c
Oskar Rough <oskarrough@gmail.com>
2014-09-14 11:04:59 +0200

    Remove the old chrome-extension

--- chrome-extension/app/scripts/background.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-chrome.runtime.onInstalled.addListener(function (details) {
-    console.log('previousVersion', details.previousVersion);
-});
-
-
-console.log('\'Allo \'Allo! Event Page for Browser Action');
