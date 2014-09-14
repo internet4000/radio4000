9373b20cd8d697fc3d67c08e39ea43d92d91a22b
Oskar Rough <oskarrough@gmail.com>
2014-09-14 15:15:36 +0200

    Clean templates

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,7 @@
-// most of this is duplicate code of app controller. should send events there instead?
-
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+	// pass the actions to the auth controller
 	actions: {
 		login: function(provider) {
 			this.get('auth').login(provider);
