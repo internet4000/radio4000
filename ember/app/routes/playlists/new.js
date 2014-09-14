a510f2f50774c19a21dd8135bd2a83930357cd94
Oskar Rough <oskarrough@gmail.com>
2014-09-14 02:37:37 +0200

    Styling

--- ember/app/routes/playlists/new.js
+++ ember/app/routes/playlists/new.js
@@ -4,16 +4,13 @@ export default Ember.Route.extend({
 	beforeModel: function() {
 
 		// abort if not logged in
-		if (!this.get('auth.authed')) {
+		if (!this.get('auth.authed') ) {
 			this.transitionTo('application');
 		}
 
-		// // abort if the user already has a playlist
-		// this.get('store').find('user', auth.get('user.id')).then(function(user) {
-		// 	if (user.get('hasPlaylist')) {
-		// 		alert('Sorry, you already have a playlist. Go add tracks!');
-		// 		self.transitionTo('application');
-		// 	}
-		// });
+		// abort if user already has a playlist
+		if (!this.get('auth.user.hasPlaylist')) {
+			this.transitionTo('application');
+		}
 	}
 });
