518acaed3e606037524d1b7e679aa441090d465e
Oskar Rough <oskarrough@gmail.com>
2014-09-19 16:31:31 +0200

    Clean up templates

--- app/routes/track.js
+++ app/routes/track.js
@@ -1,9 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model: function(params) {
-		return this.store.find('track', params.track_id);
-	},
+	// model: function(params) {
+	// 	return this.store.find('track', params.track_id);
+	// },
 
 	// don't use the outlet of playlist.hbs, but an outlet in application.hbs
 	renderTemplate: function() {
