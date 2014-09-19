5c0551671b9e619acab6c1e7181650eed36a9e13
Oskar Rough <oskarrough@gmail.com>
2014-09-19 17:35:30 +0200

    Yadayada

--- app/models/playlist.js
+++ app/models/playlist.js
@@ -4,6 +4,9 @@ export default DS.Model.extend({
 	created: DS.attr('number'),
 	title: DS.attr('string'),
 	slug: DS.attr('string'),
+	// autoSlug: function() {
+	// 	return this.get('slug').replace(/\s+/g, '-').toLowerCase();
+	// },
 	body: DS.attr('string'),
 	user: DS.belongsTo('user'),
 	tracks: DS.hasMany('track', { async: true })
--- app/views/playback.js
+++ app/views/playback.js
@@ -1,9 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-	didInsertElement: function() {
-		// var embed = '<iframe id="player" type="text/html" width="640" height="390" src="http://www.youtube.com/embed/Eu7p9i2WCHE?enablejsapi=1" frameborder="0"></iframe>';
-  // 		this.$().append(embed);
-  		// var window.muchplayer;
-	}
 });
