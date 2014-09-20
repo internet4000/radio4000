fde6b9f884202911cbd221c4de528494d5ea3f79
Oskar Rough <oskarrough@gmail.com>
2014-09-20 15:30:06 +0200

    Delete reverse helper

--- app/helpers/reverse.js
+++
@@ -1,9 +0,0 @@
-Ember.Handlebars.registerHelper('reverse', function(context, options) {
-	var ret = "";
-
-	for(var i=context.length-1; i>=0; i--) {
-		ret = ret + options.fn(context[i]);
-	}
-
-	return ret;
-});
