20ff7dac2db114b23eb0081788f06cb5240bf237
hugovieilledent <hugo.vieilledent@gmail.com>
2014-09-20 13:39:21 +0200

    wth

---
+++ app/controllers/user.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+	isEditing: false,
+
+	canEdit: function() {
+		return this.get('auth.user.playlist') === this.get('model');
+	}.property('model', 'auth'),
+
+	actions: {
+		edit: function() {
+			this.set('isEditing', true);
+		},
+		cancel: function() {
+			this.set('isEditing', false);
+		},
+		save: function() {
+			this.get('model').save().then(function(){
+				// after saving the playlist
+				this.send('cancel');
+			});
+		},
+		addToFavorite: function() {
+		}
+	}
+});
--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 	name: DS.attr('string'),
 	email: DS.attr('string'),
+	currentPlaylist: DS.attr('string'),
 	created: DS.attr('number'),
 	playlists: DS.hasMany('playlist', {async: true }),
 	favoritePlaylists: DS.hasMany('playlist', { inverse: null })
