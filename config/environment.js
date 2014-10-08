b139823e2a1d93457a6ea809415df1fe0c0455ef
Oskar Rough <oskarrough@gmail.com>
2014-10-08 09:48:16 +0200

    Remove content security stuff (for now)

--- config/environment.js
+++ config/environment.js
@@ -17,15 +17,6 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    },
-
-    contentSecurityPolicy: {
-      'default-src': "'none'",
-      'script-src': "'self'",
-      'font-src': "'self' fonts.googleapis.com; http://fonts.gstatic.com",
-      'connect-src': "'self'",
-      'img-src': "'self'",
-      'style-src': "'self'"
     }
   };
 
