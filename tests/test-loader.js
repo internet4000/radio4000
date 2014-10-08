64e42f27a4cd2a46afdf25faa971fc8a05dfb3cb
Oskar Rough <oskarrough@gmail.com>
2014-10-08 15:21:15 +0200

    Remove more unused files

--- tests/test-loader.js
+++
@@ -1,9 +0,0 @@
-/* globals requirejs,require */
-import Ember from 'ember';
-
-// TODO: load based on params
-Ember.keys(requirejs.entries).forEach(function(entry) {
-  if ((/\-test/).test(entry)) {
-    require(entry, null, null, true);
-  }
-});
