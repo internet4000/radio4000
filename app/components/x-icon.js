4bc4dc2934709939178f86c9d5366eb0cf303c24
Oskar Rough <oskarrough@gmail.com>
2014-10-17 01:17:13 +0200

    Icons

---
+++ app/components/x-icon.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+	tagName: 'svg',
+
+	// this creates a class name like "icon-iconName" (iconName comes from the variable "i")
+	classNameBindings: ['className'],
+	className: function() {
+		return 'icon-' + this.get('i');
+	}.property('i')
+});
