3c5fefdb331ef6d39526f9bc4dae9c0ed646f842
Oskar Rough <oskarrough@gmail.com>
2014-10-17 12:38:17 +0200

    Fix autofocus on add track url

--- app/components/input-url.js
+++ app/components/input-url.js
@@ -1,14 +1,16 @@
 import Ember from 'ember';
 
-// export default Ember.TextField.extend({
 export default Ember.Component.extend({
 	didInsertElement: function() {
 		this._super();
 		var self = this;
+		var input = this.$('input');
+
+		// autofocus on the url field
+		input.focus();
 
 		// use jQuery's 'paste' event
-		this.$('input').on('paste', function() {
-			var input = Ember.$(this);
+		input.on('paste', function() {
 			// dirty hack, otherwise the pasted value isn't available yet
 			setTimeout(function() {
 				// send the action to whereever the component is placed
