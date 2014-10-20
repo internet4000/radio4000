77edab42c681229098ba631aecb079af741cf70f
Oskar Rough <oskarrough@gmail.com>
2014-10-20 20:57:23 +0200

    Dont't load user/playlist on application route

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,12 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model: function() {
-		// if we don't load all playlists here, some things aren't properly loaded:
-		// - user favorites
-		// - user playlists
-		return this.store.find('user'), this.store.find('playlist');
-	},
 	actions: {
 		login: function(provider) {
 			this.get('session').login(provider);
