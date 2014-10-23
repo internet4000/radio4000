d1725fb09339364dc6ffef711148f2e7fd7b3244
hugovieilledent <hugo.vieilledent@gmail.com>
2014-10-23 16:32:11 +0200

    wth

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model: function() {
+		// if we don't load all playlists here, some things aren't properly loaded:
+		// - user favorites
+		// - user playlists
+	},
 	actions: {
 		login: function(provider) {
 			this.get('session').login(provider);
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,3 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend();
+export default Ember.Route.extend({
+	model: function(){
+		return this.store.find('user'), this.store.find('playlist');
+	}
