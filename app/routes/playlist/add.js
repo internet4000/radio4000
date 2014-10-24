a7d0a6dc944a1cae861b6d4548f57d00c02167d5
Oskar Rough <oskarrough@gmail.com>
2014-10-24 17:41:19 +0200

    Formatting adding

--- app/routes/playlist/add.js
+++ app/routes/playlist/add.js
@@ -1,13 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
 	// Abort if user isn't allowed to edit
 	beforeModel: function() {
 		var canEdit = this.controllerFor('playlist').get('canEdit');
 		if (!canEdit) { this.transitionTo('playlist', this.modelFor('playlist')); }
 	},
-
+	// Create a new model we can use for the track
 	model: function() {
 		return Ember.Object.create();
 	},
@@ -15,16 +14,12 @@ export default Ember.Route.extend({
 		controller.set('model', model);
 		controller.set('isExpanded', true);
 	},
-
-	deactivate: function() {
-		this.controller.set('isExpanded', false);
-	},
-
-	// render into the playlist template
 	renderTemplate: function() {
 		this.render({ outlet: 'playlist-body'});
 	},
-
+	deactivate: function() {
+		this.controller.set('isExpanded', false);
+	},
 	actions: {
 		//  this action is triggered from the add.js controller/template
 		saveTrack: function(track) {
