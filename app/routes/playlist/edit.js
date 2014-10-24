6cd2a5ed2ed6754585ecbe522dc0e1d412507f97
Oskar Rough <oskarrough@gmail.com>
2014-10-25 01:55:02 +0200

    Fix edit/add open and close

--- app/routes/playlist/edit.js
+++ app/routes/playlist/edit.js
@@ -8,29 +8,30 @@ export default Ember.Route.extend({
 		if (!canEdit) { this.transitionTo('playlist', this.modelFor('playlist')); }
 	},
 
-	model: function() {
-		return this.modelFor('playlist');
-	},
-
-	// not sure why this is not the model hook but that's what ember docs say
-	afterModel: function() {
-		this.set('model', this.modelFor('playlist'));
-	},
-
 	// render into the playlist template
 	renderTemplate: function() {
 		this.render({ outlet: 'playlist-header'});
 	},
 
+	// indicate we're editing (used for changing buttons etc.)
+	setupController: function(controller, model) {
+		this._super(controller, model);
+		this.controllerFor('playlist').set('isEditing', true);
+	},
+
+	// clear any unsaved changes
+	deactivate: function() {
+		this.controllerFor('playlist').get('model').rollback();
+		this.controllerFor('playlist').set('isEditing', false);
+	},
+
 	actions: {
 		//  this action is triggered from the add.js controller/template
 		saveTrack: function(track) {
-			var playlist = this.controllerFor('playlist').get('model');
-			track.save().then(function() {
-			});
+			track.save();
 		},
 		tryDelete: function() {
-			var confirmed = confirm('Are you sure? Your playlist will be gone forever. But you can always create a new one.');
+			var confirmed = confirm('Are you sure? Your channel will be gone forever.');
 			if (confirmed) {
 				this.controllerFor('playlist').send('deletePlaylist');
 			}
