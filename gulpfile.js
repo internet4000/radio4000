ceb839f72231350a4127b021aab55ad13540db3d
Oskar Rough <oskarrough@gmail.com>
2014-10-25 23:33:39 +0200

    Add a deploy script with Gulp

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var rsync = require('rsyncwrapper').rsync;
+
+/**
+ * Upload dist to dev
+ */
+gulp.task('deploy', function() {
+	rsync({
+		src: 'dist/',
+		dest: 'oskarrough@web461.webfaction.com:/home/oskarrough/webapps/radio_dev',
+		ssh: true,
+		recursive: true,
+		// deleteAll: true // Careful, this could cause data loss
+	}, function(error,stdout,stderr,cmd) {
+		if ( error ) {
+			// failed
+			console.log(error.message);
+		} else {
+			// success
+		}
+	});
+});
+
+/**
+ * Upload dist to live
+ */
+gulp.task('deploy-live', function() {
+	rsync({
+		src: 'dist/',
+		dest: 'oskarrough@web461.webfaction.com:/home/oskarrough/webapps/radio',
+		ssh: true,
+		recursive: true,
+		// deleteAll: true // Careful, this could cause data loss
+	}, function(error,stdout,stderr,cmd) {
+		if ( error ) {
+			// failed
+			console.log(error.message);
+		} else {
+			// success
+		}
+	});
+});
