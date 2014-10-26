0d82695f83abe061b0a5601ed6cd7931c5b47ed3
Oskar Rough <oskarrough@gmail.com>
2014-10-26 11:32:09 +0100

    Refactor with a focus-input component

--- app/components/input-url.js
+++ app/components/input-url.js
@@ -1,21 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	didInsertElement: function() {
-		this._super();
+
+	becomeFocused: function() {
+		this.$('input').focus();
+	}.on('didInsertElement'),
+
+	capturePaste: function() {
 		var self = this;
 		var input = this.$('input');
 
-		// autofocus on the url field
-		input.focus();
-
-		// use jQuery's 'paste' event
 		input.on('paste', function() {
-			// dirty hack, otherwise the pasted value isn't available yet
+
+			// using setTimeout hack,
+			// otherwise the pasted value isn't available yet
 			setTimeout(function() {
+
 				// send the action to whereever the component is placed
 				self.sendAction('pasted', input.val());
 			}, 100);
 		});
-	}
+	}.on('didInsertElement')
 });
--- app/views/channel/edit.js
+++ app/views/channel/edit.js
@@ -1,9 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-	didInsertElement: function() {
-		this.$('input:first').focus();
-	},
 	keyDown: function(event) {
 		// close 'add track' on esc key
 		if (event.keyCode === 27) {
