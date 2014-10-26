9a1bb025b7db8231576c563ec3d2104b3d2a0d7f
Oskar Rough <oskarrough@gmail.com>
2014-10-26 16:49:38 +0100

    Newest channels on top

--- app/controllers/channels/index.js
+++ app/controllers/channels/index.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
 	// Sort by newest on top
+	// sortProperties: ['title'],
 	sortProperties: ['created'],
 	sortAscending: false,
 
