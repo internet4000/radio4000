ed79af24d34522e38e6a49217eb20b1811c85c61
Oskar Rough <oskarrough@gmail.com>
2014-10-26 16:23:03 +0100

    Add search to /discover lol

--- app/controllers/channels/index.js
+++ app/controllers/channels/index.js
@@ -3,5 +3,19 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
 	// Sort by newest on top
 	sortProperties: ['created'],
-	sortAscending: true
+	sortAscending: false,
+
+	// returns the filtered channels if we are searching,
+	// otherwise the default array
+	channels: function() {
+		return this.get('search') ? this.get('filteredChannels') : this;
+	}.property('search', 'filteredChannels'),
+
+	// filters the array with our search value
+	filteredChannels: function() {
+		var search = this.get('search').toLowerCase();
+		return this.filter(function(channel) {
+			return channel.get('title').toLowerCase().indexOf(search) !== -1;
+		});
+	}.property('search', 'this.@each.title')
 });
