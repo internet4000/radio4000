8fe6d6a3eea6015fc71dc4b43b5b7d42d489b352
Oskar Rough <oskarrough@gmail.com>
2014-10-27 17:57:46 +0100

    Clean after Hugo

--- app/views/forum.js
+++ app/views/forum.js
@@ -2,10 +2,7 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
 	setForumIframe: function() {
-		console.log('hugo is nice');
 		this.$('#Forum')[0].src =
-			'https://groups.google.com/forum/embed/?place=forum/radio4000'
-			+ '&showsearch=true&showpopout=true&showtabs=false'
-			+ '&parenturl=' + encodeURIComponent(window.location.href);
+			'https://groups.google.com/forum/embed/?place=forum/radio4000' + '&showsearch=true&showpopout=true&showtabs=false' + '&parenturl=' + encodeURIComponent(window.location.href);
 	}.on('didInsertElement')
 });
