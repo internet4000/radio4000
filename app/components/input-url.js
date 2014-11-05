0bfa8e1f72d27df47c1ffd447b1294d7d216aa1f
Oskar Rough <oskarrough@gmail.com>
2014-11-05 14:57:04 +0100

    Refactor input URL

--- app/components/input-url.js
+++ app/components/input-url.js
@@ -1,23 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
 	becomeFocused: function() {
 		this.$('input').focus();
 	}.on('didInsertElement'),
 
+	// use jQuery's 'paste' event
 	capturePaste: function() {
 		var _this = this;
-		var input = this.$('input');
+		var $input = this.$('$input');
 
-		input.on('paste', function() {
+		$input.on('paste', function() {
 
 			// using setTimeout hack,
 			// otherwise the pasted value isn't available yet
 			setTimeout(function() {
 
 				// send the action to whereever the component is placed
-				_this.sendAction('pasted', input.val());
+				_this.sendAction('pasted', $input.val());
 			}, 100);
 		});
 	}.on('didInsertElement')
