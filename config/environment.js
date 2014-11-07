1285ac5e4b47da7790114bda54cffbfb82a5d8ca
Oskar Rough <oskarrough@gmail.com>
2014-11-07 17:07:02 +0100

    Change databases "radio4000-dev"

--- config/environment.js
+++ config/environment.js
@@ -2,8 +2,8 @@
 
 module.exports = function(environment) {
   var ENV = {
-    // firebaseURL: 'https://jsbin-test.firebaseio.com/', // LIVE DATABASE
-    firebaseURL: 'https://radio4000.firebaseio.com/', // DEV DATABASE
+    firebaseURL: 'https://radio4000-dev.firebaseio.com/', // DEV DATABASE
+    // firebaseURL: 'https://radio4000.firebaseio.com/', // LIVE DATABASE
     modulePrefix: 'radio4000',
     environment: environment,
     baseURL: '/',
