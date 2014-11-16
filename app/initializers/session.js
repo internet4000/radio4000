13d756b968a2f1f1a7b5b72d4869cf128fcd1bc9
Oskar Rough <oskarrough@gmail.com>
2014-11-16 02:25:17 +0100

    Refactor with tons of comments to session.js

--- app/initializers/session.js
+++ app/initializers/session.js
@@ -1,42 +1,70 @@
+/*
+ * Working authentication with
+ * Firebase 2.0.x + Ember.js 1.8.1 + Ember Data Canary + EmberFire 1.3.0 + Ember CLI
+ *
+ * Note: this assumes you've set up login on your Firebase,
+ * only handles Google and Facebook for now,
+ *
+ * In your templates: <button {{action 'login' 'google'}}>Log in with Google</button>
+ */
+
 /*global md5*/
 import Ember from 'ember';
-import ENV from '../config/environment';
-// https://gist.github.com/jkarsrud/941d8eb7a58e92953a55
 
+// Since I've defined my url in environment.js I can do this
+import ENV from '../config/environment';
 var ref = new window.Firebase(ENV.firebaseURL);
 
 export default {
 	name: 'session',
+
+	// Run the initializer after the store is ready
 	after: 'store',
+
 	initialize: function(container, app) {
+
+		// session object is nested here as we need access to the container to get the store
 		var session = Ember.Object.extend({
+
+			// initial state
 			authed: false,
 
-			init: function() {
-				// get access to the ember data store
-				this.store = container.lookup('store:main');
+			// get access to the ember data store
+			this.store = container.lookup('store:main');
 
+			init: function() {
+
+				// on init try to login
 				ref.onAuth(function(authData) {
+
+					// Not authenticated
 					if (!authData) {
-						// Ember.debug('Not logged in');
 						this.set('authed', false);
 						this.set('authData', null);
 						this.set('user', null);
 						return false;
 					}
 
-					// Ember.debug('Logged in');
+					// Authenticated
 					this.set('authed', true);
 					this.set('authData', authData);
 					// var hashedId = md5(authData.uid);
 					this.afterAuthentication(authData.uid);
 				}.bind(this));
 			},
+
+			// Call this from your Ember templates
 			login: function(provider) {
-				// Ember.debug('trying to login');
-				this.loginWithPopup(provider);
+				this._loginWithPopup(provider);
 			},
-			loginWithPopup: function(provider) {
+
+			// Call this from your Ember templates
+			logout: function() {
+				ref.unauth();
+			},
+
+			// Default login method
+			_loginWithPopup: function(provider) {
 				var _this = this;
 				// Ember.debug('logging in with popup');
 				ref.authWithOAuthPopup(provider, function(error, authData) {
@@ -44,35 +72,33 @@ export default {
 						if (error.code === "TRANSPORT_UNAVAILABLE") {
 							// fall-back to browser redirects, and pick up the session
 							// automatically when we come back to the origin page
-							_this.loginWithRedirect(provider);
+							_this._loginWithRedirect(provider);
 						}
 					} else if (authData) {
-						// Ember.debug('Logged in with popup');
-						// console.log(authData);
+						// we're good!
+						// this will automatically call the on ref.onAuth method inside init()
 					}
 				});
 			},
-			// firebase login with redirect (needed for chrome on iOS)
-			loginWithRedirect: function(provider) {
-				var _this = this;
-				// Ember.debug('logging in with redirect');
+
+			// Alternative login with redirect (needed for Chrome on iOS)
+			_loginWithRedirect: function(provider) {
 				ref.authWithOAuthRedirect(provider, function(error, authData) {
 					if (error) {
-						// console.log('errrrror');
+
 					} else if (authData) {
-						// Ember.debug('Logged in with redirect');
+						// we're good!
+						// this will automatically call the on ref.onAuth method inside init()
 					}
 				});
 			},
-			logout: function() {
-				ref.unauth();
-			},
 
+			// Runs after authentication
+			// It either sets a new or already exisiting user
 			afterAuthentication: function(userId) {
 				var _this = this;
-				// Ember.debug('Checking if user exists');
 
-				// See if the user exists using native firebase because of emberfire problem with "id already in use"
+				// See if the user exists using native Firebase because of EmberFire problem with "id already in use"
 				ref.child('users').child(userId).once('value', function(snapshot) {
 					var exists = (snapshot.val() !== null);
 					userExistsCallback(userId, exists);
@@ -80,7 +106,6 @@ export default {
 
 				// Do the right thing depending on whether the user exists
 				function userExistsCallback(userId, exists) {
-					// Ember.debug('user exists: ' + exists);
 					if (exists) {
 						_this.existingUser(userId);
 					} else {
@@ -89,17 +114,17 @@ export default {
 				}
 			},
 
+			// Existing user
 			existingUser: function(userId) {
 				this.store.find('user', userId).then(function(user) {
-					// Ember.debug('Found an existing user, setting it…');
-					this.afterUser(user);
+					_this.set('user', user);
 				}.bind(this));
 			},
 
+			// Create a new user
 			createUser: function(userId) {
 				var _this = this;
 
-				// create a user with the authdata firebase provides
 				this.get('store').createRecord('user', {
 					id: userId,
 					provider: this.get('authData.provider'),
@@ -107,22 +132,20 @@ export default {
 					email: this.get('authData.facebook.email') || this.get('authData.google.email'),
 					created: new Date().getTime()
 				}).save().then(function(user){
-					// Ember.debug('created a new user');
-					_this.afterUser(user);
+
+					// Proceed with the newly create user
+					_this.set('user', user);
 				});
 			},
 
 			// This is the last step in a successful authentication
+			// Set the user (either new or existing)
 			afterUser: function(user) {
 				this.set('user', user);
-				// also set the resolved channels
-				user.get('channels').then(function(channels) {
-					this.set('userChannel', channels.get('firstObject'));
-				}.bind(this));
 			}
 		});
 
-		// Register and inject the initializer into all controllers and routes
+		// Register and inject the 'session' initializer into all controllers and routes
 		app.register('session:main', session);
 		app.inject('route', 'session', 'session:main');
 		app.inject('controller', 'session', 'session:main');
