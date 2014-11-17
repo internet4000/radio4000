a62f666c6d633e450000c88afa06fe05a04abfb9
hugovieilledent <hugo.vieilledent@gmail.com>
2014-11-17 10:31:40 +0100

    demo index controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,10 @@
 import Ember from 'ember';
 
-export default Ember.ObjectController.extend();
+export default Ember.ObjectController.extend({
+
+	// demoChannel: function() {
+	// 	var channel;
+	// 	return this.store.find('channel', '-JXHtCxC9Ew-Ilck6iZ8');
+	// }
+
+});
