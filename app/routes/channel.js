f04c13516170d52ef75200d199ff319439477f16
Oskar Rough <oskarrough@gmail.com>
2014-11-17 10:48:16 +0100

    Add some comments to find a channel

--- app/routes/channel.js
+++ app/routes/channel.js
@@ -2,9 +2,29 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function(params) {
+
+		// TODO: this is how it should be, but no findQuery support in Emberfire
+		// return this.store.find('channel', { slug: params.channel_slug });
+
+		// alternative solution but loads all channels………
 		return this.store.find('channel').then(function(channels) {
 			return channels.findBy('slug', params.channel_slug);
 		});
+
+		// return new Ember.Promise(function(resolve, reject) {
+		// 	ref.orderByChild('slug').equalTo(params.channel_slug).on('child_added', function(snapshot) {
+		// 		// var results = [];
+		// 		// snapshot.forEach(function(childSnapshot) {
+		// 			// var payload = adapter._assignIdToPayload(childSnapshot);
+		// 			// adapter._updateRecordCacheForType(type, payload);
+		// 			// results.push(childSnapshot.val());
+		// 		// });
+		// 		console.log(snapshot.val());
+		// 		resolve(snapshot.val());
+		// 	}, function(error) {
+		// 		reject(error);
+		// 	});
+		// });
 	},
 
 	// because we use slugs instead of ids in the url
