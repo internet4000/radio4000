a4369eaadd7d2bd9db0e20234b047796deab6a55
Oskar Rough <oskarrough@gmail.com>
2014-11-22 22:34:09 +0100

    Refactor: use an initialiser to load the YouTube API

---
+++ app/initializers/youtube.js
@@ -0,0 +1,19 @@
+export function initialize(/* container, application */) {
+	// application.inject('route', 'foo', 'service:foo');
+
+	// Loads the iframe player API asynchronously from YouTube
+	var tag = document.createElement('script');
+	tag.src = "https://www.youtube.com/iframe_api";
+	var firstTag = document.getElementsByTagName('script')[0];
+	firstTag.parentNode.insertBefore(tag, firstTag);
+
+	// This get's called automatically by the API
+	window.onYouTubePlayerAPIReady = function() {
+		console.log('yt api ready');
+	};
+}
+
+export default {
+	name: 'youtube',
+	initialize: initialize,
+};
---
+++ tests/unit/initializers/youtube-test.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+import { initialize } from 'radio4000/initializers/youtube';
+
+var container, application;
+
+module('YoutubeInitializer', {
+  setup: function() {
+    Ember.run(function() {
+      container = new Ember.Container();
+      application = Ember.Application.create();
+      application.deferReadiness();
+    });
+  }
+});
+
+// Replace this with your real tests.
+test('it works', function() {
+  initialize(container, application);
+
+  // you would normally confirm the results of the initializer here
+  ok(true);
+});
+
