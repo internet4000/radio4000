7be362a2eed512a13e5637ee2bf5d0dc043253c7
Oskar Rough <oskarrough@gmail.com>
2014-12-01 23:12:14 +0100

    Refactor ultra-long forum

--- app/views/forum.js
+++ app/views/forum.js
@@ -1,8 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
+
+	// this sets the url on the iframe in the template to load the forum
 	setForumIframe: function() {
-		this.$('#Forum')[0].src =
-			'https://groups.google.com/forum/embed/?place=forum/radio4000' + '&showsearch=true&showpopout=true&showtabs=false' + '&parenturl=' + encodeURIComponent(window.location.href);
+		var url =	'https://groups.google.com/forum/embed/?place=forum/radio4000' +
+					 	'&showsearch=true&showpopout=true&showtabs=false' +
+					 	'&parenturl=' +
+					 	encodeURIComponent(window.location.href);
+
+		this.$('#Forum')[0].src = url;
 	}.on('didInsertElement')
 });
