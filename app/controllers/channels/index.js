7ad15adecf9f387094023cc599ee25789004db10
Oskar Rough <oskarrough@gmail.com>
2014-12-21 03:04:26 +0100

    Revert random - performance

--- app/controllers/channels/index.js
+++ app/controllers/channels/index.js
@@ -4,9 +4,21 @@ export default Ember.ArrayController.extend({
 	sortProperties: ['created', 'title'],
 	sortAscending: false,
 	// Sort by random initially
-	sortFunction: function(x, y) {
-		return 0.5 - Math.random();
-	},
+	// sortFunction: function(x, y) {
+	// 	return 0.5 - Math.random();
+	// 	// var currentIndex, randomIndex, temporaryValue;
+
+	// 	// var array = this;
+	// 	// currentIndex = array.get('length');
+
+	// 	// while (currentIndex !== 0) {
+	// 	// 	randomIndex = Math.floor(Math.random() * currentIndex);
+	// 	// 	currentIndex--;
+	// 	// 	temporaryValue = array.objectAt(currentIndex);
+	// 	// 	array.replace(currentIndex, 1, [array.objectAt(randomIndex)]);
+	// 	// 	array.replace(randomIndex, 1, [temporaryValue]);
+	// 	// }
+	// },
 
 	// returns the filtered channels if we are searching,
 	// otherwise the default array
