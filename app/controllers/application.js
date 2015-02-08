9d96ce42de5e70d0430069048bbed7ab13f1f873
hugovieilledent <hugo.vieilledent@gmail.com>
2015-02-08 18:29:03 +0100

    toogle panel main anv

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,5 +3,18 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
 	// get gui state so we can update markup in this template as well
 	needs: ['playback'],
-	isMaximized: Ember.computed.alias('controllers.playback.isMaximized')
+	isMaximized: Ember.computed.alias('controllers.playback.isMaximized'),
+	isPanelOpen: false,
+
+	actions: {
+
+		// open panel
+		openPanel: function() {
+			this.toggleProperty('isPanelOpen');
+		},
+		closePanel: function() {
+			this.toggleProperty('isPanelOpen');
+		}
+	}
+
 });
