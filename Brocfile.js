eb4208636a5f831fcba407d89923f079a5704207
Oskar Rough <oskarrough@gmail.com>
2015-02-11 00:36:58 +0100

    Update to ember 1.10.0

--- Brocfile.js
+++ Brocfile.js
@@ -3,6 +3,9 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp({
+	vendorFiles: {
+		'handlebars.js': null
+	},
 	minifyCSS: {
 		enabled: true,
 		options: {}
