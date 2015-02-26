9518e171e032134bb1015c02875ad93c04e9ba79
Oskar Rough <oskarrough@gmail.com>
2015-02-27 00:11:01 +0100

    Add commented covers

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,3 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
-	// demoChannel: function() {
-	// 	var channel;
-	// 	return this.store.find('channel', '-JXHtCxC9Ew-Ilck6iZ8');
-	// }
-});
+export default Ember.Controller.extend({});
