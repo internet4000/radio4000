a938c476b30c443298862a707fd853fdd57527ca
Oskar Rough <oskarrough@gmail.com>
2015-02-27 10:48:18 +0100

    Refactor popular (to random until we have sometime real)

--- app/controllers/channels/popular.js
+++ app/controllers/channels/popular.js
@@ -1,10 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+	max: 5,
 
-	// this is just the first five
-	// @todo figure out popular radios
+	// what we use in our template
 	filteredContent: function() {
-		return this.get('content').slice(0,5);
+		return this.get('random');
+	}.property('random.[]'),
+
+	// five random radios
+	random: function() {
+		var content = this.get('content');
+		var randomContent = [];
+
+		for (var i = this.get('max'); i >= 1; i--) {
+			var random = Math.floor(Math.random() * content.get('length'));
+			randomContent.pushObject(content.objectAt(random));
+		}
+
+		return  randomContent;
 	}.property('content.[]'),
+
+	// five newest radios
+	newest: function() {
+		return this.get('content').slice(0,this.get('max'));
+	}.property('content.[]')
 });
