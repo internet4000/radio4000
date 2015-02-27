d9f645d853c6bd3586dfce6afcad1bf2c35af886
Oskar Rough <oskarrough@gmail.com>
2015-02-27 10:28:54 +0100

    A/Z channels

---
+++ app/controllers/channels/az.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+
+export default Ember.ArrayController.extend({
+	sortProperties: ['created'],
+	sortAscending: false,
+
+	// if we're searching, return those filtered channels otherwise all
+	channels: function() {
+		return this.get('search') ? this.get('filteredChannels') : this;
+	}.property('search', 'filteredChannels'),
+
+	// filters the array with our search value
+	filteredChannels: function() {
+		var filter = this.get('search');
+    	var rx = new RegExp(filter, 'gi');
+
+		if (!filter) { return; }
+
+		return this.filter(function(channel) {
+			return rx.test(channel.get('title')) || rx.test(channel.get('body'));
+		});
+	}.property('search'),
+
+	actions: {
+		sortBy: function(property) {
+			this.set('sortProperties', [property]);
+			this.set('sortAscending', !this.get('sortAscending'));
+		}
+	}
+});
