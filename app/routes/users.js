023a22ae91f3b786bad4f3b3b89b335ac66748c9
Oskar Rough <oskarrough@gmail.com>
2015-02-28 14:13:24 +0100

    Consistent white colors

--- app/routes/users.js
+++ app/routes/users.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model: function() {
 		return this.store.find('user');
+	},
+	afterModel: function(model) {
+		model.forEach(function(user) {
+			console.log(this.get('name'));
+		});
 	}
 });
