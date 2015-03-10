fb9f21c5c198152543ace33f2bdfbdc9bf5d6b1d
Oskar Rough <oskarrough@gmail.com>
2015-03-10 22:33:50 +0100

    Update tests

--- tests/unit/mixins/reset-scroll-test.js
+++ tests/unit/mixins/reset-scroll-test.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
+import {module, test} from 'qunit';
 import ResetScrollMixin from 'radio4000/mixins/reset-scroll';
 
 module('ResetScrollMixin');
 
 // Replace this with your real tests.
-test('it works', function() {
+test('it works', function(assert) {
   var ResetScrollObject = Ember.Object.extend(ResetScrollMixin);
   var subject = ResetScrollObject.create();
-  ok(subject);
+  assert.ok(subject);
 });
--- tests/unit/utils/clean-test.js
+++ tests/unit/utils/clean-test.js
@@ -1,12 +1,14 @@
 import clean from 'radio4000/utils/clean';
 
+import {module, test} from 'qunit';
+
 module('clean');
 
-test('it works', function() {
-	var string = '&$#! S??omething// With UPPERCASE and SPACES and CH#$@#%(*!';
-	var result = clean(string);
+test('it works', function(assert) {
+    var string = '&$#! S??omething// With UPPERCASE and SPACES and CH#$@#%(*!';
+    var result = clean(string);
 
-	ok(result.indexOf(' ') < 1, 'String can not contain spaces.');
-	ok(result.indexOf('/') < 1, 'String can not contain a slash.');
-	ok(result.indexOf('?') < 1, 'String can not contain a question mark.');
+    assert.ok(result.indexOf(' ') < 1, 'String can not contain spaces.');
+    assert.ok(result.indexOf('/') < 1, 'String can not contain a slash.');
+    assert.ok(result.indexOf('?') < 1, 'String can not contain a question mark.');
 });
