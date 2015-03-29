72108a1f78e1845a593ea0a15cbfe142694822ad
Oskar Rough <oskarrough@gmail.com>
2015-03-29 21:39:11 +0200

    Refactor this and that

--- app/views/channel/edit.js
+++ app/views/channel/edit.js
@@ -2,8 +2,7 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
 	keyDown: function(event) {
-		// close 'edit channel' on esc key
-		if (event.keyCode === 27) {
+		if (event.keyCode === 27) { // ESC
 			this.get('controller').send('cancelEdit');
 		}
 	}
--- app/views/tracks/add.js
+++ app/views/tracks/add.js
@@ -1,13 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-
-	// setFocus: function() {
-	// 	return this.$('input:first').focus();
-	// }.on('didInsertElement'),
-
 	keyDown: function(event) {
-		if (event.keyCode === 27) { // "ESC"
+		if (event.keyCode === 27) { // ESC
 			this.get('controller').send('cancel');
 		}
 	}
