d7696089183e203e8fb92f95773f681ba6cc4705
Oskar Rough <oskarrough@gmail.com>
2015-03-30 11:24:41 +0200

    Fix paste

--- app/components/input-paste/component.js
+++ app/components/input-paste/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import FocusInputComponent from '../focus-input/component';
 
 export default FocusInputComponent.extend({
-	capturePaste: Ember.on('didInsertElement', () => {
+	capturePaste: Ember.on('didInsertElement', function() {
 		var self = this;
 		var $input = this.$();
 
