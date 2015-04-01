3c4ec7dad5903a2c67fa04ccc2923c81162b56a1
hugovieilledent <hugo.vieilledent@gmail.com>
2015-04-01 11:48:33 +0200

    delete channel not rdy

--- app/controllers/channel/edit.js
+++ app/controllers/channel/edit.js
@@ -185,13 +185,13 @@ export default Ember.Controller.extend({
 			user.get('channels').then(function(userChannels) {
 				console.log('// 1- remove privateChannel reference on user');
 				// 1- remove privateChannel reference on user
-				userChannels.removeObject(channel);
-				user.save().then(function() {
-					Ember.debug("privateChannel reference on user was removed");
-				});
+				// userChannels.removeObject(channel);
+				// user.save().then(function() {
+				// 	Ember.debug("privateChannel reference on user was removed");
+				// });
 			}).then(function() {
 				console.log("// 2a- remove privateChannel reference other privateChannel.favoriteChannels");
-				// 2a- remove privateChannel reference other privateChannel.favoriteChannels
+				// 2a- remove privateChannel reference on other privateChannel.favoriteChannels
 				// channel.get('channelPublic.followers').then(function(favorites) {
 				// 	// get all this channel's favorite channels and for each...
 				// 	favorites.forEach(function(fav) {
@@ -207,18 +207,22 @@ export default Ember.Controller.extend({
 			}).then(function() {
 				console.log("// 2b- remove reference privateChannel in all users.publicChannel.followers");
 				// 2b- remove reference privateChannel in all users.publicChannel.followers
-				// channel.get('favoriteChannels').then(function(favorites) {
-				// 	// get all this channel's favorite channels and for each...
-				// 	favorites.forEach(function(fav) {
-				// 		// remove reference of the privateChannel that is being deleted
-				// 		fav.get('channelPublic.followers').then(function(follower) {
-				// 			follower.removeObject(channel);
-				// 			fav.save().then(function() {
-				// 				Ember.debug("channel reference as itself a follower was removed");
-				// 			});
-				// 		});
-				// 	});
-				// });
+				channel.get('favoriteChannels').then(function(favorites) {
+					// get all this channel's favorite channels and for each...
+					favorites.forEach(function(fav) {
+						// navigate between models (promises)
+						fav.get('channelPublic').then(function(publicChannel){
+							// remove reference of the privateChannel that is being deleted
+							publicChannel.get('followers').then(function(followers) {
+								console.log(followers);
+								followers.removeObject(channel);
+								fav.save().then(function() {
+									Ember.debug("channel reference as itself a follower was removed");
+								});
+							});
+						})
+					});
+				});
 			}).then(function() {
 				console.log("// 3- delete publicChannel");
 				// 3- delete publicChannel
