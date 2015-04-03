3a1914f1d62e64bff16bf85fbba1515266ba102c
hugovieilledent <hugo.vieilledent@gmail.com>
2015-04-03 23:17:03 +0200

    toggle favorite works but icon does not change yet

--- app/components/btn-favorite/component.js
+++ app/components/btn-favorite/component.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	classNameBindings: ['value:is-favorite'],
-	classNames: ['Btn'],
-	tagName: 'button',
+	classNames: ['ToogleFavorite'],
+	tagName: 'span',
 
 	click() {
 		this.sendAction(); // triggers the action specified on the component markup
