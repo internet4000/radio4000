7eb55cc9514a556f7bc77a1ab88b3d039d5bfbe6
Oskar Rough <oskarrough@gmail.com>
2015-04-04 12:54:09 +0200

    Refactor routes

--- app/router.js
+++ app/router.js
@@ -8,43 +8,35 @@ var Router = Ember.Router.extend({
 export default Router.map(function() {
 
   // pages
+  this.route('about', function() {
+    this.route('contact');
+    this.route('log');
+    this.route('technology');
+  });
+  this.route('bookmarklet');
+  this.route('dashboard');
+  this.route('help', function() { /* help.index */ });
+  this.route('introduction');
   this.route('log');
   this.route('signin');
   this.route('stats');
   this.route('styleguide');
-  this.route('dashboard');
-  // a tour for newcomers
-  this.route('introduction');
 
-  // about route
-  this.route('about', function() {
-      this.route('contact');
-      this.route('log');
-      this.route('technology');
-    });
-
-  // help route
-  this.route('help', function() {
-    });
-
-
-  // all channels
+  // channels
   this.resource('channels', { path: '/'}, function() {
-      this.route('all');
-      this.route('new'); // new channel
-    });
+    this.route('all');
+    this.route('new');
+  });
 
   // channel
   this.resource('channel', { path: '/c/:channel_slug' }, function() {
-      this.route('index', { path: '/'}, function() {
-          this.resource('track', { path: ':track_id' });
-      });
-      this.route('favorite-channels');
-      this.route('followers');
-      this.route('add'); // add a track to channel
-      this.route('edit'); // edit channel
-      this.route('delete-channel'); // permanently remove channel and its referenes from DB
+    this.route('index', { path: '/'}, function() {
+        this.resource('track', { path: ':track_id' });
     });
-
-  this.route('bookmarklet');
+    this.route('favorite-channels');
+    this.route('followers');
+    this.route('add'); // add a track to channel
+    this.route('edit'); // edit channel
+    this.route('delete-channel'); // permanently remove channel and its referenes from DB
+  });
 });
