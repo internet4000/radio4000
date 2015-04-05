c0946bb8c71e034b5179ce7892331066113094d1
Oskar Rough <oskarrough@gmail.com>
2015-04-05 14:50:23 +0200

    Add and integrate autocomplete search when adding a track

--- app/controllers/channel/add.js
+++ app/controllers/channel/add.js
@@ -2,53 +2,43 @@ import Ember from 'ember';
 import youtube from 'radio4000/utils/youtube';
 
 export default Ember.Controller.extend({
-	formError: false,
 	queryParams: ['providerTrackUrl', 'providerTrackTitle'],
 	providerTrackUrl: null,
 	providerTrackTitle: null,
 
-
-
-	// javascript:location.href='http://localhost:4000/c/200ok/add?providerTrackUrl='+encodeURIComponent(location.href)+'&providerTrackTitle='+encodeURIComponent(document.title)
-	// bookmarklet: function() {
-	// // knowledge base
-	// // http://localhost:4000/c/200ok/add?bookmarklet=https://www.youtube.com/watch?v=doaQC-S8de8
-	// // javascript:location.href='http://www.reddit.com/submit?url='
-	// // +encodeURIComponent(location.href)
-	// // +'&title='
-	// // +encodeURIComponent(document.title)
-
-	// //http://guides.emberjs.com/v1.10.0/routing/query-params/
-
-	// 	var bookmarklet = this.get('bookmarklet');
-	// 	console.log('bookmarklet tests:');
-	// 	console.log(bookmarklet);
-
-	// 	// if (bookmarklet) {
-	// 	// 	console.log(bookmarklet);
-	// 	// } else {
-	// 	// 	console.log("no bookmarklet");
-	// 	// }
-	// }.property('url', 'model'),
-
 	// Check if the track is valid before saving
-	isValid() {
-		this.set('formError', false);
-
-		var isValid = true;
-		['url', 'title'].forEach(function(field) {
-			if (this.get(field) === '') {
-				isValid = false;
-				this.set('formError', 'Please enter a valid YouTube URL and a title.');
-			}
-		}, this);
-		return isValid;
-	},
+	isValid: Ember.computed('model.url', 'model.title', function() {
+		if (this.get('model.url') && this.get('model.title')) {
+			return true;
+		} else {
+			return false;
+		}
+	}),
 
 	actions: {
+		addFromSearch(item) {
+			var title = item.get('title');
+			var url = item.get('url');
+
+			// clean the url
+			url = url.replace('&feature=youtube_gdata_player', '');
+
+			// update current model with properties from the chosen search
+			this.set('model.title', title);
+			this.set('model.url', url);
+
+
+			setTimeout(() => {
+				this.set('justAdded', true);
+
+			}, 250);
+			setTimeout(() => {
+				this.set('justAdded', false);
+			}, 500);
+		},
 
 		addTrack() {
-			if (!this.isValid()) { return false; }
+			if (!this.get('isValid')) { return false; }
 
 			// leave it to the router to actually save the track
 			this.send('saveTrack');
@@ -60,12 +50,39 @@ export default Ember.Controller.extend({
 			var apikey = 'AIzaSyCk5FiiPiyHON7PMLfLulM9GFmSYt6W5v4';
 			var id = youtube(url);
 
-			if (!id) { Ember.debug('errrrror'); return; }
+			if (!id) {
+				console.log(id);
+				Ember.debug('errrrror');
+				return;
+			}
 
 			Ember.$.getJSON('https://www.googleapis.com/youtube/v3/videos?id='+id+'&key='+apikey+'&fields=items(id,snippet(title))&part=snippet').then(function(response) {
-				var ytTitle = response.items[0].snippet.title;
-				this.set('model.title', ytTitle);
+				var title = response.items[0].snippet.title;
+				this.set('model.title', title);
 			}.bind(this));
 		}
 	}
 });
+
+
+// javascript:location.href='http://localhost:4000/c/200ok/add?providerTrackUrl='+encodeURIComponent(location.href)+'&providerTrackTitle='+encodeURIComponent(document.title)
+// bookmarklet: function() {
+// // knowledge base
+// // http://localhost:4000/c/200ok/add?bookmarklet=https://www.youtube.com/watch?v=doaQC-S8de8
+// // javascript:location.href='http://www.reddit.com/submit?url='
+// // +encodeURIComponent(location.href)
+// // +'&title='
+// // +encodeURIComponent(document.title)
+
+// //http://guides.emberjs.com/v1.10.0/routing/query-params/
+
+// 	var bookmarklet = this.get('bookmarklet');
+// 	console.log('bookmarklet tests:');
+// 	console.log(bookmarklet);
+
+// 	// if (bookmarklet) {
+// 	// 	console.log(bookmarklet);
+// 	// } else {
+// 	// 	console.log("no bookmarklet");
+// 	// }
+// }.property('url', 'model'),
