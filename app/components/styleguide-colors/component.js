1db1ee56dc4b5894612160e74855198e27380583
Oskar Rough <oskarrough@gmail.com>
2015-04-06 11:18:35 +0200

    Update styleguide

---
+++ app/components/styleguide-colors/component.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+	colors: [
+		{ title: 'red', value: 'hsl(0, 75%, 50%)' },
+		{ title: 'yellow', value: 'hsl(59, 100%, 65%)' },
+		{ title: 'blue', value: 'hsl(255, 95%, 39%)' },
+		{ title: 'purple', value: 'hsl(260, 80%, 50%)' },
+		{ title: 'lighterblue', value: 'hsla(266, 36%, 84%, 0.8)' },
+		{ title: 'darkblue', value: 'hsl(230, 50%, 23%)' },
+		{ title: 'verydarkblue', value: 'hsl(255, 65%, 19%)' },
+		{ title: 'green', value: 'hsl(125, 82%, 35%)' },
+		{ title: 'sand', value: 'hsl(46, 5%, 93%)' },
+		{ title: 'bleach', value: 'hsl(72, 100%, 98%)' },
+		{ title: 'black', value: 'hsl(0, 0%, 10%)' },
+		{ title: 'dark', value: 'hsl(0, 0%, 30%)' },
+		{ title: 'gray', value: 'hsl(0, 0%, 50%)' },
+		{ title: 'mediumgray', value: 'hsl(0, 0%, 70%)' },
+		{ title: 'mediumlightgray', value: 'hsl(0, 0%, 85%)' },
+		{ title: 'lightgray', value: 'hsl(0, 0%, 91%)' },
+		{ title: 'slightgray', value: 'hsl(0, 0%, 95%)' },
+		{ title: 'superlightgray', value: 'hsl(0, 0%, 98%)' },
+		{ title: 'white', value: 'hsl(0, 0%, 100%)' }
+	]
+});
