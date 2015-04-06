f61eb51cbdaf585b7ffc6bc4b0ec7e50e2fc2120
Oskar Rough <oskarrough@gmail.com>
2015-04-06 16:55:54 +0200

    yea ok

--- app/initializers/cloudinary.js
+++ app/initializers/cloudinary.js
@@ -3,13 +3,13 @@ import ENV from '../config/environment';
 
 export function initialize(/* container, application */) {
 
-  // Set a global cloudinary config
-  Ember.$.cloudinary.config({
-     cloud_name: ENV.CLOUDINARY_NAME
-  });
+	// Set a global cloudinary config
+	Ember.$.cloudinary.config({
+		cloud_name: ENV.CLOUDINARY_NAME
+	});
 }
 
 export default {
-  name: 'cloudinary',
-  initialize: initialize
+	name: 'cloudinary',
+	initialize: initialize
 };
