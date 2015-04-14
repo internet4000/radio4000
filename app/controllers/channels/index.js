116a4f16c8a04daf60bb3a877ae53ab0cc638a58
Oskar Rough <oskarrough@gmail.com>
2015-04-14 21:46:07 +0200

    Performance: improve channels loading

--- app/controllers/channels/index.js
+++ app/controllers/channels/index.js
@@ -3,16 +3,16 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
 
 	// this value should match what we fetch in the route
-	maxPopular: 12,
+	// maxPopular: 12
 
 	// sort by followers
-	sortProperties: ['followers.length'],
-	sortAscending: false,
+	// sortProperties: ['followers.length'],
+	// sortAscending: false,
 
 	// get the sorted content and limit it by our max
-	popular: Ember.computed('arrangedContent', function() {
-		return this.get('arrangedContent').slice(0, this.get('maxPopular'));
-	})
+	// popular: Ember.computed('arrangedContent', function() {
+	// 	return this.get('arrangedContent').slice(0, this.get('maxPopular'));
+	// })
 
 	// // only show featured items
 	// featured: Ember.computed('model.@each.isFeatured', function() {
--- app/routes/channels.js
+++ app/routes/channels.js
@@ -2,6 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model() {
-		return this.store.find('channel', { limitToLast: 12 });
+		var items = [];
+
+		this.store.find('channel', {
+			orderBy: 'tracks',
+			limitToLast: 12,
+
+			// not sure what this does but it works
+			startAt: 200
+
+		}).then((channels) => {
+			items.addObjects(channels);
+		});
+
+		return items;
 	}
 });
