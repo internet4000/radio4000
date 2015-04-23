b535066de1de02ea363251f2333edf7dfa1ebcf5
Oskar Rough <oskarrough@gmail.com>
2015-04-23 22:48:42 +0200

    Fix track sorting

--- app/components/tracks-list/component.js
+++ app/components/tracks-list/component.js
@@ -1,9 +1,17 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend(Ember.SortableMixin, {
-	// Newest on top
-	sortProperties: ['created'],
-	sortAscending: false,
+export default Ember.Component.extend({
+
+	// not sure how to set up array controller
+	sorted: Ember.computed('', function() {
+		return Ember.ArrayController.create({
+			content : this.get('model'),
+
+			// Newest on top
+			sortProperties: ['created'],
+			sortAscending: false
+		});
+	}),
 
 	// Keep track of which track we're currently editing
 	currentTrackComponent: null
