4746f84273264a1e01773c9a25a5122629e1a311
Oskar Rough <oskarrough@gmail.com>
2015-04-23 23:58:57 +0200

    Add hashtags

--- app/components/tracks-list/component.js
+++ app/components/tracks-list/component.js
@@ -1,17 +1,59 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	filter: '',
+
+	// Returns either all tracks or the filtered tracks by hashtag
+	filtered: function() {
+		let filter = this.get('filter');
+		let model = this.get('model');
+
+		if (filter) {
+			return model.filter(function(track) {
+				let hashtags = track.get('hashtags');
+
+				if (!hashtags) { return false; }
+
+				return hashtags.contains(filter);
+			});
+		} else {
+			return model;
+		}
+	}.property('filter', 'model'),
 
 	// not sure how to set up SortableMixin
 	sorted: Ember.computed('', function() {
 		return Ember.ArrayController.create({
-			content : this.get('model'),
+			content: this.get('filtered'),
 
 			// Newest on top
 			sortProperties: ['created'],
 			sortAscending: false
 		});
 	}),
+
+	// Returns the unique tags from all models
+	tags: function() {
+		let model = this.get('model');
+		let tags = model.getEach('hashtags');
+
+		// Remove tracks without hashtags
+		tags = tags.compact();
+
+		// Flatten the array
+		tags = (tags.join(',')).split(','); // js version
+		// tags = _.flatten(tags); // lodash version
+
+		// Only keep uniques
+		tags = tags.uniq();
+
+		// Remove more empty ones (todo: shouldn't be necessary)
+		// tags = tags.reject(function(tag) {
+		// 	return tag === '';
+		// });
+
+		return tags;
+	}.property('model.@each.hashtags'),
 
 	// Keep track of which track we're currently editing
 	currentTrackComponent: null
--- app/models/track.js
+++ app/models/track.js
@@ -15,10 +15,36 @@ export default DS.Model.extend({
 	// TODO: this should definitely be saved in the db
 	// and not computed every time like it is now
 	updateProvider() {
-		var id = youtube(this.get('url'));
+		let id = youtube(this.get('url'));
 		console.log('updateYtid');
 
 		this.set('ytid', id);
 		this.save();
-	}
+	},
+
+	hashtags: Ember.computed('body', function() {
+		let body = this.get('body');
+
+		if (!body) { return; }
+
+		// find " #hashtags" (with space) https://regex101.com/r/pJ4wC5/4
+		let hashtags = body.match(/(^|\s)(#[a-z\d-]+)/ig);
+
+		if (!hashtags) { return; }
+
+		// // remove spaces on each item
+		// return hashtags.map(function(tag) {
+		// 	return tag.trim();
+		// });
+
+		return hashtags;
+	})
+
+	// bodyPlusMentions: function() {
+	// 	var body = this.get('body');
+
+	// 	// find " @channel" (with space)
+	// 	return body.replace(/(^|\s)(@[a-z\d-]+)/ig,'$1<a href="http://radio4000.com/c/$2" class="Mention">$2</a>');
+	// }.property('body')
+
 });
