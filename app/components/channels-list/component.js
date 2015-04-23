9e6125c318a53fb03163f94036e291de631ad9b2
Oskar Rough <oskarrough@gmail.com>
2015-04-23 23:00:54 +0200

    Make /all alphabetical

--- app/components/channels-list/component.js
+++ app/components/channels-list/component.js
@@ -1,3 +1,15 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({});
+export default Ember.Component.extend({
+
+	// not sure how to set up SortableMixin
+	sorted: Ember.computed('', function() {
+		return Ember.ArrayController.create({
+			content : this.get('model'),
+
+			// Alphabetical
+			sortProperties: ['title'],
+			sortAscending: true
+		});
+	})
+});
--- app/components/tracks-list/component.js
+++ app/components/tracks-list/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-	// not sure how to set up array controller
+	// not sure how to set up SortableMixin
 	sorted: Ember.computed('', function() {
 		return Ember.ArrayController.create({
 			content : this.get('model'),
