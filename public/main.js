b02022c16ca9eaab638a76c2dc3e03ec2e6e57e1
Oskar Rough <oskarrough@gmail.com>
2015-04-24 10:56:48 +0200

    Clean up for native apps

--- public/main.js
+++ public/main.js
@@ -10,27 +10,30 @@ var mainWindow = null;
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
-  if (process.platform != 'darwin')
-    app.quit();
+	if (process.platform != 'darwin') {
+		app.quit();
+	}
 });
 
 // This method will be called when atom-shell has done everything
 // initialization and ready for creating browser windows.
 app.on('ready', function() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 600, height: 800});
 
-  // and load the index.html of the app.
-  // mainWindow.loadUrl('file://' + __dirname + '/index.html');
+	// Create the browser window.
+	mainWindow = new BrowserWindow({ width: 600, height: 800 });
 
-  // or live version
-  mainWindow.loadUrl('http://radio4000.com');
+	// and load the index.html of the app.
+	// mainWindow.loadUrl('file://' + __dirname + '/index.html');
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+	// or live version
+	mainWindow.loadUrl('http://dev.radio4000.com');
+
+	// Emitted when the window is closed.
+	mainWindow.on('closed', function() {
+
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null;
+	});
 });
