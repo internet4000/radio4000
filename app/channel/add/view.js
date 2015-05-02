2caf077faa561e22f268466a26e16890f0ce7db1
Oskar Rough <oskarrough@gmail.com>
2015-05-02 17:10:31 +0200

    Add bookmarklet

--- app/channel/add/view.js
+++ app/channel/add/view.js
@@ -5,5 +5,14 @@ export default Ember.View.extend({
 		if (event.keyCode === 27) { // ESC
 			this.get('controller').send('cancelEdit');
 		}
-	}
+	},
+
+	// binding properties with :javascript in templates is forbidden because of XSS
+	// so we do this instead
+	fixBookmarklet: Ember.on('didInsertElement', function() {
+		const $link = this.$('.Bookmarklet');
+		let href = $link.attr('href').replace('TOBESLUGGED', $link.data('slug'));
+
+		$link.attr('href', href);
+	})
 });
