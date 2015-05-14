6aa5aeeabe7e5b5f6dcd807f4c31346ce419418b
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2015-05-14 18:19:16 +0200

    signin

--- app/application/route.js
+++ app/application/route.js
@@ -7,38 +7,7 @@ export default Ember.Route.extend({
 		});
 	},
 
-	// renderTemplate() {
-	// 	// because we overwrite the renderTemplate method
-	// 	// we need to run super
-	// 	this._super();
-
-	// 	// and update nav bar
-	// 	this.render('contextual-navigation/cn-channels', {
-	// 		into: 'application',
-	// 		outlet: 'contextual-navigation'
-	// 	});
-	// },
-
 	actions: {
-		// Signs a user in and redirect to either her channel
-		// or the form to create a channel
-		signIn(authWith) {
-			this.get('session').open('firebase', { authWith: authWith }).then(() => {
-				const userChannels = this.get('session.currentUser.channels');
-
-				userChannels.then((channels) => {
-					let channel = channels.get('firstObject');
-
-					if (channel) {
-						Ember.debug('user signed in with channel, redirecting to it');
-						this.transitionTo('channel', channel);
-					} else {
-						Ember.debug('user signed in without channel, redirecting to /new');
-						this.transitionTo('channels.new');
-					}
-				});
-			});
-		},
 		logout() {
 			this.get('session').close();
 		},
--- app/routes/signin.js
+++ app/routes/signin.js
@@ -2,11 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	beforeModel() {
-
-		// if already authenticated, redirect to intro
 		if (this.get('session.isAuthenticated')) {
-			Ember.debug('not authed --> intro');
-			this.transitionTo('intro');
+			Ember.debug('not authed --> home');
+			this.transitionTo('application');
 		}
 	},
 
@@ -15,5 +13,27 @@ export default Ember.Route.extend({
 	},
 	deactivate() {
 		this.controllerFor('application').set('isOnSignIn', false);
+	},
+
+	actions: {
+		// Signs a user in and redirect to either her channel
+		// or the form to create a channel
+		signIn(authWith) {
+			this.get('session').open('firebase', { authWith: authWith }).then(() => {
+				const userChannels = this.get('session.currentUser.channels');
+
+				userChannels.then((channels) => {
+					let channel = channels.get('firstObject');
+
+					if (channel) {
+						Ember.debug('user signed in with channel, redirecting to it');
+						this.transitionTo('channel', channel);
+					} else {
+						Ember.debug('user signed in without channel, redirecting to /new');
+						this.transitionTo('channels.new');
+					}
+				});
+			});
+		}
 	}
 });
