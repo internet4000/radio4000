8a15f189a48aef5394ff8bfd4b860b0a7a775d0e
Oskar Rough <oskarrough@gmail.com>
2015-05-14 14:17:57 +0200

    jshint

--- app/components/channel-cover-link/component.js
+++ app/components/channel-cover-link/component.js
@@ -1,4 +1,4 @@
-import Ember from 'ember';
+// import Ember from 'ember';
 import ChannelCoverComponent from 'radio4000/components/channel-cover/component';
 
 export default ChannelCoverComponent.extend({});
--- app/track-list/view.js
+++ app/track-list/view.js
@@ -1,7 +1,7 @@
-import Ember from 'ember';
+// import Ember from 'ember';
+// import TrackItemComponent from 'radio4000/components/track-item/component';
 import ListView from 'ember-list-view';
 import ListItemView from 'ember-list-view/list-item-view';
-import TrackItemComponent from 'radio4000/components/track-item/component';
 
 // extending ListView
 // customize the row views by subclassing ListItemView
@@ -9,6 +9,5 @@ import TrackItemComponent from 'radio4000/components/track-item/component';
 export default ListView.extend({
 	height: 550,
 	rowHeight: 40,
-	// itemViewClass: ListItemView.extend({ templateName: 'about' })
 	itemViewClass: ListItemView.extend({ templateName: 'about' })
 });
