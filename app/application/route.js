1739f59455d9b3c3e7d6ab3ac89b04608cc340de
Oskar Rough <oskarrough@gmail.com>
2015-05-16 13:02:22 +0200

    Not the best but a fix for /login without channel

--- app/application/route.js
+++ app/application/route.js
@@ -5,12 +5,16 @@ export default Ember.Route.extend({
 		// @todo, what the fuck here?
 		// sign the user in
 		Ember.debug('application:beforemodel');
-		this.get('session').fetch();
+		this.get('session').fetch().then(() => {
+			Ember.debug('fetched success?');
+		}, () => {
+			Ember.debug('fetched fail?');
+		});
 	},
 
 	actions: {
 		logout() {
-			this.get('session').close().then( () =>{
+			this.get('session').close().then(() => {
 				// todo: why does before this it: "Preparing to transition from 'channels.index' to ' login'"
 				Ember.debug('logged out, application.route.action.logout');
 				this.transitionTo('application');
