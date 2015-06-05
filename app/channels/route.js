9992a955df1ef5e6c85388976e140f70e895877a
Oskar Rough <oskarrough@gmail.com>
2015-06-05 12:03:03 +0200

    Faster channel loading on home

--- app/channels/route.js
+++ app/channels/route.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model() {
-		return this.store.find('channel');
+
+		// request all channels
+		this.store.find('channel');
+
+		// but don't return them so we render faster
+		return this.store.all('channel');
 	}
 });
