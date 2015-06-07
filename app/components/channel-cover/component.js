59028d7dcf261c5e7dce54f91bd98eaae9acdcb7
Oskar Rough <oskarrough@gmail.com>
2015-06-07 11:49:56 +0200

    Refactor grid + cover,cover--card

--- app/components/channel-cover/component.js
+++ app/components/channel-cover/component.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	tagName: 'figure',
 	classNames: ['Cover'],
+	classNameBindings: ['isCard:Cover--card'],
+	isCard: false,
 	hideTitle: false,
 	hideLink: false,
 	hideControls: false,
