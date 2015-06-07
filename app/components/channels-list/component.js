6d7015987dfd66bb21faae7c678ea409dcd746f7
Oskar Rough <oskarrough@gmail.com>
2015-06-07 19:37:40 +0200

    Channel list images

--- app/components/channels-list/component.js
+++ app/components/channels-list/component.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	// this component doesn't do anything yet,
-	// it's more a partial. But in the future it could provide
-	// sorting, filtering etc.
+	tagName: 'ul',
+	classNames: ['List'],
+	classNameBindings: ['showCover:List--cover'],
+	showCover: false
+
+	// In the future it could provide sorting, filtering etc.
 });
