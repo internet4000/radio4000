a71e70fe17590918bdd245efe4351c0fdd988486
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2015-10-08 22:54:55 +0200

    add 'your favorite radios' on homepage

--- app/channels/index/controller.js
+++ app/channels/index/controller.js
@@ -1,20 +1,25 @@
 import Ember from 'ember';
 
-const { computed } = Ember;
+const {computed} = Ember;
 
 export default Ember.Controller.extend({
-
 	// This depends on getting all public channels
 	// which we currently do in channel:route afterModel().
-	channels: computed(function() {
+	channels: computed(function () {
 		return this.store.findAll('channel');
 	}),
 
 	// Filter popular channels
-	filteredChannels: computed.filter('channels', function(channel) {
-	   return !channel.get('isFeatured') &&
-	           channel.get('coverImage') &&
-	           channel.get('channelPublic.followers.length') > 6;
+	filteredChannels: computed.filter('channels', function (channel) {
+		return !channel.get('isFeatured') &&
+           channel.get('coverImage') &&
+           channel.get('channelPublic.followers.length') > 6;
+	}),
+
+	loggedInUserFavorites: computed('session', function () {
+		// find logged in user favorite channels
+		return this.get('session.currentUser.channels.firstObject.favoriteChannels');
+
 	}),
 
 	// Sort them by followers
@@ -22,7 +27,7 @@ export default Ember.Controller.extend({
 	sortedChannels: computed.sort('filteredChannels', 'sortProperties'),
 
 	// And only show top X
-	topSortedChannels: computed('sortedChannels.[]', function() {
-	   return this.get('sortedChannels').slice(0, 4);
+	topSortedChannels: computed('sortedChannels.[]', function () {
+		return this.get('sortedChannels').slice(0, 4);
 	})
 });
