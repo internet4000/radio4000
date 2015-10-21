7beac5ce37c8960eb9c5cbca5b01f24188d9ec7b
Oskar Rough <oskarrough@gmail.com>
2015-10-21 22:46:41 +0200

    Change grunticon from grunt to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -53,3 +53,26 @@ gulp.task('electron', ['build-electron'], shell.task([
 gulp.task('build-electron', shell.task([
 	'ember build --environment=electron'
 ]));
+
+// Icons with grunticon (https://gist.github.com/dcalhoun/e79ad10d518612d70721)
+const Grunticon = require('grunticon-lib');
+const q = require('q');
+const path = require('path');
+const fs = require('fs');
+
+gulp.task('icons', function () {
+	const deferred = q.defer();
+	const inputDir = 'public/images/icons/';
+	const outputDir = 'assets/images/icons/';
+	const options = {enhanceSVG: true};
+	const files = fs.readdirSync(inputDir).map(function(fileName) {
+		return path.join(inputDir, fileName);
+	});
+	const grunticon = new Grunticon(files, outputDir, options);
+
+	grunticon.process(function () {
+		deferred.resolve();
+	});
+
+	return deferred.promise;
+});
