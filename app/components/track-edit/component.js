82bca78c9f865e18a34af2b69b4c22d3461b71ad
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2015-10-23 09:25:10 +0200

    ad validations for track.edit and also rollbackAttributes if cancel editing

--- app/components/track-edit/component.js
+++ app/components/track-edit/component.js
@@ -1,13 +1,47 @@
 import Ember from 'ember';
+import EmberValidations from 'ember-validations';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(EmberValidations, {
 	track: null,
+
+	// validations
+	showErrors: false,
+	validations: {
+		'track.url': {
+			presence: true,
+			// TODO make a check for utube URL regex
+			length: {
+				// http://you.be
+				minimum: 13
+			}
+		},
+		'track.title': {
+			presence: true,
+			length: {
+				// http://dougscripts.com/itunes/itinfo/id3tags00.php
+				// artist + songTitle
+				maximum: 60
+			}
+		},
+		'track.body': {
+			length: {
+				maximum: 300
+			}
+		}
+	},
 
 	actions: {
 		submit() {
-			this.sendAction('submit', this.get('track'));
+			this.validate().then(() => {
+				Ember.debug('Edit track validates!')
+				this.sendAction('submit', this.get('track'));
+			}).catch(() => {
+				Ember.debug('Edit does not validate…')
+				this.set('showErrors', true);
+			});
 		},
 		cancel() {
+			this.get('track').rollbackAttributes();
 			this.sendAction('cancel');
 		},
 		deleteTrack() {
