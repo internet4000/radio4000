085cac79d9df9875f750bd742a0a5a833e5fb485
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2015-11-05 23:11:46 +0100

    add flash messages for login logout

--- app/login/route.js
+++ app/login/route.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-const {debug} = Ember;
+const {debug, get} = Ember;
 
 export default Ember.Route.extend({
 	uiStates: Ember.inject.service(),
@@ -17,12 +17,14 @@ export default Ember.Route.extend({
 		// And if the user has a channel we transition to it,
 		// otherwise we transition to create a new channel.
 		logIn(authWith) {
+			const flashMessages = get(this, 'flashMessages');
 			this.get('session').open('firebase', {authWith: authWith}).then(() => {
 				const userChannels = this.get('session.currentUser.channels');
 				userChannels.then(channels => {
 					const channel = channels.get('firstObject');
 					if (channel) {
 						debug('user signed in with channel, transitioning to it');
+						flashMessages.info('You are logged in!');
 						this.transitionTo('channel', channel);
 					} else {
 						debug('user signed in without channel, transitioning to /new');
