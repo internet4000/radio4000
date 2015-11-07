ab60b1ea194b64eda167bc80718bedb280ffe9b1
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2015-11-07 13:48:06 +0100

    debug

--- app/services/user-history.js
+++ app/services/user-history.js
@@ -7,11 +7,14 @@ export default Service.extend({
 	store: inject.service(),
 
 	historyManager(playlist) {
+		Ember.debug('a playlist was passed to historyManager');
 		this.get('session.currentUser.settings').then(settings => {
 			settings.get('channelsHistory').then(history => {
-				console.log(history);
 				history.addObject(playlist);
-			})
-		})
+				settings.save().then(() => {
+					Ember.debug('playlist was saved to currentUser.settings.channelsHistory');
+				});
+			});
+		});
 	}
 });
