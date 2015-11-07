72ff0a329120d7cb9304a88c47ce4ab40bf2efca
Oskar Rough <oskarrough@gmail.com>
2015-11-07 14:23:33 +0100

    Fix model for history

--- app/channels/history/route.js
+++ app/channels/history/route.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model() {
-		this.get('session.currentUser.userSettings.channelsHistory');
+		return this.get('session.currentUser.settings.playedChannels');
 	}
 });
