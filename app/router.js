9e164618f3fed71428ce74e25ff6999f183793f8
Oskar Rough <oskar@rough.dk>
2015-11-19 00:23:55 +0100

    Update ember-cli, ember and data

--- app/router.js
+++ app/router.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import config from './config/environment';
 import googlePageview from './mixins/google-pageview';
 
-var Router = Ember.Router.extend(googlePageview, {
+const Router = Ember.Router.extend(googlePageview, {
 	location: config.locationType
 });
 
