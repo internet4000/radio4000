9e26541c65d6d949893d07898e4d2f357c6d93f7
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2015-12-16 23:22:58 +0100

    a failing test for random test

--- tests/unit/utils/random-text-test.js
+++ tests/unit/utils/random-text-test.js
@@ -4,7 +4,26 @@ import { module, test } from 'qunit';
 module('randomText');
 
 // Replace this with your real tests.
-test('it works', function(assert) {
-	let result = randomText();
-	assert.ok(typeof result === 'string', 'It is a string yay!');
+test('Generate a random string of 4 characters for the newChannel.url', function (assert) {
+	let timesToTest = 10;
+	let timesToSuccess = 8;
+	let i = 0;
+
+	let testedResults = [];
+
+	while (i < timesToTest) {
+		i++;
+		let result = randomText();
+
+		// if the string is in the array, we have a duplicate
+		// so don't push it to the array, to see how many duplicated
+		if (testedResults.indexOf(result) < -1) {
+			testedResults.push('result');
+		}
+
+		assert.ok(typeof result === 'string', 'It is a string');
+		assert.ok(result.length === 4, 'It is a string of 4 characters');
+	}
+
+	assert.ok(testedResults.length > timesToSuccess, 'At least 8 of the random strings were different');
 });
