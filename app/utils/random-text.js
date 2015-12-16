3a1046f88d96552b43c5aaf7a1d765a6c3adea54
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2015-12-16 23:24:42 +0100

    c/p veit's changes on randomText helper

--- app/utils/random-text.js
+++ app/utils/random-text.js
@@ -10,4 +10,20 @@ export default function randomText() {
 	}
 
 	return text;
+
+	// let chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+	// let len = 4;
+	// Array(len).join().split(',').map(() => chars.charAt(Math.floor(Math.random() * chars.length))).join('');
 }
+
+// in that case read it as follows:
+// create an empty array of length len(`Array(len)`),
+// make it a string(by joining) and an array again (by splitting at the commas).
+// this will give us an array of four empty strings (`.join().split(',')`).
+// then go over that array and for each element take a random char instead
+// (`.map(() => chs.charAt(Math.floor(Math.random() * s.length)))`).
+// then join again, without a separator (`.join('')`) and were done.
+// all of this is because of shitty behaviour of javascripts Array.prototype.map function.
+// because that function will just ignore empty positions in an array.
+// so if you have an array with empty positions only (even if its a million), it will not do anything.
+// for me thats more shitty than useful most of the time, but it can be cool i guess.
