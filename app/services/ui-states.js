bce941ad74c36d4b52d8260ecb285a409401abe8
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2015-12-21 01:08:47 +0100

    uiState Api for site panel left

--- app/services/ui-states.js
+++ app/services/ui-states.js
@@ -2,5 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
 	isMinimal: false,
-	isFullscreen: false
+	isFullscreen: false,
+
+	/**
+		left panel api
+	 */
+	isPanelLeftVisible: false,
+	showPanelLeft() {
+		this.set('isPanelLeftVisible', true);
+	},
+	hidePanelLeft() {
+		this.set('isPanelLeftVisible', false);
+	},
+	togglePanelLeft() {
+		this.toggleProperty('isPanelLeftVisible');
+	}
 });
