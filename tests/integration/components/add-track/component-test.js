9694a4ce38ea203f9aa765fcf91be13950f14cb5
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2015-12-21 01:10:32 +0100

    some auto generated tests

---
+++ tests/integration/components/add-track/component-test.js
@@ -0,0 +1,26 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('add-track', 'Integration | Component | add track', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  assert.expect(2);
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{add-track}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#add-track}}
+      template block text
+    {{/add-track}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
