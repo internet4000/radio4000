b66f68266b790114fde23f5cfc8afeff5a696257
Oskar Rough <oskar@rough.dk>
2015-12-22 12:43:37 +0100

    Update aside styles

--- app/components/x-aside/component.js
+++ app/components/x-aside/component.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+const {Component} = Ember;
+
+export default Component.extend({
+	tagName: 'aside',
 	classNames: ['Aside']
 });
