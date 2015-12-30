5ff9a1595fc0828ce0901b647bdd0a740cdb1f2a
Oskar Rough <oskar@rough.dk>
2015-12-30 14:01:18 +0100

    Update styleguide with forms, buttons, pods and tabs

--- app/router.js
+++ app/router.js
@@ -3,52 +3,53 @@ import config from './config/environment';
 import googlePageview from './mixins/google-pageview';
 
 const Router = Ember.Router.extend(googlePageview, {
-	location: config.locationType
+    location: config.locationType
 });
 
 Router.map(function () {
-	// pages
-	this.route('about', function () {
-		this.route('contact');
-		this.route('technology');
-	});
-	this.route('bookmarklet');
-	this.route('dashboard');
-	this.route('help', function () {
-		// help.index
-	});
-	this.route('intro');
-	this.route('404');
-	this.route('styleguide', function () {
-		this.route('colors');
-		this.route('typography');
-		this.route('forms');
-	});
-	this.route('login');
-	this.route('logout');
+    // pages
+    this.route('about', function () {
+        this.route('contact');
+        this.route('technology');
+    });
+    this.route('bookmarklet');
+    this.route('dashboard');
+    this.route('help', function () {
+        // help.index
+    });
+    this.route('intro');
+    this.route('404');
+    this.route('styleguide', function () {
+      this.route('typography');
+      this.route('colors');
+      this.route('forms');
+      this.route('tabs');
+    });
+    this.route('login');
+    this.route('logout');
 
-	this.route('channels', {path: '/'}, function () {
-		this.route('all');
-		this.route('new');
-		this.authenticatedRoute('history');
-	});
+    this.route('channels', {path: '/'}, function () {
+        this.route('all');
+        this.route('new');
+        this.authenticatedRoute('history');
+    });
 
-	// Support deprecated channels urls. Needs to be defined before 'channel' route.
-	this.route('channel-alias', {path: '/c/:slug'});
+    // Support deprecated channels urls. Needs to be defined before 'channel' route.
+    this.route('channel-alias', {path: '/c/:slug'});
 
-	this.route('channel', {path: '/:slug'}, function () {
-		this.authenticatedRoute('add', function () {
-			this.route('bookmarklet');
-		});
-		this.authenticatedRoute('edit');
-		this.authenticatedRoute('delete');
-		this.route('dashboard', {});
-		this.route('index', {path: '/'}, function () {
-			this.route('track', {path: ':track_id', resetNamespace: true});
-		});
-		this.route('favorites');
-		this.route('followers');
-	});
+    this.route('channel', {path: '/:slug'}, function () {
+        this.authenticatedRoute('add', function () {
+            this.route('bookmarklet');
+        });
+        this.authenticatedRoute('edit');
+        this.authenticatedRoute('delete');
+        this.route('dashboard', {});
+        this.route('index', {path: '/'}, function () {
+            this.route('track', {path: ':track_id', resetNamespace: true});
+        });
+        this.route('favorites');
+        this.route('followers');
+    });
 });
 
 export default Router;
