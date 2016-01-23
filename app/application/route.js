c3e0e91a431dbcc4b62f4fe08526b321a7f64233
Oskar Rough <oskarrough@gmail.com>
2016-01-23 12:44:41 +0100

    Not sure why, but this is not needed anymore.

--- app/application/route.js
+++ app/application/route.js
@@ -3,10 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	// refresh (fetch) any cached user session
 	beforeModel() {
-		return this.get('session').fetch().then(() => {
-			// Ember.debug('user logged in passively');
-		}, () => {
-			// Ember.debug('no user');
-		});
+		// return this.get('session').fetch().then(() => {
+		// 	// Ember.debug('user logged in passively');
+		// }, () => {
+		// 	// Ember.debug('no user');
+		// });
 	}
 });
