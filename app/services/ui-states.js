cb94744fdce3292fb00853761e894049cc616600
Oskar Rough <oskarrough@gmail.com>
2016-01-23 01:00:16 +0100

    New: store panel open/closed in a cookie

--- app/services/ui-states.js
+++ app/services/ui-states.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
+import Cookies from 'ember-cli-js-cookie';
+
+const {computed} = Ember;
 
 export default Ember.Service.extend({
 	isMinimal: false,
@@ -7,7 +10,18 @@ export default Ember.Service.extend({
 	/**
 		left panel api
 	 */
-	isPanelLeftVisible: true,
+	// isPanelLeftVisible: true,
+	isPanelLeftVisible: computed({
+		get() {
+			// if the cookie is undefined (not set), it returns true
+			// or if the cookie has 'true' as value, return true
+			return Cookies.get('isPanelOpen') === undefined || Cookies.get('isPanelOpen') === 'true';
+		},
+		set(key, value) {
+			Cookies.set('isPanelOpen', value, {expires: 7});
+			return value;
+		}
+	}),
 	showPanelLeft() {
 		this.set('isPanelLeftVisible', true);
 	},
