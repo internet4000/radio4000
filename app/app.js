d5edd8891100f96160ea5c55776de458f1430e58
Oskar Rough <oskarrough@gmail.com>
2016-01-24 11:08:28 +0100

    Add lazy loading to cover images (saves 10mb on /all)

--- app/app.js
+++ app/app.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
-// import lazysizes from 'npm:lazysizes';
 
 let App;
 
