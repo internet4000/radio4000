1291f006b6682fa2094c346c5af2208431df813f
Oskar Rough <oskarrough@gmail.com>
2016-02-17 00:11:37 +0100

    Don't open panel automatically on mobile and close panel when you tap a link on mobile

--- app/components/x-aside/component.js
+++ app/components/x-aside/component.js
@@ -1,8 +1,17 @@
 import Ember from 'ember';
 
-const {Component} = Ember;
+const {Component, inject} = Ember;
 
 export default Component.extend({
 	tagName: 'aside',
-	classNames: ['Aside']
+	classNames: ['Aside'],
+
+	uiStates: inject.service(),
+
+	click(event) {
+		const clickedElementHasAnHref = event.target.href;
+		if (clickedElementHasAnHref && this.get('uiStates.isPanelLeftVisible')) {
+			this.get('uiStates').perhapsClosePanel();
+		}
+	}
 });
