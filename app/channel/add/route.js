93d4f5b02fc00123169c47b9fbb54e41d9541629
Oskar Rough <oskarrough@gmail.com>
2016-02-19 19:08:47 +0100

    Fix more href-to…

--- app/channel/add/route.js
+++ app/channel/add/route.js
@@ -3,4 +3,4 @@ import ownerRouteMixin from 'radio4000/mixins/owner-route';
 
 const {Route} = Ember;
 
-export default Route.extend(ownerRouteMixin, {});
+export default Route.extend(ownerRouteMixin);
