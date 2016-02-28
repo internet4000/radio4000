fdeeaf2464c486a69b4c5c67a09e9e2257a6dd43
Oskar Rough <oskarrough@gmail.com>
2016-02-28 22:18:02 +0100

    Play with dynamic last updated

--- app/channels/history/route.js
+++ app/channels/history/route.js
@@ -9,9 +9,29 @@ export default Ember.Route.extend({
 			userSettings: this.get('session.currentUser.settings')
 		});
 	},
+
+	/*findLastTrackFromChannel(channel) {
+		const ref = new Firebase(`https://radio4000-dev.firebaseio.com/channels/${channel.get('id')}`);
+		return new Ember.RSVP.Promise((resolve, reject) => {
+			ref.child('tracks').orderByKey().limitToLast(1).on('value', snapshot => {
+				const id = Object.keys(snapshot.val())[0];
+				this.store.findRecord('track', id).then(track => {
+					resolve(track);
+				}).catch(error => {
+					reject(error);
+				});
+			});
+		});
+	},*/
+
 	actions: {
 		clearChannelHistory() {
 			this.get('playerHistory').clearChannelHistory();
-		}
+		}/*,
+		lastUpdated(channel) {
+			this.findLastTrackFromChannel(channel).then(track => {
+				console.log(track.get('title'));
+			});
+		}*/
 	}
 });
