2745a18882978705a23b174a181ba3efd5fb1ea5
Oskar Rough <oskarrough@gmail.com>
2016-03-03 14:15:08 +0100

    Fix electron-builds

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -11,7 +11,7 @@ module.exports = function (defaults) {
 
     // Don't fingerprint favicons as browsers expect standard names.
     fingerprint: {
-      exclude: ['apple-touch-icon', 'favicon', 'mstile']
+      exclude: ['apple-touch-icon', 'favicon', 'mstile', 'icns']
     },
 
     // Very verbose but this adds autoprefixer and atImport.
