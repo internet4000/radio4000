ffdf1bd171de73525e9a5f1e4d855b60be04e438
Oskar Rough <oskarrough@gmail.com>
2016-03-05 16:02:16 +0100

    Add (tested) `is-environment` helper and a note to dev environments

---
+++ app/helpers/is-environment.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+import config from '../config/environment';
+
+// Returns {boolean} if a single {string} matches the current environment.
+export function isEnvironment(params) {
+	// Because Ember sends params as an array in tempaltes, but not in tests.
+	let env;
+	if (typeof params === 'string') {
+		env = params;
+	} else {
+		env = params[0];
+	}
+
+	return config.environment === env;
+}
+
+export default Ember.Helper.helper(isEnvironment);
