b0185eb61c14cbd53d960b157e454721488311e3
Oskar Rough <oskarrough@gmail.com>
2016-03-12 13:40:29 +0100

    Add provider id to the DOM (useful for later)

--- app/components/track-item/component.js
+++ app/components/track-item/component.js
@@ -6,6 +6,7 @@ export default Component.extend({
 	player: inject.service(),
 	classNames: ['Track'],
 	classNameBindings: ['isCurrent', 'track.usedInCurrentPlayer:Track--used', 'track.finishedInCurrentPlayer:Track--finished'],
+	attributeBindings: ['track.ytid:data-pid'],
 
 	// true if the current track is loaded in the player
 	// isCurrent: computed.equal('player.model', 'track'),
