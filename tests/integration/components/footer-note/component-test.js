164b6a29db27783061f28e0cde0fabd85fed1b8c
Oskar Rough <oskarrough@gmail.com>
2016-03-12 19:29:47 +0100

    [test] fix footer-note test

--- tests/integration/components/footer-note/component-test.js
+++ tests/integration/components/footer-note/component-test.js
@@ -10,15 +10,5 @@ test('it renders', function(assert) {
   // Handle any actions with this.on('myAction', function(val) { ... });"
 
   this.render(hbs`{{footer-note}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:"
-  this.render(hbs`
-    {{#footer-note}}
-      template block text
-    {{/footer-note}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), 'Internet4000');
 });
