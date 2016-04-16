006ff680b7e198abc44ba311a61a80c6f218c185
Oskar Rough <oskarrough@gmail.com>
2016-04-16 16:01:15 +0200

    Show tracks count on channel detail

--- app/models/channel-public.js
+++ app/models/channel-public.js
@@ -1,6 +1,8 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 const {hasMany, belongsTo} = DS;
+const {computed} = Ember;
 
 // inverse tells Ember which relationship this matches on the other model
 
@@ -12,5 +14,8 @@ export default DS.Model.extend({
 	channel: belongsTo('channel', {
 		inverse: 'channelPublic',
 		async: true
+	}),
+	totalFollowers: computed('tracks', function () {
+		return this.hasMany('followers').ids().length;
 	})
 });
