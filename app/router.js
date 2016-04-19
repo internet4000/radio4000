0682036d65a228bf57239c80a9fde11a25d8dcf1
Oskar Rough <oskarrough@gmail.com>
2016-04-19 12:02:07 +0200

    Change the feedback form to be inline

--- app/router.js
+++ app/router.js
@@ -7,47 +7,48 @@ const Router = Ember.Router.extend(googlePageview, {
 });
 
 Router.map(function () {
-    // pages
-    this.route('about', function () {
-        this.route('contact');
-        this.route('technology');
-    });
-    this.route('bookmarklet');
-    this.route('dashboard');
-    this.route('help');
-    this.route('intro');
-    this.route('404');
-    this.route('styleguide', function () {
-      this.route('typography');
-      this.route('colors');
-      this.route('forms');
-      this.route('tabs');
-    });
-    this.route('login');
-    this.route('logout');
+  // pages
+  this.route('about', function () {
+      this.route('contact');
+      this.route('technology');
+  });
+  this.route('bookmarklet');
+  this.route('dashboard');
+  this.route('help');
+  this.route('intro');
+  this.route('404');
+  this.route('styleguide', function () {
+    this.route('typography');
+    this.route('colors');
+    this.route('forms');
+    this.route('tabs');
+  });
+  this.route('login');
+  this.route('logout');
 
-    this.route('channels', {path: '/'}, function () {
-        this.route('all');
-        this.route('new');
-        this.authenticatedRoute('history');
-    });
+  this.route('channels', {path: '/'}, function () {
+      this.route('all');
+      this.route('new');
+      this.authenticatedRoute('history');
+  });
 
-    // Support deprecated channels urls. Needs to be defined before 'channel' route.
-    this.route('channel-alias', {path: '/c/:slug'});
+  // Support deprecated channels urls. Needs to be defined before 'channel' route.
+  this.route('channel-alias', {path: '/c/:slug'});
 
-    this.route('channel', {path: '/:slug'}, function () {
-        this.authenticatedRoute('add', function () {
-            this.route('bookmarklet');
-        });
-        this.authenticatedRoute('edit');
-        this.authenticatedRoute('delete');
-        this.route('dashboard', {});
-        this.route('index', {path: '/'}, function () {
-            this.route('track', {path: ':track_id', resetNamespace: true});
-        });
-        this.route('favorites');
-        this.route('followers');
-    });
+  this.route('channel', {path: '/:slug'}, function () {
+      this.authenticatedRoute('add', function () {
+          this.route('bookmarklet');
+      });
+      this.authenticatedRoute('edit');
+      this.authenticatedRoute('delete');
+      this.route('dashboard', {});
+      this.route('index', {path: '/'}, function () {
+          this.route('track', {path: ':track_id', resetNamespace: true});
+      });
+      this.route('favorites');
+      this.route('followers');
+  });
+  this.route('feedback');
 });
 
 export default Router;
