dadf54f3ae2d4e5adbe96d9a9153d80a8f694b1b
Oskar Rough <oskarrough@gmail.com>
2016-04-24 13:41:20 +0200

    Refactor from component to service tasks

--- app/bot/service.js
+++ app/bot/service.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import {task} from 'ember-concurrency';
 
 const {Service, inject} = Ember;
 
@@ -6,6 +7,25 @@ const randomIndex = array => Math.floor(Math.random() * array.get('length'));
 
 export default Service.extend({
 	store: inject.service(),
+	player: inject.service(),
+
+	playAnotherRadio: task(function * (prev) {
+		const channel = yield this.findRandomChannel();
+		// if the new "random" is the same (it happens), run again
+		if (prev && prev.id === channel.id) {
+			return this.get('playAnotherRadio').perform(channel);
+		}
+		this.get('player').set('isShuffled', true);
+		yield this.get('playNewestTrack').perform(channel);
+	}).drop(),
+
+	playNewestTrack: task(function * (channel) {
+		const track = yield this.findLastTrack(channel);
+		if (!track) {
+			throw new Error('playTrack was called without a track.');
+		}
+		this.get('player').playTrack(track);
+	}).drop(),
 
 	findLast(x, type) {
 		return this.get('store').query(type, {limitToLast: x});
--- app/components/radio-bot/component.js
+++ app/components/radio-bot/component.js
@@ -1,32 +1,11 @@
 import Ember from 'ember';
-import {task} from 'ember-concurrency';
 
 const {Component, inject} = Ember;
 
 export default Component.extend({
 	classNames: ['Bot'],
-	channel: null,
 
 	bot: inject.service(),
-	player: inject.service(),
-
-	skipRadio: task(function * (prev) {
-		const channel = yield this.get('bot').findRandomChannel();
-		// if the new "random" is the same (it happens), run again
-		if (prev && prev.id === channel.id) {
-			return this.get('skipRadio').perform(channel);
-		}
-		this.set('channel', channel);
-		this.get('playTrack').perform(channel);
-		this.get('player').set('isShuffled', true);
-	}).drop(),
-
-	playTrack: task(function * (channel) {
-		const track = yield this.get('bot').findLastTrack(channel);
-		if (!track) {
-			throw new Error('playTrack was called without a track.');
-		}
-		this.get('player').playTrack(track);
-	}).drop()
+	player: inject.service()
 });
 
