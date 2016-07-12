a3933b20234fea975b1acb18dbd03a9d6f8ea335
Oskar Rough <oskarrough@gmail.com>
2016-07-12 14:15:42 +0200

    Rewrite torii adapter (facebook works, google not so much)

--- app/torii-adapters/application.js
+++ app/torii-adapters/application.js
@@ -1,129 +1,33 @@
-/* jshint unused:false */
 import Ember from 'ember';
-// import ToriiFirebaseAdapter from 'emberfire/torii-adapters/firebase';
+import ToriiFirebaseAdapter from 'emberfire/torii-adapters/firebase';
+import {getOrCreateUser, createUserSetting} from 'radio4000/utils/get-or-create-user';
 
-const {debug, inject, run} = Ember;
+const {debug, inject, run, RSVP} = Ember;
 
-// export default ToriiFirebaseAdapter.extend({
-export default Ember.Object.extend({
-	firebase: inject.service(),
+export default ToriiFirebaseAdapter.extend({
 	store: inject.service(),
+	// firebase: inject.service(),
 
-	createSettings(user) {
-		const newSettings = this.get('store').createRecord('user-setting', {user});
-		newSettings.save().then(settings => {
-			user.set('settings', settings).save().then(() => {
-				Ember.debug('saved user settings');
-			});
-		});
-	},
-
-	// creating a new authorization or authenticating a new session
-	open(auth) {
+	/**
+	* Extacts session information from authentication response
+	*
+	* @param {!firebase.User} user
+	* @return {Promise}
+	*/
+	open(user) {
+		this._super(user);
 		const store = this.get('store');
-
-		return new Ember.RSVP.Promise((resolve, reject) => {
-			debug('finding user', auth.uid);
-			return store.findRecord('user', auth.uid)
-				// we have a user, create settings if needed
-				// and resolve
-				.then(user => {
-					debug('found user');
-
-					user.get('settings').then(settings => {
-						if (settings) {
-							return;
-						}
-						debug('no settings for user, creating them');
-						this.createSettings(user);
-					});
-
-					run.bind(null, resolve({currentUser: user}));
-				})
-
-				// no user, so create one
-				.catch(() => {
-					debug('no user found, creating one');
-
-					// but first avoid this bug about unresolved record
-					// @todo maybe nut needed anymore?!
-					// store.recordForId('user', auth.uid).unloadRecord();
-					this.createUser(auth).then(user => {
-						debug('created a user');
-						run.bind(null, resolve({currentUser: user}));
-					}, error => {
-						reject({error});
-					});
+		return new RSVP.Promise(resolve => {
+			getOrCreateUser(user.uid, store).then(userModel => {
+				createUserSetting(userModel, store)
+				resolve({
+					provider: this.extractProviderId_(user),
+					uid: user.uid,
+					// currentUser: user
+					// the above three props are defaults, below is custom
+					currentUser: userModel
 				});
-		});
-	},
-
-	// validating an existing authorization (like a session stored in cookies)
-	fetch() {
-		const firebase = this.get('firebase');
-
-		// This is what should be done to determine how to fetch a session. Here I am
-		// retrieving the auth from firebase and checking if I have a user for that auth.
-		// If so, I set currentUser.
-
-		// The object containing the currentUser is merged onto the session.
-		// Because the session is injected onto controllers and routes,
-		// these values will be available to templates.
-		// https://github.com/Vestorly/torii#adapters-in-torii
-
-		return new Ember.RSVP.Promise((resolve, reject) => {
-			const auth = firebase.getAuth();
-
-			if (auth === null) {
-				debug('no auth');
-				reject('No session available');
-			}
-
-			debug('fetch resolving, looking for user');
-			this.store.findRecord('user', auth.uid).then(currentUser => {
-				debug('found user');
-				resolve({currentUser});
-			}, () => {
-				debug('no user found');
-				reject(new Error('no user found'));
 			});
 		});
-	},
-
-	// This is what should be done to teardown a session. Here I am unloading my
-	// models and setting currentUser to null. here an authorization is destroyed
-	close() {
-		const firebase = this.get('firebase');
-		firebase.unauth();
-		return Ember.RSVP.resolve();
-	},
-
-	createUser(auth) {
-		const newUser = this.store.createRecord('user', {
-			id: auth.uid,
-			provider: auth.provider,
-			name: this._nameFor(auth)
-		});
-		return newUser.save().then(user => {
-			debug('created user');
-			this.createSettings(user);
-			return user;
-		}, () => {
-			debug('couldnt create user');
-		});
-	},
-
-	_nameFor(auth) {
-		if (auth.github) {
-			return auth.github.username;
-		} else if (auth.facebook) {
-			return auth.facebook.displayName;
-		} else if (auth.twitter) {
-			return auth.twitter.displayName;
-		} else if (auth.google) {
-			return auth.google.displayName;
-		}
-
-		throw new Error('could not find a username!');
 	}
 });
---
+++ tests/unit/utils/get-or-create-user-test.js
@@ -0,0 +1,10 @@
+import getOrCreateUser from 'radio4000/utils/get-or-create-user';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | get or create user');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = getOrCreateUser();
+  assert.ok(result);
+});
