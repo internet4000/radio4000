a63846ffd06a8e9d949c77e044dba4934df41e59
Oskar Rough <oskarrough@gmail.com>
2016-07-29 12:44:27 +0200

    Fix some tests

--- tests/unit/utils/string-contains-test.js
+++ tests/unit/utils/string-contains-test.js
@@ -4,6 +4,9 @@ import {module, test} from 'qunit';
 module('Unit | Utility | string contains');
 
 test('it works', function (assert) {
-	let result = stringContains('This is the internet. Let me be your guide.', 'guide');
-	assert.ok(result);
+	assert.expect(2);
+	let test1 = stringContains('This is the internet. Let me be your guide.', 'guide');
+	assert.ok(test1);
+	let test2 = stringContains('Michael Jackson', 'Prince');
+	assert.notOk(test2);
 });
