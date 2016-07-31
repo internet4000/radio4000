20a8e83d62bebf2c0dea52d2ab5594dd1fe1bf04
Oskar Rough <oskarrough@gmail.com>
2016-07-31 15:01:59 +0200

    Refactor error handling

--- app/application/route.js
+++ app/application/route.js
@@ -1,21 +1,11 @@
 import Ember from 'ember';
 
-// const {inject} = Ember;
-
 export default Ember.Route.extend({
-	// player: inject.service(),
-	// playTrackFromId(id) {
-	// 	if (!id) {
-	// 		return;
-	// 	}
-	// 	this.store.find('track', id).then(track => {
-	// 		this.get('player').playTrack(track);
-	// 	});
-	// },
-
-	// actions: {
-	// 	queryParamsDidChange(params) {
-	// 		this.playTrackFromId(params.listen);
-	// 	}
-	// }
+	actions: {
+		error(error, transition) {
+			if (error) {
+				this.transitionTo('404');
+			}
+		}
+	}
 });
