0c98ae772be54a6af1fa687f41fe98b12ebe340c
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2016-08-10 18:11:01 +0200

    fix user settings creation

--- app/torii-adapters/application.js
+++ app/torii-adapters/application.js
@@ -5,29 +5,29 @@ import {getOrCreateUser, createUserSetting} from 'radio4000/utils/get-or-create-
 const {inject, RSVP} = Ember;
 
 export default ToriiFirebaseAdapter.extend({
-	store: inject.service(),
-	// firebase: inject.service(),
+    store: inject.service(),
+    // firebase: inject.service(),
 
-	/**
-	* Extacts session information from authentication response
-	*
-	* @param {!firebase.User} user
-	* @return {Promise}
-	*/
-	open(user) {
-		this._super(user);
-		const store = this.get('store');
-		return new RSVP.Promise(resolve => {
-			getOrCreateUser(user.uid, store).then(userModel => {
-				createUserSetting(userModel, store);
-				resolve({
-					provider: this.extractProviderId_(user),
-					uid: user.uid,
-					// currentUser: user
-					// the above three props are defaults, below is custom
-					currentUser: userModel
-				});
-			});
+    /**
+     * Extacts session information from authentication response
+     *
+     * @param {!firebase.User} user
+     * @return {Promise}
+     */
+    open(user) {
+	this._super(user);
+	const store = this.get('store');
+	return new RSVP.Promise(resolve => {
+	    getOrCreateUser(user.uid, store).then(userModel => {
+		createUserSetting(userModel, store);
+		resolve({
+		    provider: this.extractProviderId_(user),
+		    uid: user.uid,
+		    // currentUser: user
+		    // the above three props are defaults, below is custom
+		    currentUser: userModel
 		});
-	}
+	    });
+	});
+    }
 });
--- app/utils/get-or-create-user.js
+++ app/utils/get-or-create-user.js
@@ -3,38 +3,41 @@ import Ember from 'ember';
 const {RSVP} = Ember;
 
 export function createUserSetting(user, store) {
-	if (!user) {
-		throw new Error('Missing `user` argument');
-	}
-	if (user.get('settings').length) {
-		console.log('Can not create user setting. Already exists.');
-		return RSVP.Promise.resolve(user.get('settings.firstObject'));
+    if (!user) {
+	throw new Error('Missing `user` argument');
+    }
+
+    user.get('settings').then(settings => {
+	if (settings) {
+	    console.log('Can not create user setting. Already exists.');
+	    return RSVP.Promise.resolve(user.get('settings.firstObject'));
 	}
 	const userSetting = store.createRecord('user-setting', {user});
 	return new RSVP.Promise(resolve => {
-		userSetting.save().then(() => {
-			user.set('settings', userSetting);
-			user.save().then(() => resolve(userSetting));
-		});
+	    userSetting.save().then(() => {
+		user.set('settings', userSetting);
+		user.save().then(() => resolve(userSetting));
+	    });
 	});
+    });
 }
 
 export function getOrCreateUser(id, store) {
-	if (!id) {
-		throw new Error('Missing `id` argument');
-	}
-	if (!store) {
-		throw new Error('Missing `store` argument');
-	}
-	return new RSVP.Promise(resolve => {
-		store.findRecord('user', id).then(user => {
-			resolve(user);
-		}).catch(error => {
-			console.log(error);
-			const newUser = store.createRecord('user', {id});
-			newUser.save().then(() => {
-				resolve(newUser);
-			});
-		});
+    if (!id) {
+	throw new Error('Missing `id` argument');
+    }
+    if (!store) {
+	throw new Error('Missing `store` argument');
+    }
+    return new RSVP.Promise(resolve => {
+	store.findRecord('user', id).then(user => {
+	    resolve(user);
+	}).catch(error => {
+	    console.log(error);
+	    const newUser = store.createRecord('user', {id});
+	    newUser.save().then(() => {
+		resolve(newUser);
+	    });
 	});
+    });
 }
