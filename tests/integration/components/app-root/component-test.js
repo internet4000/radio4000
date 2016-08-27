4706dd7b18369c7c2cc188456b055c4284a3a69f
Oskar Rough <oskarrough@gmail.com>
2016-08-27 17:19:01 +0200

    Fix test conflict with ember-youtube

--- tests/integration/components/app-root/component-test.js
+++ tests/integration/components/app-root/component-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import {moduleForComponent, test} from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
@@ -6,9 +7,18 @@ moduleForComponent('app-root', 'Integration | Component | app root', {
 });
 
 test('it renders', function (assert) {
-	// assert.expect(2);
+	assert.expect(2);
 	this.render(hbs`{{app-root}}`);
-	// Make sure the dummy app is being removed.
-	assert.notOk(document.querySelector('.DummyApp'));
-	// assert.ok(this.$().hasClass('Root'));
+
+	// This promise is only here to trick the test to run long enough for the
+	// child "ember-youtube" component inside
+	return Ember.Test.promise(function(resolve) {
+		window.setTimeout(function () {
+			// todo: make a more relevant test than this.
+			assert.ok(this.$('.Playback-buttons').length);
+			// Make sure the dummy app is being removed.
+			assert.notOk(document.querySelector('.DummyApp'));
+			resolve();
+		}, 2000);
+	});
 });
