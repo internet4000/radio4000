c7a260552fb218a78e25b2201d419ab5c2ea7652
Oskar Rough <oskarrough@gmail.com>
2016-10-21 12:55:55 +0200

    Remove deprecated properties on user model

--- app/models/user.js
+++ app/models/user.js
@@ -3,21 +3,12 @@ import DS from 'ember-data';
 const {belongsTo, attr, hasMany, Model} = DS;
 
 export default Model.extend({
-	name: attr('string'),
-	email: attr('string'),
 	provider: attr('string'),
 	created: attr('number', {
 		defaultValue() {
 			return new Date().getTime();
 		}
 	}),
-	// @TODO: we should store like this instead,
-	// but first we should convert all old data to the new format.
-	// created: attr('date', {
-	// 	defaultValue: function () {
-	// 		return new Date();
-	// 	}
-	// }),
 	channels: hasMany('channel', {
 		async: true
 	}),
