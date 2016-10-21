e547009c6ff86251b26f8dc8bb16f066c69c3b70
Oskar Rough <oskarrough@gmail.com>
2016-10-21 17:03:27 +0200

    Update ember-electron

--- tests/electron.js
+++ tests/electron.js
@@ -1,37 +1,33 @@
 /* jshint undef: false */
 
-const {BrowserWindow, app} = require('electron');
+const BrowserWindow = require('electron').BrowserWindow;
+const app = require('electron').app;
 
 let mainWindow = null;
 
 app.on('window-all-closed', function onWindowAllClosed() {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('ready', function onReady() {
-	mainWindow = new BrowserWindow({
-		'width': 1000,
-		'height': 700,
-		'min-width': 320,
-		'min-height': 400,
-		'frame': true,
-		'resizable': true,
-		'toolbar': true
-	});
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
 
-	delete mainWindow.module;
+    delete mainWindow.module;
 
-	if (process.env.EMBER_ENV === 'test') {
-		mainWindow.loadURL(`file://${__dirname}/index.html`);
-	} else {
-		mainWindow.loadURL(`file://${__dirname}/dist/index.html`);
-	}
+    if (process.env.EMBER_ENV === 'test') {
+        mainWindow.loadURL('file://' + __dirname + '/index.html');
+    } else {
+        mainWindow.loadURL('file://' + __dirname + '/dist/index.html');
+    }
 
-	mainWindow.on('closed', function onClosed() {
-		mainWindow = null;
-	});
+    mainWindow.on('closed', function onClosed() {
+        mainWindow = null;
+    });
 });
 
 /* jshint undef: true */
