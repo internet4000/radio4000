bc6f91e92811b042c74ce82d63daf63f3339b7c9
Oskar Rough <oskarrough@gmail.com>
2016-10-21 12:54:07 +0200

    Add `created` to image model

--- app/models/image.js
+++ app/models/image.js
@@ -1,8 +1,14 @@
 import DS from 'ember-data';
+import firebase from 'firebase';
 
 const {Model, attr, belongsTo} = DS;
 
 export default Model.extend({
 	channel: belongsTo('channel'),
-	src: attr('string')
+	src: attr('string'),
+	created: attr('number', {
+		defaultValue() {
+			return firebase.database.ServerValue.TIMESTAMP;
+		}
+	})
 });
