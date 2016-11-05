e486f418ebe5c2ff1781f46d102c788f779dd2cb
Oskar Rough <oskarrough@gmail.com>
2016-11-05 12:38:05 +0100

    Fix tests for clean utility

--- app/utils/clean.js
+++ app/utils/clean.js
@@ -1,9 +1,9 @@
 // Cleans spaces in the beginning or end
 // and dasherizes, which also makes it lowercase
 // and remove special (but normally safe) chars
-// remember not do remove hyphens ----
+// remember not do remove hyphens ----.
+// For something with actual tests, consider https://github.com/pid/speakingurl
 
 export default function clean(string) {
-	const cleaned = string.trim().dasherize();
-	return cleaned.replace(/[`~!@#$%^&*()_|+=?;:'",.<>\{\}\[\]\\\/]/gi, '');
+	return string.trim().dasherize().replace(/[`~!@#$%^&*()_|+=?;:'",.<>{}[\]\\/]/gi, '');
 }
