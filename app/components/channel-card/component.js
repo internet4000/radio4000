b59b6178317354ffc1503c8111dee03fdf399c53
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2016-11-07 01:05:29 +0100

    fix channel card title with empty body channel

--- app/components/channel-card/component.js
+++ app/components/channel-card/component.js
@@ -1,10 +1,15 @@
 import Ember from 'ember';
 
-const {Component, computed} = Ember;
+const {Component, computed, get} = Ember;
 
 export default Component.extend({
-	tagName: 'article',
-	classNames: ['ChannelCard'],
-	classNameBindings: ['isActive', 'wide:ChannelCard--wide'],
-	isActive: computed.reads('channel.isInPlayer')
+    tagName: 'article',
+    classNames: ['ChannelCard'],
+    classNameBindings: ['isActive', 'wide:ChannelCard--wide'],
+    isActive: computed.reads('channel.isInPlayer'),
+    title: computed('channel.title', 'channel.body', function() {
+	const body = this.get('channel.body') || '';
+	const title = this.get('channel.title') || '';
+	return `${body} (${title})`;
+    })
 });
