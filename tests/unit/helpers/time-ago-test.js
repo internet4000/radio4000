ccb0882c41bed963c45fc3ba4d986b833275adb8
Oskar Rough <oskarrough@gmail.com>
2016-12-06 12:05:34 +0100

    Hide dates earlier than today
    and rename the helper

---
+++ tests/unit/helpers/time-ago-test.js
@@ -0,0 +1,11 @@
+import {timeAgo} from 'radio4000/helpers/time-ago';
+import {module, test} from 'qunit';
+
+module('Unit | Helper | time ago');
+
+test('it works', function (assert) {
+	let result = timeAgo([1481022124443]);
+	assert.ok(typeof result === 'string', 'it is a string');
+	assert.ok(result.indexOf(' ago') > -1, 'it contains the word "ago"');
+});
+
