c7e0386815ba0bc2b1c48f481c6eb1987adf17bc
Oskar Rough <oskar@rough.dk>
2016-12-27 18:22:03 +0100

    Refactor reset track

--- app/components/track-form-add/component.js
+++ app/components/track-form-add/component.js
@@ -4,21 +4,18 @@ import {Validations} from 'radio4000/models/track';
 
 const {get, set, computed} = Ember;
 
-// This component extends the track-form component by supporting a `initialUrl` property
-// that is used to create a new track.
-
-/*
-	Creating a real track model immediately pushes it into the store which we do not want. Because it'll appear in the interface. Instead we use this pseudoTrack, which is LATER converted into a track model. The getOwner part is mentioned in the ember-cp-validation docs.
-*/
-const pseudoTrack = Ember.Object.extend(Validations);
+// Use a normal object instead af a model.
+// This avoids it appearing in the UI before it is saved.
+const trackObject = Ember.Object.extend(Validations);
 
 export default TrackFormComponent.extend({
-	initialUrl: null,
+	initialUrl: '',
 
 	disableSubmit: computed.or('isSubmitting', 'track.validations.isInvalid'),
 
-	_reset() {
-		const track = pseudoTrack.create(Ember.getOwner(this).ownerInjection(), {
+	resetTrack() {
+		// The getOwner part is mentioned in the ember-cp-validation docs.
+		const track = trackObject.create(Ember.getOwner(this).ownerInjection(), {
 			url: get(this, 'initialUrl')
 		});
 		set(this, 'track', track);
@@ -26,6 +23,6 @@ export default TrackFormComponent.extend({
 
 	init() {
 		this._super(...arguments);
-		this._reset();
+		this.resetTrack();
 	}
 });
