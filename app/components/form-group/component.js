1a4a7beea99e17917b021944fc2f920a58930599
Oskar Rough <oskar@rough.dk>
2017-01-01 02:43:53 +0100

    Fix for keeping focus and show validations

--- app/components/form-group/component.js
+++ app/components/form-group/component.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-const {Component, computed, get, set, defineProperty} = Ember;
+const {Component, computed, get, set, defineProperty, run} = Ember;
 
 export default Component.extend({
 	classNames: ['Form-group'],
@@ -18,9 +18,26 @@ export default Component.extend({
 		defineProperty(this, 'value', computed.alias(`model.${valuePath}`));
 	},
 
+	childInputHasFocus() {
+		let focusedElement = document.activeElement;
+		let input = this.$('input');
+		if (!input) {
+			return;
+		}
+		return input[0].id === focusedElement.id;
+	},
+
 	focusOut() {
 		this._super(...arguments);
-		set(this, 'showValidations', true);
+
+		// If the nested input has autofocus this event will trigger.
+		// and we don't want it to, since focus wasn't left by the user.
+		run.schedule('afterRender', this, function () {
+			if (this.childInputHasFocus()) {
+				return;
+			}
+			set(this, 'showValidations', true);
+		});
 	}
 
 	// Use this to group labels and nested inputs. But we don't nest inputs yet.
