4807d716cf0b1f3a26f93c22de3de93dd057a3e5
Oskar Rough <oskar@rough.dk>
2017-01-13 18:31:52 +0100

    Fix indention in auth route

--- app/auth/route.js
+++ app/auth/route.js
@@ -3,37 +3,37 @@ import Ember from 'ember';
 const {Route, get, inject} = Ember;
 
 export default Route.extend({
-    flashMessages: inject.service(),
-    onLoginError(data) {
-	get(this, 'flashMessages').info(data.message);
-	// auth/invalid-email
-	// auth/user-disabled
-	// auth/user-not-found
-	// auth/wrong-password
-    },
-    actions: {
-	login(data) {
-	    console.log(data, "data auth route");
-
-	    const flashMessages = get(this, 'flashMessages');
-
-	    get(this, 'session').open('firebase', data).then(() => {
-		flashMessages.info('You are now signed in!');
-		// get or create user either in utils/get... or here
-		// .then( redirect
-		this.send('redirectAfterAuth');
-	    }).catch(error => {
-		this.onLoginError(error)
-	    });
+	flashMessages: inject.service(),
+	onLoginError(data) {
+		get(this, 'flashMessages').info(data.message);
+		// auth/invalid-email
+		// auth/user-disabled
+		// auth/user-not-found
+		// auth/wrong-password
 	},
-	redirectAfterAuth() {
-	    return get(this, 'session.currentUser.channels').then(channels => {
-		const userChannel = get(channels, 'firstObject');
-		if (userChannel) {
-		    return this.transitionTo('channel', userChannel);
+	actions: {
+		login(data) {
+			console.log(data, "data auth route");
+
+			const flashMessages = get(this, 'flashMessages');
+
+			get(this, 'session').open('firebase', data).then(() => {
+				flashMessages.info('You are now signed in!');
+				// get or create user either in utils/get... or here
+				// .then( redirect
+				this.send('redirectAfterAuth');
+			}).catch(error => {
+				this.onLoginError(error)
+			});
+		},
+		redirectAfterAuth() {
+			return get(this, 'session.currentUser.channels').then(channels => {
+				const userChannel = get(channels, 'firstObject');
+				if (userChannel) {
+					return this.transitionTo('channel', userChannel);
+				}
+				return this.transitionTo('channels.new');
+			});
 		}
-		return this.transitionTo('channels.new');
-	    });
 	}
-    }
 });
