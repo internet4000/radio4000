46314b7f57b750cb96c43fb837b7305f3252e8cd
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-01-13 10:27:55 +0100

    fix and improve login page if already authed

--- app/auth/login/route.js
+++ app/auth/login/route.js
@@ -3,21 +3,21 @@ import Ember from 'ember';
 const {Route, get} = Ember;
 
 export default Route.extend({
-		beforeModel() {
-				if (get(this, 'session.isAuthenticated')) {
-						return this.send('redirectAfterAuth');
-				}
-		},
+    beforeModel(transition) {
+	if (get(this, 'session.isAuthenticated')) {
+	    return transition.send('redirectAfterAuth');
+	}
+    },
 
-		actions: {
-				redirectAfterAuth() {
-						return get(this, 'session.currentUser.channels').then(channels => {
-								const userChannel = get(channels, 'firstObject');
-								if (userChannel) {
-										return this.transitionTo('channel', userChannel);
-								}
-								return this.transitionTo('channels.new');
-						});
-				}
+    actions: {
+	redirectAfterAuth() {
+	    return get(this, 'session.currentUser.channels').then(channels => {
+		const userChannel = get(channels, 'firstObject');
+		if (userChannel) {
+		    return this.transitionTo('channel', userChannel);
 		}
+		return this.transitionTo('channels.new');
+	    });
+	}
+    }
 });
