a74ffbba4f52362cb989121e786ecfc95b9f73b1
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-01-13 09:58:53 +0100

    move login/logout/signup under /auth/

---
+++ tests/unit/auth/login/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:auth/login', 'Unit | Route | auth/login', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
