20da3509555cb1ef54752e6d75774e80e760ac69
Oskar Rough <oskar@rough.dk>
2017-01-16 12:51:33 +0100

    Add ‘forgot password’ button

--- app/components/user-login/component.js
+++ app/components/user-login/component.js
@@ -1,11 +1,24 @@
 import Ember from 'ember';
 
-const {Component, get} = Ember;
+const {Component, get, inject} = Ember;
 
 export default Component.extend({
+	firebaseApp: inject.service(),
+
 	actions: {
 		login(provider, email, password) {
 			get(this, 'onLogin')(provider, email, password);
+		},
+
+		resetPassword(email) {
+			const auth = get(this, 'firebaseApp').auth();
+			auth.sendPasswordResetEmail(email)
+				.then(() => {
+					get(this, 'notifications').success('Password reset. Check your email.', {
+						autoClear: false
+					});
+				})
+				.catch(err => Ember.debug(err));
 		}
 	}
 });
