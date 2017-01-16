377349cd2426cb314d09475a91aa9934c5906930
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-01-16 12:51:10 +0100

    Fix login not bubbling up

--- app/auth/login/controller.js
+++ app/auth/login/controller.js
@@ -6,9 +6,11 @@ export default Controller.extend({
 	actions: {
 		// Logs in a user. Provider has to match what we've enabled in Firebase authentication.
 		// That is: 'password' or 'google' or 'facebook'
+		catchLogin(provider, email, password) {
+			this.send("login", provider, email, password);
+		},
 		login(provider, email, password) {
 			console.log( "auth.login.controller@login" );
-
 			return true;
 		}
 	}
