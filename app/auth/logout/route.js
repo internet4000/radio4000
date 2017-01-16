c71a28340a2af9a513f32d8a65ccc5a8e49e1827
Oskar Rough <oskar@rough.dk>
2017-01-16 13:34:48 +0100

    Fix firebase permissions error when signing out

--- app/auth/logout/route.js
+++ app/auth/logout/route.js
@@ -8,11 +8,14 @@ export default Ember.Route.extend({
 
 		// If we do not unload the user model, Firebase will warn about permissions.
 		const user = get(this, 'session.currentUser');
-		user.unloadRecord();
+		user.get('settings').then(settings => {
+			settings.unloadRecord();
+			user.unloadRecord();
 
-		get(this, 'session').close().then(() => {
-			flashMessages.warning(`You have been signed out`);
-			this.transitionTo('auth.login');
+			get(this, 'session').close().then(() => {
+				flashMessages.warning(`You have been signed out`);
+				this.transitionTo('auth.login');
+			});
 		});
 	}
 });
