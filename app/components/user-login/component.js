85685408371dabd9bb9c67268232005b0c48cb2c
Oskar Rough <oskar@rough.dk>
2017-01-24 22:05:14 +0100

    Refactor

--- app/components/user-login/component.js
+++ app/components/user-login/component.js
@@ -12,18 +12,17 @@ export default Component.extend({
 
 		resetPassword(email) {
 			const auth = get(this, 'firebaseApp').auth();
+			const messages = get(this, 'flashMessages');
 
 			if (!email) {
-				get(this, 'flashMessages').warning('Enter your e-mail address to reset your email.');
+				messages.warning('Enter your e-mail address to reset your email.');
 			}
 
-			auth.sendPasswordResetEmail(email)
-				.then(() => {
-					get(this, 'flashMessages').success('Password reset. Check your email.', {
-						autoClear: false
-					});
-				})
-				.catch(err => Ember.debug(err));
+			auth.sendPasswordResetEmail(email).then(() => {
+				messages.success('Password reset. Check your email.', {
+					autoClear: false
+				});
+			}).catch(err => Ember.debug(err));
 		}
 	}
 });
