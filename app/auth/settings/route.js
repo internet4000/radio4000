a23412af47842e2fcb7a530a1073f7e7977bc38c
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-01-26 10:21:23 +0100

    authenticated route for auth/settings

--- app/auth/settings/route.js
+++ app/auth/settings/route.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import authenticatedRouteMixin from 'radio4000/mixins/authenticated-route';
 
 const {Route} = Ember;
 
-export default Route.extend({
+export default Route.extend(authenticatedRouteMixin, {
 });
