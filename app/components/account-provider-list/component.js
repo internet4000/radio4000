f5acc3d7a70b596a5825f1e84649ebcc12fb455d
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-01-26 23:09:41 +0100

    Improved accounts UX, hide linked already

--- app/components/account-provider-list/component.js
+++ app/components/account-provider-list/component.js
@@ -5,12 +5,10 @@ const {Component, computed} = Ember;
 export default Component.extend({
 	classNames: ['ProviderAccounts'],
 	moreThanOneAccount: computed.gt('providerData.length', 1),
+	onlyOneAccount: computed.equal('providerData.length', 1),
 	actions: {
 		unlink(providerId) {
-			console.log( providerId );
-
 			this.get('unlink')(providerId);
-
 		}
-}
+	}
 });
