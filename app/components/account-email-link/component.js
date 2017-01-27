8433e8aaec5a665ef9353981ab25e841908f0f0a
Oskar Rough <oskar@rough.dk>
2017-01-27 13:57:27 +0100

    Style account-email-link like the other providers

--- app/components/account-email-link/component.js
+++ app/components/account-email-link/component.js
@@ -1,10 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	classNames: ['ProviderAccount'],
-	actions: {
-		link(email, password) {
-			this.get('link')(email, password);
-		}
-}
+
 });
