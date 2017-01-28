3a8600264d085ce93ed7887c63841ec183c9590f
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-01-28 17:29:47 +0100

    intro under about

--- app/router.js
+++ app/router.js
@@ -4,48 +4,49 @@ import config from './config/environment';
 import googlePageview from './mixins/google-pageview';
 
 const Router = Ember.Router.extend(googlePageview, {
-	location: config.locationType,
-	rootURL: config.rootURL
+    location: config.locationType,
+    rootURL: config.rootURL
 });
 
 Router.map(function () {
-	this.route('about', function () {
-		this.route('intro');
-		this.route('contact');
-		this.route('technology');
-	});
-	this.route('dashboard');
-	this.route('help');
-	this.route('404');
-	this.route('styleguide', function () {
-		this.route('typography');
-		this.route('colors');
-		this.route('forms');
-		this.route('tabs');
-	});
-	this.route('login');
-	this.route('logout');
-	this.route('channels', {path: '/'}, function () {
-		this.route('all');
-		this.route('new');
-		this.authenticatedRoute('history');
-	});
-	this.route('channel', {path: '/:channel_slug'}, function () {
-		this.authenticatedRoute('add');
-		this.authenticatedRoute('edit');
-		this.authenticatedRoute('delete');
-		this.route('dashboard', {});
-		this.route('index', {path: '/'}, function () {
-			this.route('track', {path: ':track_id', resetNamespace: true});
-		});
-		this.route('favorites');
-		this.route('followers');
-	});
-	this.route('feedback');
-	this.authenticatedRoute('add');
-	this.authenticatedRoute('bookmarklet');
-	// Support deprecated channels urls. Needs to be defined before 'channel' route.
-	this.route('channel-alias', {path: '/c/:channel_slug'});
+  this.route('about', function () {
+    this.route('intro', {path: '/'});
+    this.route('contact');
+    this.route('technology');
+    this.route('project');
+  });
+  this.route('dashboard');
+  this.route('help');
+  this.route('404');
+  this.route('styleguide', function () {
+      this.route('typography');
+      this.route('colors');
+      this.route('forms');
+      this.route('tabs');
+  });
+  this.route('login');
+  this.route('logout');
+  this.route('channels', {path: '/'}, function () {
+      this.route('all');
+      this.route('new');
+      this.authenticatedRoute('history');
+  });
+  this.route('channel', {path: '/:channel_slug'}, function () {
+      this.authenticatedRoute('add');
+      this.authenticatedRoute('edit');
+      this.authenticatedRoute('delete');
+      this.route('dashboard', {});
+      this.route('index', {path: '/'}, function () {
+          this.route('track', {path: ':track_id', resetNamespace: true});
+      });
+      this.route('favorites');
+      this.route('followers');
+  });
+  this.route('feedback');
+  this.authenticatedRoute('add');
+  this.authenticatedRoute('bookmarklet');
+  // Support deprecated channels urls. Needs to be defined before 'channel' route.
+  this.route('channel-alias', {path: '/c/:channel_slug'});
 });
 
 export default Router;
