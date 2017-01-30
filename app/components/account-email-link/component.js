2f2e523612eabe7a793c3a46aa30e1dd0b6be1c4
Oskar Rough <oskar@rough.dk>
2017-01-30 11:19:36 +0100

    Show e-mail inputs after add

--- app/components/account-email-link/component.js
+++ app/components/account-email-link/component.js
@@ -1,10 +1,15 @@
 import Ember from 'ember';
 
-const {Component, get} = Ember;
+const {Component, get, set} = Ember;
 
 export default Component.extend({
+	showInputs: false,
 	actions: {
 		link(email, password) {
+			if (!get(this, 'showInputs')) {
+				set(this, 'showInputs', true);
+				return;
+			}
 			get(this, 'onLink')(email, password);
 		}
 	}
