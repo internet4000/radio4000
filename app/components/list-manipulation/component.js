c320c23c7da566577d0f2d319bba36124da0bb3d
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-02-10 17:10:09 +0100

    WIP still not working

--- app/components/list-manipulation/component.js
+++ app/components/list-manipulation/component.js
@@ -3,24 +3,7 @@ import Ember from 'ember';
 const {Component, computed} = Ember;
 
 export default Component.extend({
-	// items: null,
-	items: [
-		{
-			id: 1,
-			title: 1,
-			get: function () {return this.id}
-		},
-		{
-			id: 2,
-			title: 2,
-			get: function () {return this.id}
-		},
-		{
-			id: 3,
-			title: 3,
-			get: function () {return this.id}
-		}
-	],
+	items: null,
 	manipulatedItems: computed('items', function() {
 		return this.get('items').reverseObjects();
 	})
