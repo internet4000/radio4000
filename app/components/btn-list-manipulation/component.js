2960fbda7539182038ce644870191fe2a49128eb
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-02-11 22:44:48 +0100

    refactor list-manipulation api

--- app/components/btn-list-manipulation/component.js
+++ app/components/btn-list-manipulation/component.js
@@ -2,13 +2,13 @@ import Ember from 'ember';
 
 const {Component, get} = Ember;
 
+// setKey (key:string, toggle:boolean)
+
 export default Component.extend({
 	tagName: ['Button'],
 	classNames: ['Btn'],
 	toggle: false,
 	click() {
-		// NOTE: the key is automatically passed as 1st argument
-		// here toggle is the second param to parent component action
-		get(this, 'setKey')(get(this, 'toggle'));
+		get(this, 'setKey')();
 	}
 });
--- app/components/list-manipulation/component.js
+++ app/components/list-manipulation/component.js
@@ -1,39 +1,54 @@
 import Ember from 'ember';
 
-const {Component, computed, set, get} = Ember;
+const {Component, computed, set, setProperties, get} = Ember;
 
 export default Component.extend({
 	list: null,
 	label: 'Sort by',
 	sortKey: 'updated',
-	sortDirection: 'desc',
+	sortDesc: 'true',
 
-	sortDefinition: computed('sortKey', 'sortDirection', function () {
-		return [].addObject(get(this, 'sortKey') + ':' + get(this, 'sortDirection'));
-	}),
 	manipulatedList: computed.sort('list', 'sortDefinition'),
 
+	sortDefinition: computed('sortKey', 'sortDesc', function () {
+		let sortDirection;
+		let sortKey = get(this, 'sortKey');
+
+		if(get(this, 'sortDesc')) {
+			sortDirection = 'desc';
+		} else {
+			sortDirection = 'asc';
+		}
+
+		return [`${sortKey}:${sortDirection}`];
+	}),
+
 	setSortKey(key) {
-		set(this, 'sortKey', key);
+		this.setProperties({
+			'sortKey': key,
+			'sortDesc': true
+		});
 	},
 	toggleSetSortKey(key) {
 		if (get(this, 'sortKey') === key) {
-			return this.toggleSortDirection();
+			this.toggleSortDirection();
+			return;
 		}
 		this.setSortKey(key);
 	},
 	toggleSortDirection() {
-		if (get(this, 'sortDirection') === 'asc') {
-			set(this, 'sortDirection', 'desc');
+		if (get(this, 'sortDesc')) {
+			set(this, 'sortDesc', false);
 			return;
 		}
-		set(this, 'sortDirection', 'asc');
+		set(this, 'sortDesc', true);
 	},
 	actions: {
 		// one action decides what to do, if toggle=true
 		setSortKey(key, toggle) {
 			if(toggle) {
-				return this.toggleSetSortKey(key);
+				this.toggleSetSortKey(key);
+				return;
 			}
 			this.setSortKey(key);
 		}
