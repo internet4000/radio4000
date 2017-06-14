b691c4d12f0accfea3a69d7c73ddf42b750134e7
Oskar Rough <oskar@rough.dk>
2017-06-14 21:59:27 +0200

    Add search with ember-jets

--- app/channels/all/controller.js
+++ app/channels/all/controller.js
@@ -1,31 +1,13 @@
 import Ember from 'ember';
 
-const {Controller, get, set, computed} = Ember;
+const {Controller, computed} = Ember;
 
 export default Controller.extend({
-	search: '',
 	queryParams: ['search'],
-	isList: false,
+	search: '',
 	sortKey: 'created',
 	sortDirection: 'desc',
-	searchResults: null,
 
 	// Only show channels that have tracks.
-	filteredChannels: computed.filter('model', m => m.get('totalTracks')),
-
-	// Either show filtered or search-result channels.
-	channels: computed('filteredChannels', 'searchResults', function () {
-		let filtered = get(this, 'filteredChannels');
-		let searchResults = get(this, 'searchResults');
-		return searchResults ? searchResults : filtered;
-	}),
-
-	actions: {
-		changeLayout() {
-			this.toggleProperty('isList');
-		},
-		handleSearch(searchResults) {
-			set(this, 'searchResults', searchResults);
-		}
-	}
+	channels: computed.filter('model', m => m.get('totalTracks'))
 });
