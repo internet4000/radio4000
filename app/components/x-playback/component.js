c885b77c84193c0a5ed04ff0bda7c2059c773a8b
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-06-15 20:45:50 +0200

    remove some stuff

--- app/components/x-playback/component.js
+++ app/components/x-playback/component.js
@@ -10,28 +10,8 @@ export default Component.extend(EKMixin, {
 	uiStates: inject.service(),
 	classNames: ['Playback'],
 	channel: computed.alias('player.model.channel'),
+	track: computed.alias('player.model'),
 
-	youtubePlayerVars: {
-		autoplay: 1,
-		showinfo: 0
-	},
-
-	activateKeyboard: on('init', function () {
-		set(this, 'keyboardActivated', true);
-	}),
-	swapShortcut: on(keyDown('KeyW'), function () {
-		console.log('swap shortcut');
-		get(this, 'swap').perform();
-	}),
-	playbackShortcut: on(keyDown('KeyP'), function () {
-		this.send('togglePlay');
-	}),
-	skipShortcut: on(keyDown('KeyS'), function () {
-		this.send('next');
-	}),
-	muteShortcut: on(keyDown('KeyM'), function () {
-		this.send('toggleVolume');
-	}),
 	cycleFormat: on(keyDown('KeyF'), function () {
 		get(this, 'uiStates').cycleFormat();
 	}),
@@ -39,69 +19,12 @@ export default Component.extend(EKMixin, {
 		set(this, 'uiStates.format', 1);
 	}),
 
-	swap: task(function * () {
-		const previous = get(this, 'player.playlist');
-		const channel = yield get(this, 'bot.playAnotherRadio').perform(previous);
-		return channel;
-	}).keepLatest(),
-
 	actions: {
-		togglePlay() {
-			get(this, 'emberYoutube').send('togglePlay');
-		},
-		toggleVolume() {
-			get(this, 'emberYoutube').send('toggleVolume');
-		},
-		play() {
-			get(this, 'emberYoutube').send('play');
-		},
-		pause() {
-			get(this, 'emberYoutube').send('pause');
-		},
-		prev() {
-			get(this, 'player').prev();
-		},
-		next() {
-			get(this, 'player').next();
-		},
-		toggleRandom(player = this.get('player')) {
-			if (player.get('isRandom')) {
-				player.deactivateRandom();
-			} else {
-				player.activateRandom();
-			}
-		},
-		scrollToTrack() {
-			// We wrap the actual scrolling inside this run loop,
-			// to minimize jank. Seems to work better.
-			run.scheduleOnce('afterRender', () => {
-				run.later(() => {
-					const $container = $('.html, body');
-					const offset = $('.Track.is-current').offset().top - 90;
-					$container.animate({scrollTop: offset}, 700, 'swing');
-				}, 60);
-			});
-		},
-
 		toggleMinimizedFormat() {
 			get(this, 'uiStates').toggleMinimizedFormat();
 		},
 		toggleFullscreenFormat() {
 			get(this, 'uiStates').toggleFullscreenFormat();
-		},
-
-		// ember-youtube events
-		onYouTubePlay() {
-			get(this, 'player').play();
-		},
-		onYouTubePause() {
-			get(this, 'player').pause();
-		},
-		onYouTubeEnd() {
-			get(this, 'player').trackEnded();
-		},
-		onYouTubeError(error) {
-			get(this, 'player').onError(error);
 		}
 	}
 });
--- app/services/player.js
+++ app/services/player.js
@@ -4,13 +4,25 @@ import Ember from 'ember';
 const {debug, inject} = Ember;
 
 export default Ember.Service.extend({
-	playerRandom: inject.service(),
-	playerHistory: inject.service(),
-	model: null,
-	playlist: null,
-	isLooped: true,
-	isRandom: false,
-	isPlaying: false,
+
+	// Give it a track model and it'll play it
+	playTrack(model) {
+		if (!model) {
+			debug('playTrack() was called without a track.');
+			return false;
+		}
+
+		document.querySelector('radio4000-player').trackId = model.get('id');
+
+		this.set('isPlaying', true);
+		model.get('channel').then(channel => {
+			const trackTitle = model.get('title');
+			const channelTitle = channel.get('title');
+			this.updateMetaTitle(trackTitle, channelTitle);
+			this.updatePlaylist(channel);
+			// radio4000-player-vue
+		});
+	},
 
 	// This caches the current playlist and sets it if it really did change (through the model)
 	// also sets the old one as inactive, and new as… active!
@@ -29,162 +41,6 @@ export default Ember.Service.extend({
 		newPlaylist.get('tracks').then(tracks => {
 			this.set('playlist', newPlaylist);
 			this.get('playerRandom').setNewRandomPool(tracks);
-		});
-	},
-
-	// Play/pause activations for the current track in player (and metrics).
-	play(currentTrack = this.get('model')) {
-		this.set('isPlaying', true);
-		this.get('playerHistory').setTrackAsPlayed(currentTrack);
-	},
-
-	pause() {
-		this.set('isPlaying', false);
-	},
-
-	// Give it a track model and it'll play it
-	playTrack(model) {
-		if (!model) {
-			debug('playTrack() was called without a track.');
-			return false;
-		}
-
-		// const player = document.querySelector('radio4000-player');
-		// player.trackId = model.get('id');
-		// player.channelId = model.get('channel');
-		this.setProperties({model, isPlaying: true});
-		this.set('isPlaying', true);
-		model.get('channel').then(channel => {
-			const trackTitle = model.get('title');
-			const channelTitle = channel.get('title');
-			this.updateMetaTitle(trackTitle, channelTitle);
-			this.updatePlaylist(channel);
-			// radio4000-player-vue
-		});
-	},
-
-	/**
-		prev
-		decides which track to play
-		depends on the active play mode
-	*/
-	prev() {
-		const isRandom = this.get('isRandom');
-		if (isRandom) {
-			return this.prevRandom();
-		}
-		return this.prevNormal();
-	},
-
-	prevNormal() {
-		const playlist = this.get('playlist');
-		const prev = this.getPrev();
-		if (!prev) {
-			this.get('playerHistory').clearPlayerHistory();
-			// first is last because we have newest on top
-			return this.playTrack(playlist.get('tracks.firstObject'));
-		}
-		return this.playTrack(prev);
-	},
-
-	prevRandom() {
-		this.get('playerRandom').getPrevious().then(prev => {
-			return this.playTrack(prev);
-		});
-	},
-
-	/**
-		next
-		decide which next track is going to play
-		depends on the active play mode
-	*/
-	next() {
-		if (this.get('isRandom')) {
-			return this.nextRandom();
-		}
-		return this.nextNormal();
-	},
-
-	nextNormal() {
-		const playlist = this.get('playlist');
-		const next = this.getNext();
-		if (!next) {
-			this.get('playerHistory').clearPlayerHistory();
-			// first is last because we have newest on top
-			return this.playTrack(playlist.get('tracks.lastObject'));
-		}
-		return this.playTrack(next);
-	},
-
-	nextRandom() {
-		this.get('playerRandom').getNext().then(nextRandom => {
-			return this.playTrack(nextRandom);
-		});
-	},
-
-	// Find out which actual item has to be played
-	getNext(array = this.get('playlist.tracks')) {
-		return array.objectAt(array.indexOf(this.get('model')) - 1);
-	},
-
-	getPrev(array = this.get('playlist.tracks')) {
-		return array.objectAt(array.indexOf(this.get('model')) + 1);
-	},
-
-	/**
-		On YouTube player error
-	*/
-	onError(error) {
-		this.set('isPlaying', false);
-		debug(error);
-		if (error === 2) {
-			// dont do anything on 'invalid parameter'
-			return;
-		}
-		if (error === 150) {
-			debug('error150: georestricted track');
-		}
-		// otherwise skip to next
-		this.next();
-	},
-
-	/**
-		A track ended naturally
-		Application route called this.
-		A track from the player ended, without user action. It played naturally untill the end
-	*/
-	trackEnded(finishedTrack = this.get('model')) {
-		this.set('isPlaying', false);
-		// mark this track has finished
-		this.get('playerHistory').trackEnded(finishedTrack);
-		// play next track
-		return this.next();
-	},
-
-	/**
-		@method activateRandom
-		Random was activated
-		from clicking on shuffle in playback
-		@TODO from shuffling on a channel card
-		1- visualy clear played tracks in the current channel
-		2- set pool of tracks to be used
-		and return it so we can use it as a promise
-	 */
-	activateRandom() {
-		debug('activateRandom');
-		this.set('isRandom', true);
-		this.get('playerHistory').clearPlayerHistory();
-		return this.updateRandomPoolFromPlaylist();
-	},
-
-	deactivateRandom() {
-		debug('deactivateRandom');
-		this.set('isRandom', false);
-	},
-
-	updateRandomPoolFromPlaylist() {
-		return this.get('playlist.tracks').then(items => {
-			this.get('playerRandom').setNewRandomPool(items);
 		});
 	},
 
