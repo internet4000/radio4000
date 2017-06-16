280546ddf7153eac2e4ec962e526aa33366b17c7
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-06-16 15:08:32 +0200

    add W shortcut to play random channel

---
+++ app/components/play-random-channel/component.js
@@ -0,0 +1,33 @@
+import Ember from 'ember';
+const {Component, inject, get, on} = Ember;
+import { getRandomIndex } from 'radio4000/utils/random-helpers';
+import { EKMixin, keyUp } from 'ember-keyboard';
+
+
+export default Component.extend(EKMixin, {
+	store: inject.service(),
+	player: inject.service(),
+
+	bindKeyboard: on(keyUp('KeyW'), function() {
+		this.playRandomChannel();
+	}),
+
+	click() {
+		this.playRandomChannel();
+	},
+
+	playRandomChannel() {
+		const store = this.get('store')
+		store.findAll('channel').then(data => {
+			const channel = data.objectAt(getRandomIndex(data.content));
+			channel.get('tracks').then(tracks => {
+				if(tracks.length < 2) {
+					this.playRandomChannel();
+				} else {
+					get(this, 'player').playTrack(tracks.get('lastObject'));
+				}
+			})
+		})
+	}
+
+});
