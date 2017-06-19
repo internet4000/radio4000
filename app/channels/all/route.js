11db666f824b783b58d440537e9f0af9ce7de193
Oskar Rough <oskar@rough.dk>
2017-06-19 22:33:51 +0200

    Block the UI less on /all

--- app/channels/all/route.js
+++ app/channels/all/route.js
@@ -4,74 +4,77 @@ const {Route, get} = Ember;
 
 // Set to either 'limitToLast' or 'limitToFirst' to
 // find items from newest or oldest.
-const LIMIT_TO_DYNAMIC = 'limitToLast';
-const PAGE_SIZE = 15;
+// const LIMIT_TO_DYNAMIC = 'limitToLast';
+// const PAGE_SIZE = 15;
 
 export default Route.extend({
-	startAt: null,
-	endAt: null,
+	// startAt: null,
+	// endAt: null,
+	didLoadAll: false,
 
 	model() {
 		const store = this.store;
-		return store.findAll('channel'); // all
-		// return store.peekAll('channel'); // cached
-		// return store.find('channel', {orderBy: 'published', limitToLast: 5})
-		// return store.query('channel', {orderBy: 'category', startAt: 1, endAt: 1})
-		// return store.query('channel', {limitToLast: 10}); // oldest
-		// return store.query('channel', {limitToFirst: 10}); // newest
-		// return store.query('channel', {startAt: '-J_Gj6nryBGVLHrmfZ10'});
-	},
 
-	findPrev() {
-		const model = this.get('currentModel');
-		let id;
-		this.set('startAt', null);
-		if (model) {
-			if (this.get('endAt')) {
-				id = get(model, 'lastObject.id');
-			} else {
-				id = get(model, 'firstObject.id');
-			}
-			this.set('endAt', id);
+		// Load all channels but don't return them,
+		// to not block the UI. We only need this once per session.
+		if (!get(this, 'didLoadAll')) {
+			store.findAll('channel'); // all
+			this.set('didLoadAll', true)
 		}
-		return this.findPaginated();
-	},
 
-	findNext() {
-		const model = this.get('currentModel');
-		const id = get(model, 'lastObject.id');
-		this.set('startAt', id);
-		this.set('endAt', null);
-		this.findPaginated();
-	},
+		return store.peekAll('channel'); // cached
+	}
+
+	// findPrev() {
+	// 	const model = this.get('currentModel');
+	// 	let id;
+	// 	this.set('startAt', null);
+	// 	if (model) {
+	// 		if (this.get('endAt')) {
+	// 			id = get(model, 'lastObject.id');
+	// 		} else {
+	// 			id = get(model, 'firstObject.id');
+	// 		}
+	// 		this.set('endAt', id);
+	// 	}
+	// 	return this.findPaginated();
+	// },
+
+	// findNext() {
+	// 	const model = this.get('currentModel');
+	// 	const id = get(model, 'lastObject.id');
+	// 	this.set('startAt', id);
+	// 	this.set('endAt', null);
+	// 	this.findPaginated();
+	// },
 
 	// Constructs an emerfire query supporting pagination.
 	// Inspired by https://github.com/firebase/emberfire/issues/248
-	findPaginated() {
-		let startAt = this.get('startAt');
-		let endAt = this.get('endAt');
-		let query = {};
-		query[LIMIT_TO_DYNAMIC] = PAGE_SIZE + 1;
+	// findPaginated() {
+	// 	let startAt = this.get('startAt');
+	// 	let endAt = this.get('endAt');
+	// 	let query = {};
+	// 	query[LIMIT_TO_DYNAMIC] = PAGE_SIZE + 1;
 
-		if (startAt) {
-			query.startAt = startAt;
-		}
+	// 	if (startAt) {
+	// 		query.startAt = startAt;
+	// 	}
 
-		if (endAt) {
-			query.endAt = endAt;
-			query[LIMIT_TO_DYNAMIC] = PAGE_SIZE + 1;
-		}
+	// 	if (endAt) {
+	// 		query.endAt = endAt;
+	// 		query[LIMIT_TO_DYNAMIC] = PAGE_SIZE + 1;
+	// 	}
 
-		return this.store.query('channel', query);
-	},
+	// 	return this.store.query('channel', query);
+	// },
 
-	actions: {
-		prev() {
-			this.findPrev();
-		},
-		next() {
-			this.findNext();
-		}
-	}
+	// actions: {
+	// 	prev() {
+	// 		this.findPrev();
+	// 	},
+	// 	next() {
+	// 		this.findNext();
+	// 	}
+	// }
 });
 
