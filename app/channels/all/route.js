7e590d2d69d2cf39f47467f5c4042715f506e910
Oskar Rough <oskar@rough.dk>
2017-06-21 09:46:41 +0200

    Try out normal list

--- app/channels/all/route.js
+++ app/channels/all/route.js
@@ -3,15 +3,15 @@ import Ember from 'ember';
 const {Route, get, set} = Ember;
 
 export default Route.extend({
-	didLoadAll: false,
+	didFindAll: false,
 
 	model() {
 		const store = this.store;
 		// Load all channels but don't return (block rendering)
 		// Only needed once.
-		if (!get(this, 'didLoadAll')) {
-			store.findAll('channel');
-			set(this, 'didLoadAll', true);
+		if (!get(this, 'didFindAll')) {
+			return store.findAll('channel');
+			set(this, 'didFindAll', true);
 		}
 		// Return cached channels, which is instant.
 		return store.peekAll('channel');
