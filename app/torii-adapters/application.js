824ca8fcc3aa9cbf695acc0fcf7c2e45844799b7
Oskar Rough <oskar@rough.dk>
2017-06-30 11:50:36 +0200

    Fix "id already in use" error when creating a new user

--- app/torii-adapters/application.js
+++ app/torii-adapters/application.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import ToriiFirebaseAdapter from 'emberfire/torii-adapters/firebase';
 
-const {debug, inject, RSVP} = Ember;
+const {debug, inject, RSVP, run} = Ember;
 
 export default ToriiFirebaseAdapter.extend({
 	store: inject.service(),
@@ -10,11 +10,10 @@ export default ToriiFirebaseAdapter.extend({
 	open(user) {
 		this._super(user);
 		let provider = this.extractProviderId_(user);
-		let providerIsPassword = (provider === 'password');
 
 		// reject login and send email is loging with email && not verified
 		return new RSVP.Promise((resolve, reject) => {
-			if (!user.emailVerified && providerIsPassword) {
+			if (!user.emailVerified && provider === 'password') {
 				let err = {
 					code: 'auth/email-not-verified',
 					message: 'Before you can log in, first verify your email address. Check your inbox.'
@@ -35,6 +34,8 @@ export default ToriiFirebaseAdapter.extend({
 					// not an ember model `user` like in this case.
 					currentUser: userModel
 				});
+			}).catch(err => {
+				debug('could not get or create user', err);
 			});
 		});
 	},
@@ -48,11 +49,15 @@ export default ToriiFirebaseAdapter.extend({
 		return new RSVP.Promise(resolve => {
 			store.findRecord('user', id).then(user => {
 				resolve(user);
-			}).catch(error => {
-				debug(error);
-				const newUser = store.createRecord('user', {id});
-				newUser.save().then(() => {
-					resolve(newUser);
+			}).catch(() => {
+				store.recordForId('user', id).unloadRecord();
+				// Unloading a record does not happen immediately,
+				// so we wrap this in a run loop.
+				run.next(() => {
+					const newUser = store.createRecord('user', {id});
+					newUser.save().then(() => {
+						resolve(newUser);
+					});
 				});
 			});
 		});
