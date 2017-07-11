5d4e1f5a699affcba587e74fd85f784cea958089
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-07-11 23:20:37 +0200

    remove test

--- tests/integration/components/feedback-form-google-spreadsheet/component-test.js
+++ tests/integration/components/feedback-form-google-spreadsheet/component-test.js
@@ -1,11 +1,5 @@
-import {moduleForComponent, test} from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import {moduleForComponent} from 'ember-qunit';
 
 moduleForComponent('feedback-form-google-spreadsheet', 'Integration | Component | feedback form google spreadsheet', {
 	integration: true
-});
-
-test('it renders', function (assert) {
-  this.render(hbs`{{feedback-form-google-spreadsheet}}`);
-  assert.equal(this.$().text().trim(), '');
 });
