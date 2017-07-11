354414583b54ec4827fd9c2c86ff25ad68c2fe47
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-07-11 23:04:26 +0200

    lint/fix failing test

--- tests/integration/components/feedback-form-google-spreadsheet/component-test.js
+++ tests/integration/components/feedback-form-google-spreadsheet/component-test.js
@@ -1,25 +1,11 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import {moduleForComponent, test} from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('feedback-form-google-spreadsheet', 'Integration | Component | feedback form google spreadsheet', {
-  integration: true
+	integration: true
 });
 
-test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
+test('it renders', function (assert) {
   this.render(hbs`{{feedback-form-google-spreadsheet}}`);
-
   assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#feedback-form-google-spreadsheet}}
-      template block text
-    {{/feedback-form-google-spreadsheet}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });
