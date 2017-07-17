9c18ef20fd21284e5f5213b5fc507f8ca495d3ad
hugurp <hugurp@users.noreply.github.com>
2017-07-17 10:27:45 +0200

    Feature/group tracks (#30)
    * new group-by util
    
    * add groupBy to tracks in list
    
    * style rendered grouped tracks
    
    * lint
    
    * add credits
    
    * fix naming conflict
    
    * Avoid nested ol>ol
    
    * Style the dates a bit more in the background

--- app/components/tracks-list/component.js
+++ app/components/tracks-list/component.js
@@ -1,12 +1,15 @@
 import Ember from 'ember';
+import groupBy from 'radio4000/utils/group-by';
 
 const {Component, computed} = Ember;
 
 export default Component.extend({
+	classNames: ['Tracks'],
 	items: null,
 	numbered: false,
 
 	// sorts our items newest on top
 	sortKeys: ['created:desc'],
-	sortedItems: computed.sort('items', 'sortKeys')
+	sortedItems: computed.sort('items', 'sortKeys'),
+	groupedItems: groupBy('sortedItems', 'createdMonth')
 });
