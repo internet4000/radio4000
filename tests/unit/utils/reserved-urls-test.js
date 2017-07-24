05401ea7d539ddc813cd92f11e75a743a08a112e
Oskar Rough <oskar@rough.dk>
2017-07-24 16:27:52 +0300

    Lint

--- tests/unit/utils/reserved-urls-test.js
+++ tests/unit/utils/reserved-urls-test.js
@@ -1,12 +1,12 @@
 import reservedUrls from 'radio4000/utils/reserved-urls';
-import { module, test } from 'qunit';
+import {module, test} from 'qunit';
 
 module('Unit | Utility | reserved urls');
 
-test('it works', function(assert) {
-  assert.ok(reservedUrls.length > 1);
-  assert.ok(reservedUrls.includes('blog'));
-  assert.ok(reservedUrls.includes('help'));
-  assert.ok(reservedUrls.includes('bookmarklet'));
-  assert.ok(reservedUrls.includes('intro'));
+test('it works', function (assert) {
+	assert.ok(reservedUrls.length > 1);
+	assert.ok(reservedUrls.includes('blog'));
+	assert.ok(reservedUrls.includes('help'));
+	assert.ok(reservedUrls.includes('bookmarklet'));
+	assert.ok(reservedUrls.includes('intro'));
 });
