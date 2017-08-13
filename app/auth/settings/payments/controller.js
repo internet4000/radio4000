006103c8f9c84bb6a91a5a1b63c1816b4dcff91e
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-08-13 09:58:35 +0200

    why you no work

--- app/auth/settings/payments/controller.js
+++ app/auth/settings/payments/controller.js
@@ -6,6 +6,13 @@ export default Controller.extend({
 	actions: {
 		processStripeToken(token, args) {
 			console.log('token & args', token, args);
+			/* $.ajax({
+			 *   type: 'POST',
+			 *   url: 'http://localhost:3000/payments',
+			 *   data: { name: "John", location: "Boston" }
+			 * }).then(answer => {
+				 console.log('answer', answer)
+				 })*/
 		}
 	}
 });
