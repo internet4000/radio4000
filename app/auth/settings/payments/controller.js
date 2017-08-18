92bd794fb4697e340c325600b0f4a27e112e96f4
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-08-18 21:19:38 +0200

    send card to backend

--- app/auth/settings/payments/controller.js
+++ app/auth/settings/payments/controller.js
@@ -5,15 +5,38 @@ const { Controller, computed } = Ember;
 export default Controller.extend({
 	userChannel: computed.alias('session.currentUser.channels.firstObject'),
 	actions: {
-		processStripeToken(payment, args) {
-			console.log('payment', payment);
+
+		/* Payment object exemple
+		 * received from stripe after sending
+		 * the checkout transation
+		 * it is sent to the backend api
+		 *
+		 * payment: {
+		 * 	card: Object
+		 * 	client_ip: "95.91.214.87"
+		 * 	created: 1503074589
+		 * 	email: "hu@hu.hu"
+		 * 	id: "tok_1AsDYvCSxcuHyPxSjjSNPvSc"
+		 * 	livemode: false
+		 * 	object: "token"
+		 * 	type: "card"
+		 * 	used: false
+		 * }
+		 */
+
+		/* Sends token to backend api
+		 * `response` Object is the charged transaction
+		 * the user processed
+		 */
+		processStripeToken(card, args) {
+			console.log('card', card);
 			console.log('args', args);
 			fetch('http://localhost:3000/payments', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json'
 				},
-				body: JSON.stringify(payment)
+				body: JSON.stringify(card)
 			}).then(response => {
 				console.log("response", response)
 			})
