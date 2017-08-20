5231d3fb0cef82f388ba64d5cba1021724bd9238
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-08-20 13:37:41 +0200

    refactor delete notification and template

--- app/channel/delete/route.js
+++ app/channel/delete/route.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import ownerRouteMixin from 'radio4000/mixins/owner-route';
 
-const {Route} = Ember;
+const {Route, get} = Ember;
 
 export default Route.extend(ownerRouteMixin, {
 	// Don't render into channel because we don't want channel templates here.
@@ -9,20 +9,5 @@ export default Route.extend(ownerRouteMixin, {
 		this.render({
 			into: 'application'
 		});
-	},
-	afterModel() {
-		const flashMessages = Ember.get(this, 'flashMessages');
-		flashMessages.danger(`Your radio will be lost forever! 🌇`, {
-			sticky: true
-		});
-	},
-
-	actions: {
-		willTransition() {
-			const flashMessages = Ember.get(this, 'flashMessages');
-			flashMessages.clearMessages().info('Radio deleted…', {
-				timeout: 400
-			});
-		}
 	}
 });
