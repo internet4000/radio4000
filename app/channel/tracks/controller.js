670694f88d36d53e3eeb5f3da62c6d86fda0c29a
Oskar Rough <oskar@rough.dk>
2017-08-22 21:40:13 +0200

    Lint

--- app/channel/tracks/controller.js
+++ app/channel/tracks/controller.js
@@ -3,5 +3,5 @@ import Ember from 'ember';
 const {Controller, inject} = Ember;
 
 export default Controller.extend({
-	channelController: inject.controller('channel'),
+	channelController: inject.controller('channel')
 });
