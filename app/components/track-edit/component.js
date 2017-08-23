f4d9b6b78166bd67136b34c10bee53e37024a4aa
Oskar Rough <oskar@rough.dk>
2017-08-23 12:18:24 +0200

    Fix track edit tasks?

--- app/components/track-edit/component.js
+++ app/components/track-edit/component.js
@@ -2,20 +2,25 @@ import Ember from 'ember';
 import {task} from 'ember-concurrency';
 import TrackFormComponent from 'radio4000/components/track-form/component';
 
-const {get,
-			 computed} = Ember;
+const {get, computed} = Ember;
 
 export default TrackFormComponent.extend({
+	track: null,
 	disableSubmit: computed.oneWay('track.validations.isInvalid'),
+
+	updateTrack: task(function * () {
+		yield get(this, 'track.update').perform();
+		console.log('updated track')
+	}).drop(),
+
+	deleteTrack: task(function * () {
+		yield get(this, 'track.delete').perform();
+		console.log('deleted track')
+	}).drop(),
+
 	actions: {
 		cancel() {
 			get(this, 'cancel')();
-		},
-		delete: task(function *() {
-			return get(this, 'track.delete').perform();
-		}).drop(),
-		update: task(function *() {
-			return get(this, 'track.update').perform();
-		}).drop()
+		}
 	}
 });
