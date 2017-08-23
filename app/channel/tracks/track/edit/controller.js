d9b85e250ca94f0f03b3d98c2b8a83831ec3bb38
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-08-23 13:22:54 +0200

    fix actions with transitions after execution

--- app/channel/tracks/track/edit/controller.js
+++ app/channel/tracks/track/edit/controller.js
@@ -3,9 +3,13 @@ import Ember from 'ember';
 const {Controller, get} = Ember;
 
 export default Controller.extend({
+
 	actions: {
 		cancel() {
 			get(this, 'model').rollbackAttributes();
+			this.send('transitionRoute');
+		},
+		transitionRoute() {
 			this.transitionToRoute('channel.tracks');
 		}
 	}
--- app/components/track-edit/component.js
+++ app/components/track-edit/component.js
@@ -2,20 +2,31 @@ import Ember from 'ember';
 import {task} from 'ember-concurrency';
 import TrackFormComponent from 'radio4000/components/track-form/component';
 
-const {get, computed} = Ember;
+const {get,
+			 computed} = Ember;
 
 export default TrackFormComponent.extend({
+	// track model to edit
 	track: null,
+	// function passed as a prop
+	transitionRoute: null,
 	disableSubmit: computed.oneWay('track.validations.isInvalid'),
+
+	notify(message) {
+		const messages = get(this, 'flashMessages');
+		messages.success(message);
+	},
 
 	updateTrack: task(function * () {
 		yield get(this, 'track.update').perform();
-		console.log('updated track')
+		this.notify('Track updated');
+		yield get(this, 'transitionRoute')()
 	}).drop(),
 
 	deleteTrack: task(function * () {
 		yield get(this, 'track.delete').perform();
-		console.log('deleted track')
+		this.notify('Track deleted');
+		yield get(this, 'transitionRoute')()
 	}).drop(),
 
 	actions: {
