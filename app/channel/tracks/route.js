df0802cc00a2fd8dd9477f62b4f496cc5e476a37
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-09-03 00:22:54 +0200

    query tracks from channel, faster?

--- app/channel/tracks/route.js
+++ app/channel/tracks/route.js
@@ -1,3 +1,12 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({});
+export default Ember.Route.extend({
+	model() {
+		const channel = this.modelFor('channel');
+		console.log('channel', channel)
+		return this.store.query('track', {
+			orderBy: 'channel',
+			equalTo: channel.id
+		})
+	}
+});
