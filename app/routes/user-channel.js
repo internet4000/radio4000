961369d0cedca149a3a940a9a3494fbf1e6d32b3
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-09-04 15:19:27 +0200

    update authenticatedRoute check as mixins

--- app/routes/user-channel.js
+++ app/routes/user-channel.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
+import authenticatedRoute from 'radio4000/mixins/authenticated-route';
 
 const {Route, get} = Ember;
 
 // If there is no user or no channel, redirect to login,
 // otherwise return the channel as model.
 
-export default Route.extend({
+export default Route.extend(authenticatedRoute, {
 	model() {
 		const user = get(this, 'session.currentUser');
 		if (!user) {
