1f970fa8b3ecc69c37dc2657de85ab27cdbb916c
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-09-04 15:40:33 +0200

    lint

--- app/application/route.js
+++ app/application/route.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
 
-const {Route, debug} = Ember;
+const {Route} = Ember;
 
 export default Route.extend({});
--- app/auth/settings/route.js
+++ app/auth/settings/route.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 import authenticatedRoute from 'radio4000/mixins/authenticated-route';
-import ownerRoute from 'radio4000/mixins/owner-route';
 
 export default Ember.Route.extend(authenticatedRoute, {
 });
