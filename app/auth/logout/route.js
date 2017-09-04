b96cf34c95a952fecd33ebccce84f028365921ae
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-09-04 15:19:01 +0200

    make logout route authenticated + move logic to afterModel

--- app/auth/logout/route.js
+++ app/auth/logout/route.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
+import authenticatedRoute from 'radio4000/mixins/authenticated-route';
 
 const {get} = Ember;
 
-export default Ember.Route.extend({
-	beforeModel() {
+export default Ember.Route.extend(authenticatedRoute, {
+	afterModel() {
 		const flashMessages = get(this, 'flashMessages');
 
 		// If we do not unload the user model, Firebase will warn about permissions.
