95d2b1a2d7d82d5235c15f7f02ee054ddd8f668e
Oskar Rough <oskar@rough.dk>
2017-09-04 21:46:56 +0200

    Style and lint

--- tests/integration/components/cloudinary-upload/component-test.js
+++ tests/integration/components/cloudinary-upload/component-test.js
@@ -6,19 +6,7 @@ moduleForComponent('cloudinary-upload', 'Integration | Component | cloudinary up
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{cloudinary-upload}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#cloudinary-upload}}
-      template block text
-    {{/cloudinary-upload}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$('input[type="file"]').length, 1);
+  assert.ok(this.$('button').length > 0);
 });
