b33437b7293ae5bb63252e20e55fbf698f716190
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-09-06 13:30:24 +0200

    add an input to share radio on followers when empty

--- app/components/focus-input/component.js
+++ app/components/focus-input/component.js
@@ -1,8 +1,19 @@
 import Ember from 'ember';
 
-export default Ember.TextField.extend({
+const {TextField, get} = Ember;
+
+export default TextField.extend({
+	// user param
+	select: false,
+
 	// http://emberjs.com/guides/cookbook/user_interface_and_interaction/focusing_a_textfield_after_its_been_inserted/
 	becomeFocused: Ember.on('didInsertElement', function () {
 		this.element.focus();
+
+		if(get(this, 'select')) {
+			this.element.select();
+		}
 	})
+
+
 });
