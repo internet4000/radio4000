8b38fcd130d556cd3ee010a3c451811a57e5df20
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-09-06 17:13:55 +0200

    add play random channel button in channels.all, copy

--- app/components/play-random-channel/component.js
+++ app/components/play-random-channel/component.js
@@ -1,11 +1,14 @@
 import Ember from 'ember';
-const {Component, inject, get, on} = Ember;
+const {Component, inject, get, on, computed} = Ember;
 import {getRandomIndex} from 'radio4000/utils/random-helpers';
 import {EKMixin, keyUp} from 'ember-keyboard';
 
 export default Component.extend(EKMixin, {
 	store: inject.service(),
 	player: inject.service(),
+	tagNames: ['button'],
+	classNames: ['Btn'],
+	isVisible: computed.not('keyboardActivated'),
 
 	bindKeyboard: on(keyUp('KeyW'), function () {
 		this.playRandomChannel();
