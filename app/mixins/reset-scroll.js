949041d24c4838acb1c8407b8f0ec16a9a92282a
Oskar Rough <oskar@rough.dk>
2017-09-06 20:44:10 +0200

    Lint

--- app/mixins/reset-scroll.js
+++ app/mixins/reset-scroll.js
@@ -4,11 +4,11 @@
 
 import Ember from 'ember';
 
-const {Mixin, observer} = Ember;
+const {Mixin} = Ember;
 
 export default Mixin.create({
 	beforeModel() {
 		window.scrollTo(0, 0);
-		this._super();
+		this._super(...arguments);
 	}
 });
