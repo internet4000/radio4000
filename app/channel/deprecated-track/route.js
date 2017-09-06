d20250970712543a8e0bb0ef2c2cf2182aa97c5b
Oskar Rough <oskar@rough.dk>
2017-09-06 13:00:16 +0200

    Add redirect to the now deprecated track URL

---
+++ app/channel/deprecated-track/route.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+	// Redirect the now deprecated channel/:track_id route
+	// to our new one. We can completely remove this once
+	// we no longer care about the old track URLs.
+	redirect(model) {
+		this._super(...arguments)
+		this.transitionTo('channel.tracks.track', model)
+	}
+});
