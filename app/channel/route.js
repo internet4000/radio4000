e22b6c1d8fdff3ba7b724d64bfb6ef0acbbaeb45
Oskar Rough <oskar@rough.dk>
2017-09-15 21:29:15 +0200

    Enable meta tags for channels

--- app/channel/route.js
+++ app/channel/route.js
@@ -2,9 +2,10 @@
 /* global document, window */
 import Ember from 'ember';
 
-const {Route} = Ember;
+const {Route, get, set, inject} = Ember;
 
 export default Route.extend({
+	headData: inject.service(),
 	model(params) {
 		return this.store.query('channel', {
 			orderBy: 'slug',
@@ -12,11 +13,11 @@ export default Route.extend({
 		}).then(data => data.get('firstObject'));
 	},
 	afterModel(model) {
-		if (model) {
-			document.title = `${model.get('title')} - Radio4000`;
-		} else {
+		if (!model) {
 			this.transitionTo('404');
+			return
 		}
+		this.setHeadData(model)
 	},
 	serialize(model) {
 		return {channel_slug: model.get('slug')};
@@ -25,7 +26,26 @@ export default Route.extend({
 		window.scrollTo(0, 0);
 	},
 	deactivate() {
-		// Reset doc title when leaving the route
-		document.title = 'Radio4000';
+		// Reset meta tags when leaving the route.
+		get(this, 'headData').setProperties({
+			title: null, description: null, image: null
+		})
+	},
+	setHeadData(model) {
+		const headData = get(this, 'headData')
+		const title = `${model.get('title')} - Radio4000`;
+		const body = model.get('body')
+
+		set(headData, 'title', title)
+		if (body) {
+			set(headData, 'description', body)
+		}
+		// `model.get('coverImage.src')` doesn't seem to work
+		model.get('images').then(images => {
+			const src = images.get('lastObject.src')
+			if (src) {
+				set(headData, 'image', src)
+			}
+		})
 	}
 });
