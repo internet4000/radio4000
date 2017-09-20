245f652c83579a5cd3e5ca2fe1f35c9f01e35460
Oskar Rough <oskar@rough.dk>
2017-09-20 11:34:40 +0200

    Fix missing className on track-items

--- app/components/track-item/component.js
+++ app/components/track-item/component.js
@@ -6,18 +6,16 @@ export default Component.extend({
 	player: inject.service(),
 	classNames: ['Track'],
 	classNameBindings: [
-		'isCurrent',
 		'track.liveInCurrentPlayer:Track--live',
 		'track.playedInCurrentPlayer:Track--played',
 		'track.finishedInCurrentPlayer:Track--finished'
 	],
 	attributeBindings: ['track.ytid:data-pid'],
 
-	// true if the current track is loaded in the player
-	// isCurrent: computed.equal('player.model', 'track'),
-	isCurrent: computed('player.currentTrack', 'track', function () {
-		return this.get('player.currentTrack') === this.get('track');
-	}),
+	// // true if the current track is loaded in the player
+	// isCurrent: computed('player.currentTrack', 'track', function () {
+	// 	return this.get('player.currentTrack') === this.get('track');
+	// }),
 
 	actions: {
 		edit() {
