f879cc1c476dca601b49b410e3d5cd141bb3f38b
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-10-04 18:10:05 +0200

    add route /channel/play/index to fix subroute bug

--- app/channel/play/route.js
+++ app/channel/play/route.js
@@ -1,19 +1,4 @@
 import Ember from 'ember';
 
-const {Route, inject, get} = Ember;
-
-export default Route.extend({
-	player: inject.service('player'),
-
-	afterModel(model) {
-
-		const tracks = model.get('tracks');
-
-		if(tracks) {
-			const firstTrack = tracks.get('lastObject');
-			get(this, 'player').playTrack(firstTrack);
-		}
-
-		this.transitionTo('channel');
-	}
+export default Ember.Route.extend({
 });
