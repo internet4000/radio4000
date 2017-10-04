5db27fc0983aafafcea8b18bc8ab1469a1d75a21
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-10-04 18:07:21 +0200

    add /channel/play/random

---
+++ tests/unit/channel/play/random/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:channel/play/random', 'Unit | Route | channel/play/random', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
