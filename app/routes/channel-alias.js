5680d6636e719ad1ba4c0eaaf22c8cdaa04bd6cd
Oskar Rough <oskar@rough.dk>
2017-10-13 13:20:37 +0200

    Remove old dashboard and channel-alias routes

--- app/routes/channel-alias.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-	// Deprecated: channel routes used to be `/c/:channel_slug`.
-	// here we redirect to the current, real URL.
-	model(params) {
-		this.replaceWith(`/${params.channel_slug}`);
-	}
-});
