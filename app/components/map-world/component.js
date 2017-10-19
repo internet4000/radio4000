ba339d702b77f221104467472f97e4efa96565af
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-10-19 14:32:50 +0200

    fix query params, lat, lng, zoom

--- app/components/map-world/component.js
+++ app/components/map-world/component.js
@@ -5,14 +5,17 @@ const {Component} = Ember;
 export default Component.extend({
 	classNames: ['Map', 'Map--fullscreen'],
 	lat: 9.96885060854611,
-  lng: 53.08593750000001,
+  lng: 53.08593750,
   zoom: 2,
 	actions: {
     updateCenter(e) {
+			console.log(e)
       let center = e.target.getCenter();
+			let zoom = e.target.getZoom();
+			console.log(zoom)
       this.set('lat', center.lat);
       this.set('lng', center.lng);
-			this.set('zoom', center.zoom);
+			this.set('zoom', zoom);
     }
   }
 });
