691e4a4b4d695d0a1685760ec59796132b909098
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-10-20 11:15:42 +0200

    update coordinates and behavior of world map

--- app/components/map-world/component.js
+++ app/components/map-world/component.js
@@ -4,15 +4,14 @@ const {Component} = Ember;
 
 export default Component.extend({
 	classNames: ['Map', 'Map--fullscreen'],
-  zoom: 2,
-	maxZoom: 2,
-	maxBounds: [[90,-180], [90, 180]],
+  zoom: 5,
+	minZoom: 2,
+	maxBounds: [[-90,-180], [90, 180]],
 	actions: {
     updateCenter(e) {
-			console.log(e)
       let center = e.target.getCenter();
 			let zoom = e.target.getZoom();
-			console.log(zoom)
+
       this.set('lat', center.lat);
       this.set('lng', center.lng);
 			this.set('zoom', zoom);
