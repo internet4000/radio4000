74c43aaea132c81177f3c591fc211cd668d5a3c8
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-10-20 11:35:13 +0200

    add current position on user map

--- app/components/map-position-selection/component.js
+++ app/components/map-position-selection/component.js
@@ -4,16 +4,16 @@ const {Component} = Ember;
 
 export default Component.extend({
 	classNames: ['Map', 'Map--selection'],
-	lat: 0,
-  lng: 20.7421875,
+	newLat: 0,
+  newLng: 20.7421875,
   zoom: 0.7,
 	maxBounds: [[90,-180], [-90, 180]],
 	actions: {
     updateCenter(e) {
 			console.log('center', center)
       let center = e.target.getCenter();
-      this.set('lat', center.lat);
-      this.set('lng', center.lng);
+      this.set('newLat', center.lat);
+      this.set('newLng', center.lng);
     }
   }
 });
