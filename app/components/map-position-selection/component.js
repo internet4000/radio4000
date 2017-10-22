e73e06a0ec1047da3f6e691e160d6e224e290933
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-10-22 11:44:19 +0200

    put the other map in

--- app/components/map-position-selection/component.js
+++ app/components/map-position-selection/component.js
@@ -1,20 +1,24 @@
 import Ember from 'ember'
+import {task} from 'ember-concurrency'
 
-const { Component } = Ember
+const { Component,
+				computed,
+				get } = Ember
 
 export default Component.extend({
 	classNames: ['Map', 'Map--selection'],
 	newLat: 0,
 	newLng: 20.7421875,
+	location: computed('lat', 'lng', function () {
+		const lat = get(this, 'lat'),
+					lng = get(this, 'lng');
+		return { lat, lng }
+	}),
 	zoom: 0.7,
 	maxBounds: [[90, -180], [-90, 180]],
 	actions: {
-		updateCenter(e) {
-			let center = e.target.getCenter()
-			this.setProperties({
-				newLat: center.lat,
-				newLng: center.lng
-			})
-		}
+		update: task(function * () {
+			console.log(test)
+		})
 	}
 })
