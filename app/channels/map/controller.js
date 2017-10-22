da16789254cd9378a7149fcd858ba2ced025e370
Oskar Rough <oskar@rough.dk>
2017-10-22 12:43:42 +0200

    Filter out channels without coords for the map

--- app/channels/map/controller.js
+++ app/channels/map/controller.js
@@ -1,12 +1,19 @@
 import Ember from 'ember'
 
-const { Controller } = Ember
+const { Controller, computed } = Ember
 
 export default Controller.extend({
 	queryParams: ['lat', 'lng', 'zoom'],
 	lat: 22.105998799750566,
 	lng: 33.04687500000001,
 	zoom: 2,
+
+	channelsWithCoordinates: computed.filter('model', function(channel) {
+		return (
+			channel.get('coordinatesLatitude') &&
+			channel.get('coordinatesLongitude')
+		)
+	}),
 
 	actions: {
 		updatePosition(pos) {
