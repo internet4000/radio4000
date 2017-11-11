1f919d636d803c33ae1d7391f0de93760ccd9116
Oskar Rough <oskar@rough.dk>
2017-11-11 19:23:54 +0100

    Formatting

--- app/router.js
+++ app/router.js
@@ -1,11 +1,11 @@
 /* eslint array-callback-return:0 */
-import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import EmberRouter from '@ember/routing/router'
+import config from './config/environment'
 import googlePageview from './mixins/google-pageview'
 
 const Router = EmberRouter.extend(googlePageview, {
-  location: config.locationType,
-  rootURL: config.rootURL
+	location: config.locationType,
+	rootURL: config.rootURL
 });
 
 Router.map(function() {
