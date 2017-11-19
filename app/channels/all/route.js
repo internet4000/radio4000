6414fbc59097ebc58d7f21871cc6e0cc714e6458
Oskar Rough <oskar@rough.dk>
2017-11-19 12:40:29 +0100

    Add basic pagination

--- app/channels/all/route.js
+++ app/channels/all/route.js
@@ -1,8 +1,13 @@
-import Ember from 'ember';
+import Ember from 'ember'
 
-const {Route} = Ember;
+const {Route} = Ember
+
+const PAGE_SIZE = 4
 
 export default Route.extend({
+	startAt: null,
+	endAt: null,
+
 	// When search changes the url, don't replace (add) to the history.
 	// e.g. back/forth in browser works as expected.
 	queryParams: {
@@ -12,8 +17,44 @@ export default Route.extend({
 	},
 
 	model() {
-		// return this.store.query('channel', {limitToLast: 100})
-		this.store.findAll('channel')
-		return this.store.peekAll('channel')
+	//	// return this.store.query('channel', {limitToLast: 100})
+	// 	this.store.findAll('channel')
+	// 	return this.store.peekAll('channel')
+		var query = {
+			limitToFirst: PAGE_SIZE + 1
+		}
+
+		if (this.get('startAt')) {
+			query.startAt = this.get('startAt')
+		}
+
+		if (this.get('endAt')) {
+			query.endAt = this.get('endAt')
+			delete query.limitToFirst
+			query.limitToLast = PAGE_SIZE + 1
+		}
+
+		return this.store.query('channel', query).then(posts => {
+			if (this.get('startAt')) {
+				return posts.slice(1)
+			}
+			return posts.slice(0, posts.get('length') - 1)
+		})
+	},
+
+	actions: {
+		prev() {
+			var id = this.get('currentModel').get('firstObject.id')
+			this.set('startAt', null)
+			this.set('endAt', id)
+			this.refresh()
+		},
+		next() {
+			var id = this.get('currentModel').get('lastObject.id')
+			this.set('startAt', id)
+			this.set('endAt', null)
+			this.refresh()
+		}
 	}
-});
+})
+
