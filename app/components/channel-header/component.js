6c7fd702bf1c67027f6ac24e4cdcc0eb078629ea
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-11-20 09:37:57 +0100

    hide updated if more than two month ago

--- app/components/channel-header/component.js
+++ app/components/channel-header/component.js
@@ -1,8 +1,23 @@
 import Ember from 'ember';
+import addMonths from 'npm:date-fns/add_months';
+import isAfter from 'npm:date-fns/is_after';
 
-const {Component, computed} = Ember;
+const {
+	Component,
+	computed,
+	get
+} = Ember;
 
 export default Component.extend({
 	classNames: ['ChannelHeader'],
-	hasLinks: computed.or('channel.link', 'channel.hasCoordinates')
+	hasLinks: computed.or('channel.link', 'channel.hasCoordinates'),
+	shouldShowUpdated: computed('channel.updated', function() {
+		const updatedTimestamp = get(this, 'channel.updated');
+
+		if(!updatedTimestamp) { return }
+
+		const limit = addMonths(new Date(), -2);
+		console.log('limit', limit)
+		return isAfter(new Date(updatedTimestamp), limit);
+	})
 });
