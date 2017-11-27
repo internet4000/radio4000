cbf3a95b7428bbf420c397e36e6fc1f434cef127
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-11-27 20:14:33 +0100

    move add current Track

--- app/components/track-form-add/component.js
+++ app/components/track-form-add/component.js
@@ -2,13 +2,19 @@ import Ember from 'ember';
 import TrackFormComponent from 'radio4000/components/track-form/component';
 import {Validations} from 'radio4000/models/track';
 
-const {get, set, computed} = Ember;
+const {
+	inject,
+	get,
+	set,
+	computed
+} = Ember;
 
 // Use a normal object instead af a model.
 // This avoids it appearing in the UI before it is saved.
 const trackObject = Ember.Object.extend(Validations);
 
 export default TrackFormComponent.extend({
+	player: inject.service(),
 	disableSubmit: computed.or('submitTask.isRunning', 'isSubmitting', 'track.validations.isInvalid'),
 
 	// Called on init as well as after submitting a track.
