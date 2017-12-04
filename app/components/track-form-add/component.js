6001c7ddacd38fec3e265bf767bd5e796151dba9
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-12-04 18:10:48 +0100

    condition repost track to channel.premium

--- app/components/track-form-add/component.js
+++ app/components/track-form-add/component.js
@@ -14,16 +14,18 @@ const {
 const trackObject = Ember.Object.extend(Validations);
 
 export default TrackFormComponent.extend({
+	session: inject.service(),
 	player: inject.service(),
+	flashMessages: inject.service(),
 	disableSubmit: computed.or('submitTask.isRunning', 'isSubmitting', 'track.validations.isInvalid'),
 
 	// Called on init as well as after submitting a track.
 	// Also see the same method on the `TrackForm` component, from which this extends.
 	resetForm() {
 		// The getOwner part is mentioned in the ember-cp-validation docs.
-		const track = trackObject.create(Ember.getOwner(this).ownerInjection(), {
-			url: get(this, 'initialUrl')
-		});
+			const track = trackObject.create(Ember.getOwner(this).ownerInjection(), {
+				url: get(this, 'initialUrl')
+			});
 		set(this, 'track', track);
 		this._super(...arguments);
 	},
@@ -42,6 +44,14 @@ export default TrackFormComponent.extend({
 				title: currentTrack.get('title'),
 				body: `thx @${currentChannel.get('slug')}`
 			});
+		},
+		inviteToPremium() {
+			const message = `Hello!
+This feature can be used by upgraded channels only.
+Go to your settings to upgrade your Radio.`
+			get(this, 'flashMessages').info(message, {
+				timeout: 5000
+			})
 		}
 	}
 
