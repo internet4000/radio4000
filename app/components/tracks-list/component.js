8bb2903ba1f93e79bc15f54c5fe21d58a5e8b73e
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-12-18 11:54:03 +0100

    get track ids fronm jetSearch

--- app/components/tracks-list/component.js
+++ app/components/tracks-list/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember'
 import { array } from 'ember-awesome-macros'
 import raw from 'ember-macro-helpers/raw'
 
-const { Component, computed, get } = Ember
+const { Component, computed, get, set } = Ember
 
 export default Component.extend({
 	classNames: ['Tracks'],
@@ -17,33 +17,19 @@ export default Component.extend({
 	sortedItems: array.sort('items', ['created:desc']),
 
 	// Tracks grouped by month.
-	groupedItems: array.groupBy('sortedItems', raw('createdMonth')),
+	/* groupedItems: array.groupBy('sortedItems', raw('createdMonth')),
+		 groupedSearchedItems: array.groupBy('searchedItems', raw('createdMonth')),*/
 
-	groupedSearchedItems: array.groupBy('searchedItems', raw('createdMonth')),
-
-	searchedItems: computed('searchQuery', 'sortedItems', function() {
-		const search = get(this, 'searchQuery')
-			.split(' ')
-			.join('|');
-		const re = new RegExp(search, 'gi')
-
-		const tracks = get(this, 'items')
-		const selection = [];
-
-		console.log('re', re)
-		tracks.forEach(track => {
-			if (track.get('searchableData').search(re) > 0) {
-				selection.push(track)
-			}
-		})
-		return selection;
-		/* return selection.map(i => i.get('searchableData'));*/
-	}),
+	getTrackIdsFromSearch: function() {
+		const q = get(this, 'searchQuery')
+		const s = document.querySelectorAll(`#TrackList > .List-item[data-jets *= "${q}"]`);
+		return Array.from(s).map(item => item.attributes['data-track-id'].value)
+	},
 
 	actions: {
 		playSelection() {
-			const s = get(this, 'searchedItems');
-			console.log('sss', s)
+			const s = this.getTrackIdsFromSearch();
+			console.log('search results', s)
 		}
 	}
 })
