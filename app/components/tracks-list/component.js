fe2782408969aeb9b4720372a43d260d9f26e21e
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-12-18 04:00:42 +0100

    stable but slow

--- app/components/tracks-list/component.js
+++ app/components/tracks-list/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember'
 import { array } from 'ember-awesome-macros'
 import raw from 'ember-macro-helpers/raw'
 
-const { Component } = Ember
+const { Component, computed, get } = Ember
 
 export default Component.extend({
 	classNames: ['Tracks'],
@@ -10,9 +10,40 @@ export default Component.extend({
 	numbered: false,
 	grouped: false,
 
+	searchQuery: '',
+	cannotPlay: computed.not('searchQuery'),
+
 	// Newest on top.
 	sortedItems: array.sort('items', ['created:desc']),
 
 	// Tracks grouped by month.
-	groupedItems: array.groupBy('sortedItems', raw('createdMonth'))
+	groupedItems: array.groupBy('sortedItems', raw('createdMonth')),
+
+	groupedSearchedItems: array.groupBy('searchedItems', raw('createdMonth')),
+
+	searchedItems: computed('searchQuery', 'sortedItems', function() {
+		const search = get(this, 'searchQuery')
+			.split(' ')
+			.join('|');
+		const re = new RegExp(search, 'gi')
+
+		const tracks = get(this, 'items')
+		const selection = [];
+
+		console.log('re', re)
+		tracks.forEach(track => {
+			if (track.get('searchableData').search(re) > 0) {
+				selection.push(track)
+			}
+		})
+		return selection;
+		/* return selection.map(i => i.get('searchableData'));*/
+	}),
+
+	actions: {
+		playSelection() {
+			const s = get(this, 'searchedItems');
+			console.log('sss', s)
+		}
+	}
 })
