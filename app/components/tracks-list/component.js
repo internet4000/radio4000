42456952142afe8cf647a05510159e99f2fc63ec
Oskar Rough <oskar@rough.dk>
2017-12-19 02:02:33 +0100

    Lint

--- app/components/tracks-list/component.js
+++ app/components/tracks-list/component.js
@@ -1,6 +1,6 @@
 import Ember from 'ember'
 import { array } from 'ember-awesome-macros'
-import raw from 'ember-macro-helpers/raw'
+// import raw from 'ember-macro-helpers/raw'
 
 const { Component, $, inject, computed, get, set } = Ember
 
@@ -19,8 +19,8 @@ export default Component.extend({
 	sortedItems: array.sort('items', ['created:desc']),
 
 	// Tracks grouped by month.
-	/* groupedItems: array.groupBy('sortedItems', raw('createdMonth')),
-		 groupedSearchedItems: array.groupBy('searchedItems', raw('createdMonth')),*/
+	// groupedItems: array.groupBy('sortedItems', raw('createdMonth')),
+	// groupedSearchedItems: array.groupBy('searchedItems', raw('createdMonth')),
 
 	didUpdate() {
 		this.performSearchOnModels()
@@ -38,7 +38,7 @@ export default Component.extend({
 			const trackIds = get(this, 'searchResultTrackIds');
 
 			get(this, 'items.firstObject.channel').then(channel => {
-				const playlist =  player.buildPlaylistExport(channel, trackIds);
+				const playlist = player.buildPlaylistExport(channel, trackIds);
 				player.loadPlayistInWebComponent(playlist)
 			})
 		}
