59753a92bfee6864b30151c720414d623c8d35e0
Oskar Rough <oskar@rough.dk>
2017-12-19 17:48:56 +0100

    Add 's' shortcut to focus search on that route

--- app/components/tracks-list/component.js
+++ app/components/tracks-list/component.js
@@ -1,28 +1,33 @@
 import Ember from 'ember'
 import { array } from 'ember-awesome-macros'
+import {EKMixin, keyUp} from 'ember-keyboard'
 // import raw from 'ember-macro-helpers/raw'
 // import { debounce } from '@ember/runloop'
 
 const { Component, $, inject, computed, get, set } = Ember
 
-export default Component.extend({
+export default Component.extend(EKMixin, {
 	player: inject.service(),
 	classNames: ['Tracks'],
 	items: null,
 	numbered: false,
 	grouped: false,
-
 	searchQuery: '',
+	selection: [],
+
 	noSearchQuery: computed.not('searchQuery'),
 
 	// Newest on top.
 	sortedItems: array.sort('items', ['created:desc']),
 
+	focusSearch: Ember.on(keyUp('KeyS'), function () {
+		this.element.querySelector('input[type="search"]').focus()
+	}),
+
 	// Tracks grouped by month.
 	// groupedItems: array.groupBy('sortedItems', raw('createdMonth')),
 	// groupedSearchedItems: array.groupBy('searchedItems', raw('createdMonth')),
 
-	selection: [],
 	setSelectionFromSearch() {
 		const $els = $('#TrackList .List-item:visible')
 		const ids = $.map($els, el => el.getAttribute('data-track-id'))
