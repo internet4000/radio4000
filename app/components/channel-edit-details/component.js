85fa38f22f92d483efc4be64507b6bb6f06e08ee
Oskar Rough <oskar@rough.dk>
2018-01-10 22:40:00 +0100

    Remove unused code

--- app/components/channel-edit-details/component.js
+++ app/components/channel-edit-details/component.js
@@ -12,24 +12,14 @@ export default Component.extend({
 		set(this, 'proxy', {})
 	},
 
-	nothingChanged: computed.not('channel.hasDirtyAttributes'),
-	cantSave: computed.or('channel.validations.isInvalid', 'nothingChanged'),
-	disableSubmit: computed.or('submitTask.isRunning', 'cantSave'),
-
 	actions: {
-		update(a, b) {
-			console.log(a, b)
-		},
 		cancel() {
 			get(this, 'onCancel')()
 		},
 		submit() {
 			const proxy = get(this, 'proxy')
 			const channel = get(this, 'channel')
-
-			// console.log(proxy)
 			const updated = Object.assign(channel, proxy)
-			console.log(updated.get('title'))
 			get(this, 'submitTask').perform(proxy)
 		}
 	}
