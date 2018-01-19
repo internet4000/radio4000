c7cd8af2e09f6cfcc249fbe5943f161e1760b8a9
Oskar Rough <oskar@rough.dk>
2018-01-19 13:12:17 +0100

    Allow 'random-button' to react to the "R" shortcut

--- app/components/play-random-channel/component.js
+++ app/components/play-random-channel/component.js
@@ -1,8 +1,16 @@
 import PlayButtonComponent from 'radio4000/components/play-btn/component'
 import {task, timeout} from 'ember-concurrency'
+import {or} from 'ember-awesome-macros'
+import {computed} from '@ember/object'
 
 export default PlayButtonComponent.extend({
 	title: 'Play a random Radio4000 channel [⌨ r]',
+
+	// Slightly different logic here so that the button also reacts
+	// when the random channel task is running OUTSIDE of this component.
+	// Like when you press the "R" shortcut.
+	isRunning: or('player.playRandomChannel.isRunning', 'clickTask.isRunning'),
+	disabled: computed.reads('isRunning'),
 
 	clickTask: task(function * () {
 		yield this.get('player.playRandomChannel').perform()
