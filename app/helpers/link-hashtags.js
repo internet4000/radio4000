c12170892e8433f2b35b3c6b9cece79a4cc51532
Oskar Rough <oskar@rough.dk>
2018-01-22 13:40:24 +0100

    Make it safe
    hopefully

--- app/helpers/link-hashtags.js
+++ app/helpers/link-hashtags.js
@@ -1,4 +1,6 @@
-import { helper } from '@ember/component/helper';
+import Ember from 'ember'
+import {helper} from '@ember/component/helper'
+import {htmlSafe} from '@ember/string'
 
 // https://regex101.com/r/pJ4wC5/1
 const hashtagRegex = /(^|\s)(#[a-z\d-]+)/ig
@@ -8,16 +10,18 @@ export function linkHashtags([string, slug]) {
 		return string
 	}
 
-	let href = `/${slug}/tracks?search=$2`
-	let link = string.replace(hashtagRegex, `$1<a href="${href}">$2</a>`)
-
-	// link = encodeURIComponent(link)
+	let stringWithLinks = string
+		.replace(hashtagRegex, `$1<a href="${slug}/tracks?search=$2">$2</a>`)
 
 	// Ember doesn't understand # in the URL. Replacing "#" with "%23" works.
-	link = link.replace(/(=#)/gi, '=%23')
+	// link = encodeURIComponent(link)
+	stringWithLinks = stringWithLinks.replace(/(=#)/gi, '=%23')
 
-	return link
+	// Make sure result is html safe.
+	Ember.Handlebars.Utils.escapeExpression(stringWithLinks)
+
+	// And "mark" it as safe.
+	return htmlSafe(stringWithLinks)
 }
 
 export default helper(linkHashtags)
-
--- tests/integration/helpers/link-hashtags-test.js
+++ tests/integration/helpers/link-hashtags-test.js
@@ -1,3 +1,4 @@
+import {linkHashtags} from 'radio4000/helpers/link-hashtags'
 import { moduleForComponent, test } from 'ember-qunit'
 import hbs from 'htmlbars-inline-precompile'
 
@@ -5,18 +6,14 @@ moduleForComponent('link-hashtags', 'helper:link-hashtags', {
 	integration: true
 })
 
-test('without slug it just returns the original string', function(assert) {
+test('without slug it returns original string', function(assert) {
 	this.set('inputValue', '1234 #cool')
 	this.render(hbs`{{link-hashtags inputValue}}`)
 
 	assert.equal(this.$().text().trim(), '1234 #cool')
 })
 
-test('with a slug it transforms hashtags to links', function(assert) {
-	this.set('inputValue', 'hey #cool right')
-	this.set('slugValue', '200ok')
-	this.render(hbs`{{link-hashtags inputValue slugValue}}`)
-
-	assert.equal(this.$().text().trim(), 'hey <a href="/200ok/tracks?search=%23cool">#cool</a> right')
+test('with slug it transforms hashtags to links', function(assert) {
+	const result = linkHashtags(['1234 #cool', '200ok'])
+	assert.equal(result.string, '1234 <a href="200ok/tracks?search=%23cool">#cool</a>')
 })
-
