ecdd5f3b2bc50929a2222649700017bb058dc3d5
Oskar Rough <oskar@rough.dk>
2018-01-22 13:02:29 +0100

    Lint regex

--- app/helpers/link-hashtags.js
+++ app/helpers/link-hashtags.js
@@ -4,13 +4,17 @@ import { helper } from '@ember/component/helper';
 const hashtagRegex = /(^|\s)(#[a-z\d-]+)/ig
 
 export function linkHashtags([string, slug]) {
-	if (!slug) return string
+	if (!slug) {
+		return string
+	}
 
 	let href = `/${slug}/tracks?search=$2`
 	let link = string.replace(hashtagRegex, `$1<a href="${href}">$2</a>`)
 
+	// link = encodeURIComponent(link)
+
 	// Ember doesn't understand # in the URL. Replacing "#" with "%23" works.
-	link = link.replace(/=#/gi, '=%23')
+	link = link.replace(/(=#)/gi, '=%23')
 
 	return link
 }
