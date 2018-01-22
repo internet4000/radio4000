6bca6246167781044f4d545f48621a3815951bca
Oskar <oskarrough@gmail.com>
2018-01-22 16:43:25 +0100

    Add link-hashtags helper (#195)
    * Add link-hashtags helper
    
    * Lint regex
    
    * Make it safe
    
    hopefully
    
    * Use absolute urls
    
    * Use helper on channel cards

---
+++ app/helpers/link-hashtags.js
@@ -0,0 +1,32 @@
+import Ember from 'ember'
+import {helper} from '@ember/component/helper'
+import {htmlSafe, isHTMLSafe} from '@ember/string'
+
+// https://regex101.com/r/pJ4wC5/1
+const hashtagRegex = /(^|\s)(#[a-z\d-]+)/ig
+
+export function linkHashtags([string, slug]) {
+	if (!slug) {
+		return string
+	}
+
+	// Support use cases like {{link-hashtags (do-truncate "my-string" 20)}}
+	if (isHTMLSafe(string)) {
+		string = string.string
+	}
+
+	let stringWithLinks = string
+		.replace(hashtagRegex, `$1<a href="/${slug}/tracks?search=$2">$2</a>`)
+
+	// Ember doesn't understand # in the URL. Replacing "#" with "%23" works.
+	// link = encodeURIComponent(link)
+	stringWithLinks = stringWithLinks.replace(/(=#)/gi, '=%23')
+
+	// Make sure result is html safe.
+	Ember.Handlebars.Utils.escapeExpression(stringWithLinks)
+
+	// And "mark" it as safe.
+	return htmlSafe(stringWithLinks)
+}
+
+export default helper(linkHashtags)
---
+++ tests/integration/helpers/link-hashtags-test.js
@@ -0,0 +1,19 @@
+import {linkHashtags} from 'radio4000/helpers/link-hashtags'
+import { moduleForComponent, test } from 'ember-qunit'
+import hbs from 'htmlbars-inline-precompile'
+
+moduleForComponent('link-hashtags', 'helper:link-hashtags', {
+	integration: true
+})
+
+test('without slug it returns original string', function(assert) {
+	this.set('inputValue', '1234 #cool')
+	this.render(hbs`{{link-hashtags inputValue}}`)
+
+	assert.equal(this.$().text().trim(), '1234 #cool')
+})
+
+test('with slug it transforms hashtags to links', function(assert) {
+	const result = linkHashtags(['1234 #cool', '200ok'])
+	assert.equal(result.string, '1234 <a href="/200ok/tracks?search=%23cool">#cool</a>')
+})
