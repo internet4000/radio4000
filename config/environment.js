a76e3ad563636b811c3e38189085c30378253c82
Oskar Rough <oskar@rough.dk>
2018-01-29 20:30:03 +0100

    Use ember-page-title
    It also uses ember-cli-head and offers a nice API

--- config/environment.js
+++ config/environment.js
@@ -2,26 +2,26 @@
 'use strict';
 
 module.exports = function(environment) {
-  let ENV = {
-    modulePrefix: 'radio4000',
-    environment,
-    rootURL: '/',
-    locationType: 'auto',
-    EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      },
-      EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false
-      }
-    },
+	let ENV = {
+		modulePrefix: 'radio4000',
+		environment,
+		rootURL: '/',
+		locationType: 'auto',
+		EmberENV: {
+			FEATURES: {
+				// Here you can enable experimental features on an ember canary build
+				// e.g. 'with-controller': true
+			},
+			EXTEND_PROTOTYPES: {
+				// Prevent Ember Data from overriding Date.parse.
+				Date: false
+			}
+		},
 
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
-    },
+		APP: {
+			// Here you can pass flags/options to your application instance
+			// when it is created
+		},
 
 		// https://console.developers.google.com/apis/credentials/key/2?project=firebase-radio4000
 		youtubeApiKey: 'AIzaSyBPY0xzz4PQEl5hgm-wggEA4Zlho11Rtfk',
@@ -34,8 +34,8 @@ module.exports = function(environment) {
 		},
 
 		stripe: {
-      key: 'pk_test_Ti8aYhMv1as1ZrbaZzaI8QOk'
-    },
+			key: 'pk_test_Ti8aYhMv1as1ZrbaZzaI8QOk'
+		},
 
 		torii: {
 			sessionServiceName: 'session'
@@ -45,29 +45,29 @@ module.exports = function(environment) {
 			timeout: 3000,
 			extendedTimeout: 1000
 		}
-  };
+	};
 
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
-  }
+	if (environment === 'development') {
+		// ENV.APP.LOG_RESOLVER = true;
+		// ENV.APP.LOG_ACTIVE_GENERATION = true;
+		// ENV.APP.LOG_TRANSITIONS = true;
+		// ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+		// ENV.APP.LOG_VIEW_LOOKUPS = true;
+	}
 
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.locationType = 'none';
+	if (environment === 'test') {
+		// Testem prefers this...
+		ENV.locationType = 'none';
 
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+		// keep test console output quieter
+		ENV.APP.LOG_ACTIVE_GENERATION = false;
+		ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-    ENV.APP.rootElement = '#ember-testing';
-  }
+		ENV.APP.rootElement = '#ember-testing';
+	}
 
-  if (environment === 'production') {
-    // here you can enable a production-specific feature
+	if (environment === 'production') {
+		// here you can enable a production-specific feature
 		// Switch to live Firebase
 		ENV.firebase = {
 			apiKey: 'AIzaSyAyeuzsaD353IhPbffzvP8EE88HNTwSTY0',
@@ -79,7 +79,7 @@ module.exports = function(environment) {
 		ENV.googleAnalytics = {
 			webPropertyId: 'UA-3906535-23'
 		};
-  }
+	}
 
-  return ENV;
+	return ENV;
 };
