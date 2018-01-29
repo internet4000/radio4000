c1bef9f6b5eee846801118e54006969c3aaffd95
Oskar Rough <oskar@rough.dk>
2018-01-29 20:31:16 +0100

    Set some page titles

--- app/about/route.js
+++ app/about/route.js
@@ -1,12 +1,4 @@
-/* global document */
 import Ember from 'ember'
 import resetScroll from 'radio4000/mixins/reset-scroll'
 
-export default Ember.Route.extend(resetScroll, {
-	afterModel() {
-		document.title = 'About Radio4000'
-	},
-	deactivate() {
-		document.title = 'Radio4000'
-	}
-})
+export default Ember.Route.extend(resetScroll, {})
