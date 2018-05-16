89ea0db6a4c30958c3647ebcba0a7b70bff9587f
Oskar Rough <oskar@rough.dk>
2018-05-16 09:52:22 +0200

    Make featured radios on /about random
    By reusing the model hook from our homepage.

--- app/about/intro/route.js
+++ app/about/intro/route.js
@@ -1,16 +1,6 @@
 import Ember from 'ember'
+import Route from 'radio4000/channels/index/route'
 
-const { Route, inject, RSVP } = Ember
+// Reuse the "random featured radios" model
 
-export default Route.extend({
-	uiStates: inject.service(),
-
-	model() {
-		return RSVP.hash({
-			featured: this.store.query('channel', {
-				orderBy: 'isFeatured',
-				equalTo: true
-			})
-		})
-	}
-})
+export default Route.extend({})
