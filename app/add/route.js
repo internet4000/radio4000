6d9bf965ed33173ed364b3fd770cb267d07e4452
Oskar Rough <oskar@rough.dk>
2018-05-23 10:06:00 +0200

    Manually set URL query param

--- app/add/route.js
+++ app/add/route.js
@@ -1,4 +1,10 @@
 import UserChannelRoute from 'radio4000/routes/user-channel'
 import resetScroll from 'radio4000/mixins/reset-scroll'
 
-export default UserChannelRoute.extend(resetScroll, {})
+export default UserChannelRoute.extend(resetScroll, {
+	setupController(controller, model) {
+		let parsedUrl = new URL(window.location.href)
+		let videoUrl = parsedUrl.searchParams.get('url')
+		this.set('controller.url', videoUrl)
+	}
+})
