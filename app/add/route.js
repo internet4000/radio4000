64378cd7e478a3cad9ff2474194cd5928378f7cc
Oskar Rough <oskar@rough.dk>
2018-05-25 11:14:13 +0200

    Fix wrong super call

--- app/add/route.js
+++ app/add/route.js
@@ -2,8 +2,8 @@ import UserChannelRoute from 'radio4000/routes/user-channel'
 import resetScroll from 'radio4000/mixins/reset-scroll'
 
 export default UserChannelRoute.extend(resetScroll, {
-	setupController(controller) {
-		this.super(...arguments)
+	setupController(controller, model) {
+		this._super(...arguments)
 		let parsedUrl = new URL(window.location.href)
 		let videoUrl = parsedUrl.searchParams.get('url')
 		controller.set('url', videoUrl)
