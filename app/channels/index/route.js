b2ee1d37c0cf2bb7e8a9ed130ed337c633c9f619
Oskar Rough <oskar@rough.dk>
2018-06-11 14:25:37 +0200

    Fix whitespace

--- app/channels/index/route.js
+++ app/channels/index/route.js
@@ -4,59 +4,59 @@ import {pickRandom} from 'radio4000/utils/random-helpers'
 import {all} from 'rsvp'
 
 export default Route.extend({
-  // By combining and shuffling/randomizing featured channels
-  // with their favorites we get a more exciting selection.
+	// By combining and shuffling/randomizing featured channels
+	// with their favorites we get a more exciting selection.
 
-  maxFeatured: 20,
-  maxFavoritesPerChannel: 10,
-  maxTotal: 9,
+	maxFeatured: 20,
+	maxFavoritesPerChannel: 10,
+	maxTotal: 9,
 
-  model() {
-    return this.findFeatured().then(featured => {
-      // Find favorites from the featured channels
-      let favorites = featured
-        .map(channel => this.getRandomFavorites(channel))
-        .reduce((prev, curr) => prev.concat(curr))
-        .uniq()
+	model() {
+		return this.findFeatured().then(featured => {
+			// Find favorites from the featured channels
+			let favorites = featured
+				.map(channel => this.getRandomFavorites(channel))
+				.reduce((prev, curr) => prev.concat(curr))
+				.uniq()
 
-      // Merge featured + favorites, remove duplicates and randomize.
-      let merged = featured
-        .toArray()
-        .map(f => f.id)
-        .concat(favorites)
-      merged = merged.uniq()
+			// Merge featured + favorites, remove duplicates and randomize.
+			let merged = featured
+				.toArray()
+				.map(f => f.id)
+				.concat(favorites)
+			merged = merged.uniq()
 
-      // Limit how many and make it random.
-      const result = pickRandom(merged, get(this, 'maxTotal'))
+			// Limit how many and make it random.
+			const result = pickRandom(merged, get(this, 'maxTotal'))
 
-      // Create an array of promises.
-      const promises = result.map(id => this.store.findRecord('channel', id))
+			// Create an array of promises.
+			const promises = result.map(id => this.store.findRecord('channel', id))
 
-      return all(promises).then(channels => {
-        return channels.filterBy('coverImage')
-      })
-    })
-  },
+			return all(promises).then(channels => {
+				return channels.filterBy('coverImage')
+			})
+		})
+	},
 
-  findFeatured() {
-    return this.store.query('channel', {
-      orderBy: 'isFeatured',
-      equalTo: true,
-      limitToLast: get(this, 'maxFeatured')
-    })
-  },
+	findFeatured() {
+		return this.store.query('channel', {
+			orderBy: 'isFeatured',
+			equalTo: true,
+			limitToLast: get(this, 'maxFeatured')
+		})
+	},
 
-  // Returns an array of arrays with ids of favorites.
-  // [[1,6,2], [3,4,5]]
-  getRandomFavorites(channel) {
-    const ids = channel.hasMany('favoriteChannels').ids()
-    return pickRandom(ids, get(this, 'maxFavoritesPerChannel'))
-  },
+	// Returns an array of arrays with ids of favorites.
+	// [[1,6,2], [3,4,5]]
+	getRandomFavorites(channel) {
+		const ids = channel.hasMany('favoriteChannels').ids()
+		return pickRandom(ids, get(this, 'maxFavoritesPerChannel'))
+	},
 
-  actions: {
-    refreshSelection() {
-      // refresh the model
-      this.refresh()
-    }
-  }
+	actions: {
+		refreshSelection() {
+			// refresh the model
+			this.refresh()
+		}
+	}
 })
