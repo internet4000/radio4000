6155ef3b8f2cbb9bb25b257dbe3fa99213460643
Oskar <oskarrough@gmail.com>
2018-06-19 20:06:47 +0200

    Fix/channel edit slug (#230)
    Two things were broken:
    
    1. We compared `if (props)` but `props` could be an empty object, which is `true`. This meant it would wrongly continue to try and validate slug.
    
    2. Getting the initial slug from the model via the route didn't work so `slugChanged` was always true.
    
    When you tap 'save' without any changes, it doesn't actually save but still I introduced a "Saved" notification. Felt better. Otherwise I wasn't sure if it was stuck saving.. 
    
    Please test, try to change slug, try to change it to that of an existing channel or reserved route like `about` or `help`.
    
    cc @hellerve

--- app/settings/channel/route.js
+++ app/settings/channel/route.js
@@ -1,13 +1,4 @@
 import UserChannelRoute from 'radio4000/routes/user-channel'
 
 export default UserChannelRoute.extend({
-	setupController(controller, model) {
-		controller.set('initialSlug', model.get('slug'))
-		this._super(...arguments)
-	},
-
-	deactivate() {
-		// Clear any unsaved changes.
-		this.currentModel.rollbackAttributes()
-	}
 })
