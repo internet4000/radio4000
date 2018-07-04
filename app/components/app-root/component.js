207cc2a4e572815ff42e56f4769a28d5067e84b3
Oskar Rough <oskar@rough.dk>
2018-07-04 21:14:51 +0200

    Lint

--- app/components/app-root/component.js
+++ app/components/app-root/component.js
@@ -1,5 +1,4 @@
 /* global document */
-import Ember from 'ember';
 import Component from '@ember/component'
 import {get, set} from '@ember/object'
 import { inject as service } from '@ember/service'
