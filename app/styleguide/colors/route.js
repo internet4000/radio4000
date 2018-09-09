f286bae7d3d5dbe2043d4ae30e758ad8b218b6f7
Oskar Rough <oskar@rough.dk>
2018-09-09 08:16:23 +0200

    Remove unused colors and update styleguide/colors

--- app/styleguide/colors/route.js
+++ app/styleguide/colors/route.js
@@ -1,33 +1,31 @@
-import Ember from 'ember';
+import Ember from 'ember'
+
+// These colors are copy/pasted from _colors.scss
+// and might not be up to date.
 
 const colors = [
 	{title: 'purple', value: 'hsl(260, 80%, 50%)'},
 	{title: 'red', value: 'hsl(0, 75%, 50%)'},
-	{title: 'yellow', value: 'hsl(52, 100%, 67%)'},
+	{title: 'yellow', value: 'rgba(255, 233, 87, 0.5)'},
 	{title: 'green', value: 'hsl(125, 82%, 35%)'},
-	{title: 'blue', value: 'hsl(255, 95%, 39%)'},
 	{title: 'fadedblue', value: 'hsl(231, 48%, 95%)'},
-	{title: 'lighterblue', value: 'hsla(266, 36%, 84%, 0.8)'},
-	{title: 'darkblue', value: 'hsl(230, 50%, 23%)'},
-	{title: 'verydarkblue', value: 'hsl(255, 65%, 19%)'}
-];
+	{title: 'bleach', value: 'hsl(72, 100%, 98%)'}
+]
 
 const grays = [
 	{title: 'white', value: 'hsl(0, 0%, 100%)'},
 	{title: 'superlightgray', value: 'hsl(0, 0%, 98%)'},
-	{title: 'bleach', value: 'hsl(72, 100%, 98%)'},
 	{title: 'slightgray', value: 'hsl(0, 0%, 95%)'},
-	{title: 'sand', value: 'hsl(46, 5%, 85%)'},
-	{title: 'lightgray', value: 'hsl(0, 0%, 91%)'},
+	{title: 'lightgray', value: 'hsl(45, 5%, 90%)'},
 	{title: 'mediumlightgray', value: 'hsl(0, 0%, 85%)'},
 	{title: 'mediumgray', value: 'hsl(0, 0%, 70%)'},
-	{title: 'gray', value: 'hsl(0, 0%, 50%)'},
+	{title: 'gray', value: 'hsl(0, 0%, 45%)'},
 	{title: 'dark', value: 'hsl(0, 0%, 30%)'},
 	{title: 'black', value: 'hsl(0, 0%, 10%)'}
-];
+]
 
 export default Ember.Route.extend({
 	model() {
-		return {colors, grays};
+		return {colors, grays}
 	}
-});
+})
