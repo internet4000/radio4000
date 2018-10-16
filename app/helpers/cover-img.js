5b34617a472a86d522696d038a2491daed0de375
Oskar Rough <oskar@rough.dk>
2018-10-16 16:16:55 +0200

    Add tests for cover-img helper

--- app/helpers/cover-img.js
+++ app/helpers/cover-img.js
@@ -1,24 +1,26 @@
-import Ember from 'ember';
+import Ember from 'ember'
 
 // This helper returns a full Cloudinary image url
 // with some optimization and formats.
 
-const base = 'https://res.cloudinary.com/radio4000/image/upload';
 const quality = 50;
 
-export function coverImg(params, {size, format}) {
-	const id = params[0];
+export function coverImg([id], {size = 200, format}) {
+	const base = `https://res.cloudinary.com/radio4000/image/upload/w_${size},h_${size},c_thumb`
 
+	// This is a predefined animated version of webp.
 	if (format === 'awebp') {
-		// This is an animated version of webp.
-		return `${base}/w_${size},h_${size},q_${quality},c_thumb,fl_awebp/${id}.webp`;
+		return `${base},q_${quality},fl_awebp/${id}.webp`
 	}
 
+	// Use this to overwrite the image format.
 	if (format) {
-		return `${base}/w_${size},h_${size},q_auto,c_thumb/${id}.${format}`;
+		quality = 'auto'
+		return `${base},q_${quality}/${id}.${format}`
 	}
 
-	return `${base}/q_${quality},w_${size},h_${size},c_thumb,c_fill,fl_lossy/${id}`;
+	// The default cover image.
+	return `${base},q_${quality},c_fill,fl_lossy/${id}`
 }
 
-export default Ember.Helper.helper(coverImg);
+export default Ember.Helper.helper(coverImg)
--- tests/unit/helpers/cover-img-test.js
+++ tests/unit/helpers/cover-img-test.js
@@ -6,5 +6,17 @@ module('Unit | Helper | cover img', function() {
 		// {{cover-img src size=250}}
 		const result = coverImg(['somestringid'], {size: 250})
 		assert.equal(typeof result, 'string')
+		assert.ok(result.indexOf('w_250') !== -1)
+		assert.ok(result.indexOf('h_250') !== -1)
+	})
+
+	test('it supports changing format', function(assert) {
+		const result = coverImg(['abc123'], {format: 'jpg'})
+		assert.ok(result.indexOf('.jpg') !== -1)
+	})
+
+	test('animated webp format includes fl_webap', function(assert) {
+		const result = coverImg(['abc123'], {format: 'awebp'})
+		assert.ok(result.indexOf('fl_awebp') !== -1)
 	})
 })
