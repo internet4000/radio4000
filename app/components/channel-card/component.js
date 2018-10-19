f1d295efffb1fb813d9863ddcde2adf7602e7a36
Oskar Rough <oskar@rough.dk>
2018-10-19 13:09:23 +0200

    Disable lazyloading on first two rows of card

--- app/components/channel-card/component.js
+++ app/components/channel-card/component.js
@@ -1,15 +1,17 @@
-import Ember from 'ember';
-
-const {Component, computed, get} = Ember;
+import Component from '@ember/component'
+import {computed, get} from '@ember/object'
 
 export default Component.extend({
 	tagName: 'article',
 	classNames: ['ChannelCard'],
 	classNameBindings: ['isActive', 'wide:ChannelCard--wide'],
 	isActive: computed.reads('channel.isInPlayer'),
-	title: computed('channel.{title,body}', function () {
-		const body = get(this, 'channel.body') || '';
-		const title = get(this, 'channel.title') || '';
-		return `${body} (${title})`;
+	title: computed('channel.{title,body}', function() {
+		const body = get(this, 'channel.body') || ''
+		const title = get(this, 'channel.title') || ''
+		return `${body} (${title})`
+	}),
+	disableLazyload: computed('index', function() {
+		return get(this, 'index') > 0 && get(this, 'index') < 10
 	})
-});
+})
