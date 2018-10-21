eeceb52a3b2144eab4bf57d5179cd222bc7b81b7
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2018-10-21 21:06:38 +0200

    fix: track.discogsUrl validator

--- app/validators/discogs-url.js
+++ app/validators/discogs-url.js
@@ -5,6 +5,10 @@ const defaultError = 'Enter a valid Discogs release or master URL'
 
 const DiscogsUrl = BaseValidator.extend({
 	validate(value) {
+		// if there is an empty value, it is value
+		// as we allow not to have a discogsURL
+		if (!value) return true
+
 		// If the parser finds a Discogs id it is valid.
 		try {
 			let result = mediaUrlParser(value)
