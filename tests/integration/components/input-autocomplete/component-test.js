605269f94ff45c5de88e0f32376c1e25c793b9cc
Oskar Rough <oskar@rough.dk>
2019-01-10 21:10:58 +0100

    Clean up

--- tests/integration/components/input-autocomplete/component-test.js
+++ tests/integration/components/input-autocomplete/component-test.js
@@ -1,26 +1,14 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import {module, test} from 'qunit'
+import {setupRenderingTest} from 'ember-qunit'
+import {render} from '@ember/test-helpers'
+import hbs from 'htmlbars-inline-precompile'
 
 module('Integration | Component | input-autocomplete', function(hooks) {
-  setupRenderingTest(hooks);
+	setupRenderingTest(hooks)
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`{{input-autocomplete}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#input-autocomplete}}
-        template block text
-      {{/input-autocomplete}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
-  });
-});
+	test('it renders', async function(assert) {
+		await render(hbs`{{input-autocomplete}}`)
+		assert.ok(this.element.querySelector('.aa-container'), 'has required class')
+		assert.ok(this.element.querySelector('input'), 'has an input')
+	})
+})
