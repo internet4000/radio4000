8f813208adad729046b324a48b0bed9a17304820
Oskar Rough <oskar@rough.dk>
2019-01-17 20:45:35 +0100

    Lint

--- tests/integration/components/navigation-toggle-btn/component-test.js
+++ tests/integration/components/navigation-toggle-btn/component-test.js
@@ -1,26 +1,13 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import {module, test} from 'qunit'
+import {setupRenderingTest} from 'ember-qunit'
+import {render} from '@ember/test-helpers'
+import hbs from 'htmlbars-inline-precompile'
 
 module('Integration | Component | navigation-toggle-btn', function(hooks) {
-  setupRenderingTest(hooks);
+	setupRenderingTest(hooks)
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`{{navigation-toggle-btn}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#navigation-toggle-btn}}
-        template block text
-      {{/navigation-toggle-btn}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
-  });
-});
+	test('it renders', async function(assert) {
+		await render(hbs`{{navigation-toggle-btn}}`)
+		assert.equal(this.element.textContent.trim(), '')
+	})
+})
