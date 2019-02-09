8c5c15151dbeeb231cdf526b637221b4eef63b6e
Oskar Rough <oskar@rough.dk>
2019-02-09 14:54:03 +0100

    Rename "clean" utility to "slugify"

--- tests/unit/utils/slugify.js
+++ tests/unit/utils/slugify.js
@@ -1,11 +1,11 @@
-import clean from 'radio4000/utils/clean'
+import slugify from 'radio4000/utils/slugify'
 
 import {module, test} from 'qunit'
 
-module('clean', function() {
-	test('it works', function(assert) {
+module('slugify', function() {
+	test('it slugifies a string', function(assert) {
 		let string = '&$#! S??omething// With UPPERCASE and SPACES and CH#$@#%(*!'
-		let result = clean(string)
+		let result = slugify(string)
 
 		assert.ok(result.indexOf('<') < 1, 'String can not contain HTML tags.')
 		assert.ok(result.indexOf('<') < 1, 'String can not contain HTML tags.')
