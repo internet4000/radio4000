4a1e5187cd20828407ea2d46c5bd18ddddaca605
hugurp <hugurp@protonmail.com>
2019-07-16 11:04:26 +0200

    bump version of r4 player

--- app/validators/provider-url.js
+++ app/validators/provider-url.js
@@ -3,11 +3,11 @@ import { mediaUrlParser } from 'media-url-parser'
 
 const ProviderUrl = BaseValidator.extend({
 	validate(value) {
+		const parsed = mediaUrlParser(value)
 		const knownProviders = {
 			youtube: true,
 			soundcloud: true
 		}
-		const parsed = mediaUrlParser(value)
 
 		if (parsed.url && parsed.id && parsed.provider) {
 			const canParse = knownProviders[parsed.provider] === true
