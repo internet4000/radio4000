8d84d7a000f239945372795a6d5f1567e916bb2e
hugurp <hugurp@protonmail.com>
2019-07-20 14:30:48 +0200

    lint

--- app/channel/home/route.js
+++ app/channel/home/route.js
@@ -2,6 +2,8 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
 	renderTemplate: function() {
-    this.render({'into':'application'});
-  }
+		this.render({
+			into: 'application'
+		});
+	}
 });
