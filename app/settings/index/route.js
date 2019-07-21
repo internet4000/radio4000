7b2973fb345c86f9c1177aa701641f15ddc1a3bb
hugurp <hugurp@protonmail.com>
2019-07-21 12:46:06 +0200

    fix bug preventing going back to channel after settings.channel

--- app/settings/index/route.js
+++ app/settings/index/route.js
@@ -4,6 +4,6 @@ export default Route.extend({
 	redirect() {
 		const channel = this.get('session.currentUser.channels.firstObject')
 		const defaultRoute = channel ? 'settings.channel' : 'settings.account'
-		this.replaceWith(defaultRoute)
+		this.transitionTo(defaultRoute)
 	}
 })
