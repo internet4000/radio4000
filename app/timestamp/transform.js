0833c02b4f349584d4337344bcad07cc00947895
Oskar Rough <oskar@rough.dk>
2019-07-21 14:08:47 +0200

    Workaround for firebase timestamps

--- app/timestamp/transform.js
+++ app/timestamp/transform.js
@@ -1,5 +1,5 @@
-import Transform from 'ember-data/transforms/transform'
-import { inject as service } from '@ember/service';
+import Transform from '@ember-data/serializer/transform'
+import {inject as service} from '@ember/service'
 
 export default Transform.extend({
 	firebaseApp: service(),
@@ -7,11 +7,12 @@ export default Transform.extend({
 	// From server to client.
 	deserialize(timestamp) {
 		return timestamp
-		// return new Date(timestamp)
 	},
+
 	// From client to server.
 	// Note, this ALWAYS sets a new timestamp. Do not use it for "created" type dates.
 	serialize() {
-		return this.firebaseApp.database.ServerValue.TIMESTAMP
+		return {'.sv': 'timestamp'}
+		// return this.firebaseApp.database.ServerValue.TIMESTAMP
 	}
 })
