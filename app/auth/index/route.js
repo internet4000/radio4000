fd8b5899d37a9b6544041c8f00a46e73d1092b46
hugurp <hugurp@protonmail.com>
2019-07-22 22:34:00 +0200

    lint

--- app/auth/index/route.js
+++ app/auth/index/route.js
@@ -1,9 +1,9 @@
-import Route from '@ember/routing/route';
-
-const {get} = Ember;
+import Ember from 'ember'
+import Route from '@ember/routing/route'
+const {get} = Ember
 
 export default Route.extend({
 	beforeModel() {
-		!get(this, 'session.isAuthenticated') && this.replaceWith('auth.login');
-	},
+		return !get(this, 'session.isAuthenticated') && this.replaceWith('auth.login')
+	}
 });
