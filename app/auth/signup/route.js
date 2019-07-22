2ddb2f4fd904acce2bb70c7ba4df7714d6417349
hugurp <hugurp@protonmail.com>
2019-07-22 22:14:20 +0200

    uniformize /auth template when login, and transition

--- app/auth/signup/route.js
+++ app/auth/signup/route.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
+import {get} from '@ember/object'
 
 export default Ember.Route.extend({
+	beforeModel(transition) {
+		if (get(this, 'session.isAuthenticated')) {
+			return transition.send('redirectAfterAuth')
+		}
+	},
 });
