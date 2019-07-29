46822e95114875c555df91436b721c44fe52d3f1
hugurp <hugurp@protonmail.com>
2019-07-29 22:30:16 +0200

    simplest icon sharer to a radio

--- app/components/channel-sharer-url/component.js
+++ app/components/channel-sharer-url/component.js
@@ -7,6 +7,7 @@ const { Component, computed, get } = Ember
 export default Component.extend({
 	// slug: '',
 	// showEmbed: false,
+	// showIcon: false,
 
 	permalink: computed('slug', function () {
 		return `https://radio4000.com/${get(this, 'slug')}`
@@ -14,10 +15,17 @@ export default Component.extend({
 	embedCode: computed('slug', function () {
 		return `<iframe src="https://api.radio4000.com/embed?slug=${get(this, 'slug')}" width="320" height="500" frameborder="0"></iframe>`
 	}),
+	iconUrl: computed('slug', function () {
+		const slug = get(this, 'slug')
+		return `<a href="https://radio4000.com/${slug}"><img width="30" src="https://assets.radio4000.com/icon-r4.svg" alt="${slug}@r4"></a>`
+	}),
 
 	url: computed('slug', 'showEmbed', function() {
 		if (get(this, 'showEmbed')) {
 			return get(this, 'embedCode')
+
+		} else if (get(this, 'showIcon')) {
+			return get(this, 'iconUrl')
 		}
 		return get(this, 'permalink')
 	})
