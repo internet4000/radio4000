b6838ad7e8b8e59cd12952a378fdaa4a822ce24b
hugurp <hugurp@protonmail.com>
2019-08-04 20:21:46 +0200

    fix 404 page for non existing track

--- app/channel/tracks/track/route.js
+++ app/channel/tracks/track/route.js
@@ -1,3 +1,11 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({});
+export default Ember.Route.extend({
+	model(params) {
+		return this.store
+			.findRecord('track', params.track_id)
+			.catch(error => {
+				return this.transitionTo('channel.tracks')
+			})
+	}
+});
