b17ea36b7c993dae5c5e985e662752a2241795be
oskarrough <oskar@rough.dk>
2019-10-13 13:06:43 +0200

    Switch away from Firebase timestamp for user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,16 +1,13 @@
 import DS from 'ember-data';
-// import { inject as service } from '@ember/service';
-import firebase from 'firebase/app'
+// import * as firebase from 'firebase/app'
 
 const {Model, attr, belongsTo, hasMany} = DS;
 
 export default Model.extend({
-	// firebaseApp: service(),
-
 	created: attr('number', {
 		defaultValue() {
-			// return new Date().getTime()
-			return firebase.database.ServerValue.TIMESTAMP
+			return new Date().getTime()
+			// return firebase.database.ServerValue.TIMESTAMP
 		}
 	}),
 	channels: hasMany('channel'),
