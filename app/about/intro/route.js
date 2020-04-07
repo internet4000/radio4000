1df561bbe1dee2691501c1cfa4f4d4d1b467ad01
hugurp <hugurp@pm.me>
2020-04-07 09:45:24 +0200

    fix (quick) about intro use channelIndexRoute

--- app/about/intro/route.js
+++ app/about/intro/route.js
@@ -2,12 +2,4 @@ import ChannelsIndexRoute from 'radio4000/channels/index/route'
 
 // Reuse the "random featured radios" model
 
-export default ChannelsIndexRoute.extend({
-	model() {
-		return this.store.query('channel', {
-			orderBy: 'isFeatured',
-			equalTo: true,
-			limitToLast: 10
-		});
-	}
-})
+export default ChannelsIndexRoute.extend({})
