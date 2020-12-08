9f75d951370d4bf7fb86a671292056275fde14b6
hugurp <hugurp@pm.me>
2020-12-08 08:12:37 +0100

    fix signup error, missing notification

--- app/auth/signup/controller.js
+++ app/auth/signup/controller.js
@@ -1,35 +1,34 @@
-import Ember from 'ember';
+import Ember from 'ember'
 
-const {Controller, get, inject, debug} = Ember;
+const {Controller, get, inject, debug} = Ember
 
 export default Controller.extend({
 	firebaseApp: inject.service(),
 
 	async createFirebaseUser(email, password) {
-		let auth = get(this, 'firebaseApp').auth();
-		const user = await auth.createUserWithEmailAndPassword(email, password);
+		let auth = get(this, 'firebaseApp').auth()
+		const user = await auth.createUserWithEmailAndPassword(email, password)
 		return user
 	},
 
 	onSignupError(err) {
-		const messages = get(this, 'flashMessages');
-		let msg;
+		const messages = get(this, 'flashMessages')
+		let msg
 
 		if (err.code === 'auth/email-already-in-use') {
-			msg = 'There already exists an account with the given email address.';
+			msg = 'There already exists an account with the given email address.'
 		}	else if (err.code === 'auth/invalid-email') {
-			msg = 'Email address is not valid.';
+			msg = 'Email address is not valid.'
 		}	else if (err.code === 'auth/operation-not-allowed') {
-			msg = 'Email/password accounts are not enabled.';
+			msg = 'Email/password accounts are not enabled.'
 		}	else if (err.code === 'auth/weak-password') {
-			msg = 'Password is not strong enough.';
+			msg = 'Password is not strong enough.'
 		} else {
-			debug('Signup error is not referenced');
-			debug(err);
+			debug('Signup error is not referenced')
 		}
 
 		if (msg) {
-			messages.warning(msg, {timeout: 8000});
+			messages.warning(msg, {timeout: 8000})
 		}
 	},
 
@@ -41,7 +40,8 @@ export default Controller.extend({
 					await this.createFirebaseUser(email, password)
 					this.send('login', provider, email, password)
 				} catch (error) {
-					debug(error);
+					this.onSignupError(error)
+					debug(error)
 				}
 			} else {
 				this.send('login', provider)
